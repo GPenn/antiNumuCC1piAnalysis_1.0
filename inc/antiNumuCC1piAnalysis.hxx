cd532ecd372084766561aab5c5f058bdf4558665
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-06 14:32:23 +0000

    Add missing comma

--- inc/antiNumuCC1piAnalysis.hxx
+++ inc/antiNumuCC1piAnalysis.hxx
@@ -51,7 +51,7 @@ class antiNumuCC1piAnalysis: public baseTrackerAnalysis {
     HMNT_NEcalSegments,
     HMNT_ecal_EMenergy,
     HMNT_ecal_length,
-    HMNT_ecal_mippion
+    HMNT_ecal_mippion,
   
     enumStandardMicroTreesLast_antiNumuCC1piAnalysis
   };
