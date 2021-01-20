dcd25ab89213cb503b06b504a563ac618a785347
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-20 15:16:45 +0000

    Add plots for all segment combinations

--- macro/sig_bkg_ecalsegsamples.C
+++ macro/sig_bkg_ecalsegsamples.C
@@ -7,5 +7,12 @@
   TCanvas sig_bkg_bothsegs;
   draw.Draw(default,"topology==1",2,-0.5,1.5,"topology","accum_level[][1]>7&&selmu_necals==1&&HMNT_NEcalSegments==1");
   
+  TCanvas sig_bkg_museg;
+  draw.Draw(default,"topology==1",2,-0.5,1.5,"topology","accum_level[][1]>7&&selmu_necals==1&&HMNT_NEcalSegments==0");
   
+  TCanvas sig_bkg_piseg;
+  draw.Draw(default,"topology==1",2,-0.5,1.5,"topology","accum_level[][1]>7&&selmu_necals==0&&HMNT_NEcalSegments==1");
+  
+  TCanvas sig_bkg_nosegs;
+  draw.Draw(default,"topology==1",2,-0.5,1.5,"topology","accum_level[][1]>7&&selmu_necals==0&&HMNT_NEcalSegments==0");
 }
