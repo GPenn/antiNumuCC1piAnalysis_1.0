c828ab873115e1f2e984838b2e87527c7549631d
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-20 14:58:00 +0000

    Add plots to check multiple segments

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -7,6 +7,12 @@
   TCanvas selmu_necals;
   draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5");
   
+  TCanvas selmu_necals_multiple;
+  draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5&&selmu_necals>1");
+  
   TCanvas selpi_necals;
   draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5");
+  
+  TCanvas selpi_necals_multiple;
+  draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>1");
 }
