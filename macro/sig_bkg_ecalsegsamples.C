5abdf915f2dd1bd7bbecd0e5b1d021a242839a09
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-20 15:11:03 +0000

    Create sig_bkg_ecalsegsamples.C

---
+++ macro/sig_bkg_ecalsegsamples.C
@@ -0,0 +1,11 @@
+{
+  DrawingTools draw("output/output_p6T_run7_all.root");
+  DataSample mc("output/output_p6T_run7_all.root");
+
+  draw.DumpPOT(mc);
+  
+  TCanvas sig_bkg_bothsegs;
+  draw.Draw(default,"topology==1",5,0,5,"topology","accum_level[][1]>7&&selmu_necals==1&&HMNT_NEcalSegments==1");
+  
+  
+}
