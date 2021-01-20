3327c15edb14e717324f742a79db29f3d8782e34
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-20 15:13:11 +0000

    Try fractional limits to centre bin value

--- macro/sig_bkg_ecalsegsamples.C
+++ macro/sig_bkg_ecalsegsamples.C
@@ -5,7 +5,7 @@
   draw.DumpPOT(mc);
   
   TCanvas sig_bkg_bothsegs;
-  draw.Draw(default,"topology==1",5,0,5,"topology","accum_level[][1]>7&&selmu_necals==1&&HMNT_NEcalSegments==1");
+  draw.Draw(default,"topology==1",-0.5,0,1.5,"topology","accum_level[][1]>7&&selmu_necals==1&&HMNT_NEcalSegments==1");
   
   
 }
