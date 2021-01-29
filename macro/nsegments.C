dd43f8ee2a589f907a4f61f6f3e7956c37b65251
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-29 16:17:31 +0000

    Add lower limit to HMNT ecals

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -11,7 +11,7 @@
   draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5&&selmu_necals>1");
   
   TCanvas selpi_necals;
-  draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5");
+  draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>=0");
   
   TCanvas selpi_necals_multiple;
   draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>1");
