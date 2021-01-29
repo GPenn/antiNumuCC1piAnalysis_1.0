0735152ae2a8d353a99fecae25c1575c3620ad77
Gabriel Penn <callidus@hotmail.co.uk>
2021-01-29 16:19:59 +0000

    Only plot events with at least 1 ECal segment

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -5,13 +5,13 @@
   draw.DumpPOT(mc);
   
   TCanvas selmu_necals;
-  draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5");
+  draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5&&selmu_necals>0");
   
   TCanvas selmu_necals_multiple;
   draw.Draw(default,"selmu_necals",5,0,5,"topology","accum_level[][1]>5&&selmu_necals>1");
   
   TCanvas selpi_necals;
-  draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>=0");
+  draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>0");
   
   TCanvas selpi_necals_multiple;
   draw.Draw(default,"HMNT_NEcalSegments",5,0,5,"topology","accum_level[][1]>5&&HMNT_NEcalSegments>1");
