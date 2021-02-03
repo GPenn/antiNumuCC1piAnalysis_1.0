72dc071f2163c13c48fd445effd9603cc8756aa7
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-03 17:17:55 +0000

    Update POT.C

--- macro/POT.C
+++ macro/POT.C
@@ -1,17 +1,17 @@
 {
   DrawingTools run5("output/output_p6T_run5_all.root");
   DataSample mc_run5("output/output_p6T_run5_all.root");
-  draw.DumpPOT(mc);
+  run5.DumpPOT(mc_run5);
   
   DrawingTools run6("output/output_p6T_run6_all.root");
   DataSample mc_run6("output/output_p6T_run6_all.root");
-  draw.DumpPOT(mc);
+  run6.DumpPOT(mc_run6);
   
   DrawingTools run7("output/output_p6T_run7_all.root");
   DataSample mc_run7("output/output_p6T_run7_all.root");
-  draw.DumpPOT(mc);
+  run7.DumpPOT(mc_run7);
   
   DrawingTools run9("output/output_p6T_run9_all.root");
   DataSample mc_run9("output/output_p6T_run9_all.root");
-  draw.DumpPOT(mc);
+  run9.DumpPOT(mc_run9);
  }
