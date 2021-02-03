1554851284a0a1ce22bb34f891d0a57cb5c5b7c3
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-03 11:58:56 +0000

    Adjust all position axes

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -30,17 +30,17 @@
   TCanvas selmu_ecal_endpos_correlation0;
   draw.SetTitleX("selmu_ecal_showerendpos[1][0]");
   draw.SetTitleY("selmu_ecal_showerendpos[0][0]");
-  draw.Draw(default,"selmu_ecal_showerendpos[0][0]:selmu_ecal_showerendpos[1][0]",50,-2000,8000,50,-2000,8000,"all","accum_level[][1]>5&&selmu_necals>1","colz");
+  draw.Draw(default,"selmu_ecal_showerendpos[0][0]:selmu_ecal_showerendpos[1][0]",50,-2000,2000,50,-2000,2000,"all","accum_level[][1]>5&&selmu_necals>1","colz");
   
   TCanvas selmu_ecal_endpos_correlation1;
   draw.SetTitleX("selmu_ecal_showerendpos[1][1]");
   draw.SetTitleY("selmu_ecal_showerendpos[0][1]");
-  draw.Draw(default,"selmu_ecal_showerendpos[0][1]:selmu_ecal_showerendpos[1][1]",50,-2000,8000,50,-2000,8000,"all","accum_level[][1]>5&&selmu_necals>1","colz");
+  draw.Draw(default,"selmu_ecal_showerendpos[0][1]:selmu_ecal_showerendpos[1][1]",50,500,2000,50,500,2000,"all","accum_level[][1]>5&&selmu_necals>1","colz");
   
   TCanvas selmu_ecal_endpos_correlation2;
   draw.SetTitleX("selmu_ecal_showerendpos[1][2]");
   draw.SetTitleY("selmu_ecal_showerendpos[0][2]");
-  draw.Draw(default,"selmu_ecal_showerendpos[0][2]:selmu_ecal_showerendpos[1][2]",50,-2000,8000,50,-2000,8000,"all","accum_level[][1]>5&&selmu_necals>1","colz");
+  draw.Draw(default,"selmu_ecal_showerendpos[0][2]:selmu_ecal_showerendpos[1][2]",50,500,3500,50,500,3500,"all","accum_level[][1]>5&&selmu_necals>1","colz");
   
   TCanvas selmu_ecal_endpos_correlation3;
   draw.SetTitleX("selmu_ecal_showerendpos[1][3]");
