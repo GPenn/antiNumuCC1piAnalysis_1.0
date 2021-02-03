0e5d547f497bb1a78652c3720212264fb694e007
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-03 17:15:27 +0000

    Create POT.C

---
+++ macro/POT.C
@@ -0,0 +1,17 @@
+{
+  DrawingTools run5("output/output_p6Trun5_all.root");
+  DataSample mc_run5("output/output_p6Trun5_all.root");
+  draw.DumpPOT(mc);
+  
+  DrawingTools run6("output/output_p6Trun6_all.root");
+  DataSample mc_run6("output/output_p6Trun6_all.root");
+  draw.DumpPOT(mc);
+  
+  DrawingTools run7("output/output_p6Trun7_all.root");
+  DataSample mc_run7("output/output_p6Trun7_all.root");
+  draw.DumpPOT(mc);
+  
+  DrawingTools run9("output/output_p6Trun9_all.root");
+  DataSample mc_run9("output/output_p6Trun9_all.root");
+  draw.DumpPOT(mc);
+ }
