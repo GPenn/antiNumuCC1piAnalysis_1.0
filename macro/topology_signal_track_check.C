3f6e497db240b74e28a305267340978dcd04e26a
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-04 12:42:04 +0000

    Comment out earlier plots, add HMNT requirement, stack by topology

--- macro/topology_signal_track_check.C
+++ macro/topology_signal_track_check.C
@@ -4,7 +4,7 @@
 
   draw.DumpPOT(mc);
   
-  TCanvas muontrack_sig;
+  /*TCanvas muontrack_sig;
   draw.Draw(default,"particle==-13",2,0,2,"particle","accum_level[][1]>8&&topology==1");
   
   TCanvas muontrack_bkg;
@@ -14,11 +14,11 @@
   draw.Draw(default,"HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology==1");
   
   TCanvas piontrack_bkg;
-  draw.Draw(default,"HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology!=1");
+  draw.Draw(default,"HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology!=1");*/
   
   TCanvas bothtracks_sig;
-  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"particle","accum_level[][1]>8&&topology==1");
+  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology==1&&HMNT_mom>0");
   
   TCanvas bothtracks_bkg;
-  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"particle","accum_level[][1]>8&&topology!=1");
+  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology!=1&&HMNT_mom>0");
 }
