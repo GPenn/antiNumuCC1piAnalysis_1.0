3cbfbeb021e5219cac6b8d214698109f736fc4eb
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-04 12:32:31 +0000

    Add bothtracks plots

--- macro/topology_signal_track_check.C
+++ macro/topology_signal_track_check.C
@@ -4,10 +4,21 @@
 
   draw.DumpPOT(mc);
   
-  TCanvas muontrack;
+  TCanvas muontrack_sig;
   draw.Draw(default,"particle==-13",2,0,2,"particle","accum_level[][1]>8&&topology==1");
   
-  TCanvas piontrack;
-  draw.Draw(default,"HMNT_truepdg==211",2,0,2,"topology","accum_level[][1]>8&&topology==1");
+  TCanvas muontrack_bkg;
+  draw.Draw(default,"particle==-13",2,0,2,"particle","accum_level[][1]>8&&topology!=1");
   
+  TCanvas piontrack_sig;
+  draw.Draw(default,"HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology==1");
+  
+  TCanvas piontrack_bkg;
+  draw.Draw(default,"HMNT_truepdg==-211",2,0,2,"topology","accum_level[][1]>8&&topology!=1");
+  
+  TCanvas bothtracks_sig;
+  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"particle","accum_level[][1]>8&&topology==1");
+  
+  TCanvas bothtracks_bkg;
+  draw.Draw(default,"particle==-13&&HMNT_truepdg==-211",2,0,2,"particle","accum_level[][1]>8&&topology!=1");
 }
