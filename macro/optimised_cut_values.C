0a1a39300cab1b316355a18d3ff0701d5373fcab
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-05 14:53:48 +0000

    Add bothseg muon mippion

--- macro/optimised_cut_values.C
+++ macro/optimised_cut_values.C
@@ -8,5 +8,9 @@
   draw.DrawCutLineVertical(1.0,true,"l");
   draw.DrawCutLineVertical(1.1,true,"l");
   
-  
+  TCanvas bothseg_muon_mippion;
+  draw.SetTitleX("Antimuon candidate ECal MipPion");
+  draw.Draw(default,"selmu_ecal_mippion",50,-50,50,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
+  draw.DrawCutLineVertical(0.0,true,"l");
+  draw.DrawCutLineVertical(8.3,true,"l");
 }
