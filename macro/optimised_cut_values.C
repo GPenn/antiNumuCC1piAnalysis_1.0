6d83daca7e10b99761f5bdc9bfc6915904e8a7c4
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-05 11:52:23 +0000

    Fix HMNT_NEcalSegments

--- macro/optimised_cut_values.C
+++ macro/optimised_cut_values.C
@@ -4,7 +4,7 @@
   
   draw.SetTitleX("Antimuon candidate ECal energy/length");
   
-  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_necalsegments>0");
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
   
   
 }
