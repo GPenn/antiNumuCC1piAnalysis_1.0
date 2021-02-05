ba8ea8978d82258a98e67877eb9d00c6f7204af9
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-05 11:47:36 +0000

    Create optimised_cut_values.C

---
+++ macro/optimised_cut_values.C
@@ -0,0 +1,10 @@
+{
+  DrawingTools draw("output/output_p6T_allruns.root");
+  DataSample mc("output/output_p6T_allruns.root");
+  
+  draw.SetTitleX("Antimuon candidate ECal energy/length");
+  
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_necalsegments>0");
+  
+  
+}
