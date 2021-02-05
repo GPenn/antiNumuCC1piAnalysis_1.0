8d577bf2131d73303378c182e4b14f865881538c
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-05 15:41:59 +0000

    Add pion candidate plots for bothseg

--- macro/optimised_cut_values.C
+++ macro/optimised_cut_values.C
@@ -10,7 +10,19 @@
   
   TCanvas bothseg_muon_mippion;
   draw.SetTitleX("Antimuon candidate ECal MipPion");
-  draw.Draw(default,"selmu_ecal_mippion",50,-50,50,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
+  draw.Draw(default,"selmu_ecal_mippion",50,-30,70,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
   draw.DrawCutLineVertical(0.0,true,"l");
   draw.DrawCutLineVertical(8.3,true,"l");
+  
+  TCanvas bothseg_pion_EbyL;
+  draw.SetTitleX("Pi- candidate ECal energy/length");
+  draw.Draw(default,"HMNT_ecal_EMenergy/HMNT_ecal_length",50,0,4,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
+  draw.DrawCutLineVertical(1.0,true,"r");
+  draw.DrawCutLineVertical(0.1,true,"r");
+  
+  TCanvas bothseg_muon_mippion;
+  draw.SetTitleX("Pi- candidate ECal MipPion");
+  draw.Draw(default,"HMNT_ecal_mippion",50,-30,70,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
+  draw.DrawCutLineVertical(0.0,true,"r");
+  draw.DrawCutLineVertical(-6.0,true,"r");
 }
