c83e58292efb143a02348160d2e03d055d09cc89
Gabriel Penn <callidus@hotmail.co.uk>
2021-02-05 12:00:00 +0000

    Update optimised_cut_values.C

--- macro/optimised_cut_values.C
+++ macro/optimised_cut_values.C
@@ -2,9 +2,13 @@
   DrawingTools draw("output/output_p6T_allruns.root");
   DataSample mc("output/output_p6T_allruns.root");
   
+  TCanvas muon_EbyL_overall;
   draw.SetTitleX("Antimuon candidate ECal energy/length");
-  
   draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"topology","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0");
+  
+  TCanvas muon_EbyL_signal;
+  draw.SetTitleX("Antimuon candidate ECal energy/length");
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"","accum_level[][1]>8&&selmu_necals>0&&HMNT_NEcalSegments>0&&topology==1");
   
   
 }
