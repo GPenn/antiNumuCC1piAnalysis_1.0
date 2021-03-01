996f1e2964f7932f708f72123478f5f3af189b6f
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-01 12:10:09 +0000

    Change likelihood axis cap to 1

--- macro/PID_var_dist.C
+++ macro/PID_var_dist.C
@@ -4,17 +4,17 @@
   
   TCanvas selmu_tpc_like_mu;
   draw.SetTitleX("Muon candidate TPC likelihood - muon");
-  draw.Draw(default,"selmu_tpc_like_mu",50,0,2,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_tpc_like_mu",50,0,1,"topology","accum_level[][1]>7");
   
   TCanvas selmu_tpc_like_e;
   draw.SetTitleX("Muon candidate TPC likelihood - electron");
-  draw.Draw(default,"selmu_tpc_like_e",50,0,2,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_tpc_like_e",50,0,1,"topology","accum_level[][1]>7");
   
   TCanvas selmu_tpc_like_p;
   draw.SetTitleX("Muon candidate TPC likelihood - proton");
-  draw.Draw(default,"selmu_tpc_like_p",50,0,2,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_tpc_like_p",50,0,1,"topology","accum_level[][1]>7");
   
   TCanvas selmu_tpc_like_pi;
   draw.SetTitleX("Muon candidate TPC likelihood - pion");
-  draw.Draw(default,"selmu_tpc_like_pi",50,0,2,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");
 }
