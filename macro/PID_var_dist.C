3140c9d06c008f0c300195d18192b88a548c3dc4
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-01 16:42:17 +0000

    Add FGD2 pulls

--- macro/PID_var_dist.C
+++ macro/PID_var_dist.C
@@ -18,7 +18,7 @@
   draw.SetTitleX("Muon candidate TPC likelihood - pion");
   draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");*/
   
-  TCanvas selmu_fgd1_pull_mu;
+  /*TCanvas selmu_fgd1_pull_mu;
   draw.SetTitleX("Muon candidate FGD1 pull - muon");
   draw.Draw(default,"selmu_fgd1_pull_mu",50,-10,40,"topology","accum_level[][1]>7");
   
@@ -32,6 +32,22 @@
   
   TCanvas selmu_fgd1_pull_pi;
   draw.SetTitleX("Muon candidate FGD1 pull - pi");
-  draw.Draw(default,"selmu_fgd1_pull_pi",50,-10,40,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_fgd1_pull_pi",50,-10,40,"topology","accum_level[][1]>7");*/
+  
+  TCanvas selmu_fgd2_pull_mu;
+  draw.SetTitleX("Muon candidate FGD2 pull - muon");
+  draw.Draw(default,"selmu_fgd2_pull_mu",50,-10,40,"topology","accum_level[][1]>7");
+  
+  TCanvas selmu_fgd2_pull_e;
+  draw.SetTitleX("Muon candidate FGD2 pull - electron");
+  draw.Draw(default,"selmu_fgd2_pull_e",50,-10,40,"topology","accum_level[][1]>7");
+  
+  TCanvas selmu_fgd2_pull_p;
+  draw.SetTitleX("Muon candidate FGD2 pull - proton");
+  draw.Draw(default,"selmu_fgd2_pull_p",50,-10,40,"topology","accum_level[][1]>7");
+  
+  TCanvas selmu_fgd2_pull_pi;
+  draw.SetTitleX("Muon candidate FGD2 pull - pi");
+  draw.Draw(default,"selmu_fgd2_pull_pi",50,-10,40,"topology","accum_level[][1]>7");
   
 }
