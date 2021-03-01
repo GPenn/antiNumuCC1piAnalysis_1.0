cb4f8fde7f1b74304150c59e9412ae219fa27983
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-01 15:18:05 +0000

    Change input file, start adding FGD vars

--- macro/PID_var_dist.C
+++ macro/PID_var_dist.C
@@ -1,8 +1,8 @@
 {
-  DrawingTools draw("output/output_p6T_allruns_tpcliketest.root");
-  DataSample mc("output/output_p6T_allruns_tpcliketest.root");
+  DrawingTools draw("output/output_p6T_run5_fgdpulltest.root");
+  DataSample mc("output/output_p6T_run5_fgdpulltest.root");
   
-  TCanvas selmu_tpc_like_mu;
+  /*TCanvas selmu_tpc_like_mu;
   draw.SetTitleX("Muon candidate TPC likelihood - muon");
   draw.Draw(default,"selmu_tpc_like_mu",50,0,1,"topology","accum_level[][1]>7");
   
@@ -16,5 +16,10 @@
   
   TCanvas selmu_tpc_like_pi;
   draw.SetTitleX("Muon candidate TPC likelihood - pion");
-  draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");
+  draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");*/
+  
+  TCanvas selmu_fgd1_pull_mu;
+  draw.SetTitleX("Muon candidate FGD1 pull - muon");
+  draw.Draw(default,"selmu_fgd1_pull_mu",50,-1,1,"topology","accum_level[][1]>7");
+  
 }
