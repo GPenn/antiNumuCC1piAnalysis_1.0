97dff0a117df2e9f1b0df769221f47d9151d40f9
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-02 14:52:31 +0000

    Reenable all plots and add fgd segment booleans

--- macro/PID_var_dist.C
+++ macro/PID_var_dist.C
@@ -1,8 +1,8 @@
 {
-  DrawingTools draw("output/output_p6T_run5_fgdpulltest.root");
-  DataSample mc("output/output_p6T_run5_fgdpulltest.root");
+  DrawingTools draw("output/output_p6T_run5_PIDvartest.root");
+  DataSample mc("output/output_p6T_run5_PIDvartest.root");
   
-  /*TCanvas selmu_tpc_like_mu;
+  TCanvas selmu_tpc_like_mu;
   draw.SetTitleX("Muon candidate TPC likelihood - muon");
   draw.Draw(default,"selmu_tpc_like_mu",50,0,1,"topology","accum_level[][1]>7");
   
@@ -16,9 +16,13 @@
   
   TCanvas selmu_tpc_like_pi;
   draw.SetTitleX("Muon candidate TPC likelihood - pion");
-  draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");*/
+  draw.Draw(default,"selmu_tpc_like_pi",50,0,1,"topology","accum_level[][1]>7");
   
-  /*TCanvas selmu_fgd1_pull_mu;
+  TCanvas selmu_has_fgd1seg;
+  draw.SetTitleX("Muon candidate has FGD1 segment");
+  draw.Draw(default,"selmu_has_fgd1seg",2,0,2,"topology","accum_level[][1]>7");
+  
+  TCanvas selmu_fgd1_pull_mu;
   draw.SetTitleX("Muon candidate FGD1 pull - muon");
   draw.Draw(default,"selmu_fgd1_pull_mu",50,-10,40,"topology","accum_level[][1]>7");
   
@@ -32,7 +36,11 @@
   
   TCanvas selmu_fgd1_pull_pi;
   draw.SetTitleX("Muon candidate FGD1 pull - pi");
-  draw.Draw(default,"selmu_fgd1_pull_pi",50,-10,40,"topology","accum_level[][1]>7");*/
+  draw.Draw(default,"selmu_fgd1_pull_pi",50,-10,40,"topology","accum_level[][1]>7");
+  
+  TCanvas selmu_has_fgd2seg;
+  draw.SetTitleX("Muon candidate has FGD2 segment");
+  draw.Draw(default,"selmu_has_fgd2seg",2,0,2,"topology","accum_level[][1]>7");
   
   TCanvas selmu_fgd2_pull_mu;
   draw.SetTitleX("Muon candidate FGD2 pull - muon");
