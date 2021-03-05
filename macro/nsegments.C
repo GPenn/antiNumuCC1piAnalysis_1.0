0577a482f3cd8665059530369e81563b9248fdd3
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-05 17:07:24 +0000

    Comment out PID var correlation plots, add average PID var plots

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -1,6 +1,6 @@
 {
-  DrawingTools draw("output/output_p6T_allruns.root");
-  DataSample mc("output/output_p6T_allruns.root");
+  DrawingTools draw("output/output_p6T_run5_ecalavgvartest.root");
+  DataSample mc("output/output_p6T_run5_ecalavgvartest.root");
 
   draw.DumpPOT(mc);
   
@@ -52,7 +52,7 @@
   draw.SetTitleY("selmu_ecal_showerendpos[0][2]");
   draw.Draw(default,"selmu_ecal_showerendpos[0][2]:selmu_ecal_showerendpos[0][0]",50,-2000,2000,50,500,3500,"all","accum_level[][1]>5&&selmu_necals>1","colz");*/
   
-  TCanvas selmu_ecal_mippion_correlation;
+  /*TCanvas selmu_ecal_mippion_correlation;
   draw.SetTitleX("selmu_ecal_mippion[1]");
   draw.SetTitleY("selmu_ecal_mippion[0]");
   draw.Draw(default,"selmu_ecal_mippion[0]:selmu_ecal_mippion[1]",50,-20,30,50,-20,30,"all","accum_level[][1]>4&&selmu_necals>1","colz");
@@ -69,4 +69,9 @@
   draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]:selmu_ecal_EMenergy[1]/selmu_ecal_length[1]",50,0,4,50,0,4,"all","accum_level[][1]>4&&selmu_necals>1&&particle==-13","colz");
   TCanvas selmu_ecal_EbyL_correlation_bkg;
   draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]:selmu_ecal_EMenergy[1]/selmu_ecal_length[1]",50,0,4,50,0,4,"all","accum_level[][1]>4&&selmu_necals>1&&particle!=-13","colz");
+  */
+  TCanvas selmu_ecal_avg_mippion;
+  draw.Draw(default,"selmu_ecal_avg_mippion",50,-20,30,"topology","accum_level[][1]>4&&selmu_necals>0");
+  TCanvas selmu_ecal_avg_EbyL;
+  draw.Draw(default,"selmu_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&selmu_necals>0");
 }
