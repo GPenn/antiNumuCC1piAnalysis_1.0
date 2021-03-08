75c5bdaac78f5d8f2777b5120725b65bf7f16dbe
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-08 14:23:00 +0000

    Comment out previous plots, add consistency check for 1ecal case

--- macro/nsegments.C
+++ macro/nsegments.C
@@ -70,7 +70,7 @@
   TCanvas selmu_ecal_EbyL_correlation_bkg;
   draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]:selmu_ecal_EMenergy[1]/selmu_ecal_length[1]",50,0,4,50,0,4,"all","accum_level[][1]>4&&selmu_necals>1&&particle!=-13","colz");
   */
-  TCanvas selmu_ecal_avg_mippion;
+  /*TCanvas selmu_ecal_avg_mippion;
   draw.Draw(default,"selmu_ecal_avg_mippion",50,-20,80,"topology","accum_level[][1]>4&&selmu_necals>0");
   TCanvas selmu_ecal_avg_mippion_1ecal;
   draw.Draw(default,"selmu_ecal_avg_mippion",50,-20,80,"topology","accum_level[][1]>4&&selmu_necals==1");
@@ -94,6 +94,25 @@
   TCanvas HMNT_ecal_avg_EbyL_1ecal;
   draw.Draw(default,"HMNT_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&HMNT_NEcalSegments==1");
   TCanvas HMNT_ecal_avg_EbyL_multiplecals;
-  draw.Draw(default,"HMNT_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&HMNT_NEcalSegments>1");
+  draw.Draw(default,"HMNT_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&HMNT_NEcalSegments>1");*/
   
+  TCanvas selmu_ecal_avg_mippion_1ecal;
+  draw.Draw(default,"selmu_ecal_avg_mippion",50,-20,80,"topology","accum_level[][1]>4&&selmu_necals==1");
+  TCanvas selmu_ecal_mippion_1ecal;
+  draw.Draw(default,"selmu_ecal_mippion[0]",50,-20,80,"topology","accum_level[][1]>4&&selmu_necals==1");
+  
+  TCanvas selmu_ecal_avg_EbyL_1ecal;
+  draw.Draw(default,"selmu_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&selmu_necals==1");
+  TCanvas selmu_ecal_EbyL_1ecal;
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0,4,"topology","accum_level[][1]>4&&selmu_necals==1");
+  
+  TCanvas HMNT_ecal_avg_mippion_1ecal;
+  draw.Draw(default,"HMNT_ecal_avg_mippion",50,-20,80,"topology","accum_level[][1]>4&&HMNT_NEcalSegments==1");
+  TCanvas HMNT_ecal_mippion_1ecal;
+  draw.Draw(default,"HMNT_ecal_mippion",50,-20,80,"topology","accum_level[][1]>4&&HMNT_NEcalSegments==1");
+  
+  TCanvas HMNT_ecal_avg_EbyL_1ecal;
+  draw.Draw(default,"HMNT_ecal_avg_EbyL",50,0,4,"topology","accum_level[][1]>4&&HMNT_NEcalSegments==1");
+  TCanvas HMNT_ecal_EbyL_1ecal;
+  draw.Draw(default,"HMNT_ecal_EMenergy/HMNT_ecal_length",50,0,4,"topology","accum_level[][1]>4&&HMNT_NEcalSegments==1");
 }
