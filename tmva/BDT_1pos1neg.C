8ea7817043bd5912da50b5a8d828bf85d058b2ca
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-19 16:40:13 +0000

    Try including numbers of Ecal segments

--- tmva/BDT_1pos1neg.C
+++ tmva/BDT_1pos1neg.C
@@ -197,7 +197,7 @@ void BDT_1pos1neg( TString myMethodList = "" )
    // Muon candidate global variables
    //factory->AddVariable( "selmu_mom",                               "Muon candidate global momentum",        "", 'F' );
    // Muon candidate ECal variables
-   //factory->AddVariable( "selmu_necals",                            "Muon candidate ECal segments",          "", 'I' );
+   factory->AddVariable( "selmu_necals",                            "Muon candidate ECal segments",          "", 'I' );
    //factory->AddVariable( "selmu_ecal_bestseg_EMenergy",             "Muon candidate ECal EM energy",         "", 'F' );
    //factory->AddVariable( "selmu_ecal_bestseg_EbyL",                 "Muon candidate ECal E/L",               "", 'F' );
    //factory->AddVariable( "selmu_ecal_bestseg_mippion",              "Muon candidate ECal MipPion",           "", 'F' );
@@ -219,7 +219,7 @@ void BDT_1pos1neg( TString myMethodList = "" )
    // Pion candidate global variables
    //factory->AddVariable( "HMNT_mom",                                "Pion candidate global momentum",        "", 'F' );
    // Muon candidate ECal variables
-   //factory->AddVariable( "HMNT_NEcalSegments",                      "Pion candidate ECal segments",          "", 'I' );
+   factory->AddVariable( "HMNT_NEcalSegments",                      "Pion candidate ECal segments",          "", 'I' );
    //factory->AddVariable( "HMNT_ecal_bestseg_EMenergy",              "Pion candidate ECal EM energy",         "", 'F' );
    //factory->AddVariable( "HMNT_ecal_bestseg_EbyL",                  "Pion candidate ECal E/L",               "", 'F' );
    //factory->AddVariable( "HMNT_ecal_bestseg_mippion",               "Pion candidate ECal MipPion",           "", 'F' );
