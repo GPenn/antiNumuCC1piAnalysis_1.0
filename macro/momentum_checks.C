522da2dbd179510ea4bebe0dd95f012640acb59e
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-26 15:08:00 +0000

    Extend axis range again

--- macro/momentum_checks.C
+++ macro/momentum_checks.C
@@ -6,12 +6,12 @@
   draw.DumpPOT(mc);
   
   TCanvas selmu_mom;
-  draw.Draw(default,"selmu_mom[0]",50,0,10000,"topology","accum_level[][1]>4");
+  draw.Draw(default,"selmu_mom[0]",50,0,15000,"topology","accum_level[][1]>4");
   TCanvas selmu_mom_low;
   draw.Draw(default,"selmu_mom[0]",50,0,500,"topology","accum_level[][1]>4");
  
   TCanvas HMNT_mom;
-  draw.Draw(default,"HMNT_mom[0]",50,0,10000,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
+  draw.Draw(default,"HMNT_mom[0]",50,0,15000,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
   TCanvas HMNT_mom_low;
   draw.Draw(default,"HMNT_mom[0]",50,0,500,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
 }
