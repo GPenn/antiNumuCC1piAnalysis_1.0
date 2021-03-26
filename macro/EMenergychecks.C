f63bd1e39cbe689b7b63333c73d5be48fda694ca
Gabriel Penn <callidus@hotmail.co.uk>
2021-03-26 16:01:21 +0000

    Fix category for correlation plot

--- macro/EMenergychecks.C
+++ macro/EMenergychecks.C
@@ -11,7 +11,7 @@
   draw.Draw(default,"selmu_ecal_bestseg_EMenergy/selmu_mom[0]",50,0,250,"topology","accum_level[][1]>4&&selmu_necals>0");
   
   TCanvas selmu_ecal_bestseg_EMenergy_vs_EbyP;
-  draw.Draw(default,"(selmu_ecal_bestseg_EMenergy/selmu_mom[0]):selmu_ecal_bestseg_EMenergy",50,0,10000,50,0,250,"accum_level[][1]>4&&selmu_necals>0", "colz");
+  draw.Draw(default,"(selmu_ecal_bestseg_EMenergy/selmu_mom[0]):selmu_ecal_bestseg_EMenergy",50,0,10000,50,0,250,"all", "accum_level[][1]>4&&selmu_necals>0", "colz");
   
   TCanvas HMNT_ecal_bestseg_EMenergy;
   draw.Draw(default,"HMNT_ecal_bestseg_EMenergy",50,0,10000,"topology","accum_level[][1]>4&&HMNT_NEcalSegments>0");
