1cafbe98c31bee85b8c12e99c8ce982ff845e5c2
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-02 15:16:56 +0100

    Comment out non-1pos1neg plots, add ECal segment breakdown for 1pos1neg

--- macro/BDT_samples.C
+++ macro/BDT_samples.C
@@ -18,7 +18,35 @@
   TCanvas tpc1pos1neg_bkg;
   draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
-  TCanvas tpc1pos0neg;
+  TCanvas tpc1pos1neg_bothseg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments>0)");
+  TCanvas tpc1pos1neg_bothseg_sig;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments>0)");
+  TCanvas tpc1pos1neg_bothseg_bkg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments>0)");
+  
+  TCanvas tpc1pos1neg_museg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments==0)");
+  TCanvas tpc1pos1neg_museg_sig;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments==0)");
+  TCanvas tpc1pos1neg_museg_bkg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals>0)&&(HMNT_NEcalSegments==0)");
+  
+  TCanvas tpc1pos1neg_piseg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments>0)");
+  TCanvas tpc1pos1neg_piseg_sig;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments>0)");
+  TCanvas tpc1pos1neg_piseg_bkg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments>0)");
+  
+  TCanvas tpc1pos1neg_noseg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments==0)");
+  TCanvas tpc1pos1neg_noseg_sig;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments==0)");
+  TCanvas tpc1pos1neg_noseg_bkg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(selmu_necals==0)&&(HMNT_NEcalSegments==0)");
+  
+  /*TCanvas tpc1pos0neg;
   draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==0)&&(NME==1)");
   TCanvas tpc1pos0neg_sig;
   draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==0)&&(NME==1)");
@@ -37,6 +65,6 @@
   TCanvas tpc2pos0neg_sig;
   draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology==1)&&(ntpcposQualityFV==2)&&(ntpcnegQualityFV==0)&&(NME==1)");
   TCanvas tpc2pos0neg_bkg;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==2)&&(ntpcnegQualityFV==0)&&(NME==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>4)&&(topology!=1)&&(ntpcposQualityFV==2)&&(ntpcnegQualityFV==0)&&(NME==1)");*/
 
 }
