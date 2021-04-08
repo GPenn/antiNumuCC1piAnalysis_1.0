dd2b3391987769f8cb7660b6035a60baf6d31f4d
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-08 16:19:39 +0100

    Add low HMNT mom signal plot

--- macro/momentum_checks.C
+++ macro/momentum_checks.C
@@ -16,6 +16,8 @@
   draw.Draw(default,"HMNT_mom[0]",50,0,15000,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
   TCanvas HMNT_mom_low;
   draw.Draw(default,"HMNT_mom[0]",50,0,500,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
+  TCanvas HMNT_mom_low_sig;
+  draw.Draw(default,"HMNT_mom[0]",50,0,500,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)&&(topology==1)");
   
   TCanvas preselection_eff_selmu_mom;
   draw.DrawRatio(default,"selmu_truemom",50,0,5000,"(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(topology==1)","topology==1");
