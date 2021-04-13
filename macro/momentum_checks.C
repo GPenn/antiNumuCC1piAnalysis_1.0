8cccb96b9171e9faf7af6a72f3255d34149a79e0
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-13 18:34:38 +0100

    Add x-axis titles

--- macro/momentum_checks.C
+++ macro/momentum_checks.C
@@ -5,13 +5,15 @@
 
   draw.DumpPOT(mc);
   
+  draw.SetTitleX("Muon candidate reco momentum");
   TCanvas selmu_mom;
   draw.Draw(default,"selmu_mom[0]",50,0,15000,"topology","accum_level[][1]>4");
   TCanvas selmu_mom_low;
   draw.Draw(default,"selmu_mom[0]",50,0,500,"topology","accum_level[][1]>4");
   TCanvas selmu_mom_low_sig;
   draw.Draw(default,"selmu_mom[0]",50,0,500,"topology","accum_level[][1]>4&&topology==1");
- 
+  
+  draw.SetTitleX("Pion candidate reco momentum");
   TCanvas HMNT_mom;
   draw.Draw(default,"HMNT_mom[0]",50,0,15000,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)");
   TCanvas HMNT_mom_low;
@@ -19,16 +21,19 @@
   TCanvas HMNT_mom_low_sig;
   draw.Draw(default,"HMNT_mom[0]",50,0,500,"topology","(accum_level[][1]>4)&&(ntpcnegQualityFV>0)&&(topology==1)");
   
+  draw.SetTitleX("Muon candidate true momentum");
   TCanvas preselection_eff_selmu_truemom;
   draw.DrawRatio(default,"selmu_truemom",50,0,5000,"(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(topology==1)","topology==1");
   TCanvas preselection_eff_selmu_truemom_low;
   draw.DrawRatio(default,"selmu_truemom",50,0,500,"(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)&&(topology==1)","topology==1");
   
+  draw.SetTitleX("True lepton true momentum");
   TCanvas muon_eff_truelepton_mom;
   draw.DrawRatio(default,"truelepton_mom",50,0,5000,"(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(particle==-13)","particle==-13");
   TCanvas muon_eff_truelepton_mom_low;
   draw.DrawRatio(default,"truelepton_mom",50,0,500,"(accum_level[][1]>4)&&(ntpcposQualityFV==1)&&(particle==-13)","particle==-13");
   
+  draw.SetTitleX("True pion true momentum");
   TCanvas pion_eff_truepi_mom;
   draw.DrawRatio(default,"truepi_mom",50,0,5000,"(accum_level[][1]>4)&&(ntpcnegQualityFV==1)&&(HMNT_truepdg==-211)","HMNT_truepdg==-211");
   TCanvas pion_eff_truepi_mom_low;
