7d15b565c3b1cfa6b9f361f362ac16aa1b8ccb3c
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-19 15:41:26 +0100

    Create standard_cc1pi.C

---
+++ macro/standard_cc1pi.C
@@ -0,0 +1,17 @@
+{
+  DrawingTools draw("/hepstore/gpenn/nd280gitlab/12.31/antiNumuCCMultiPiAnalysis_2.11/output/output_prod6T_allruns.root");
+  DataSample mc("/hepstore/gpenn/nd280gitlab/12.31/antiNumuCCMultiPiAnalysis_2.11/output/output_prod6T_allruns.root");
+  
+  draw.SetTitleX("Event topology");
+  
+  TCanvas effpur;
+  draw.DrawEffPurVSCut(mc,"topology==1");
+  
+  TCanvas events;
+  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>6");
+  TCanvas sig;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>6)&&(topology==1)");
+  TCanvas bkg;
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>6)&&(topology!=1)");
+  
+}
