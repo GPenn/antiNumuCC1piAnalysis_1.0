389ef63abfdffcf0677ca57e68edecb6b44d8796
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-21 19:07:34 +0100

    Increase accum level

--- macro/modified_cc1pi.C
+++ macro/modified_cc1pi.C
@@ -8,11 +8,11 @@
   draw.DrawEffPurVSCut(mc, 1, "topology==1");
   
   TCanvas events;
-  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>9");
+  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>10");
   TCanvas sig;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology==1)");
   TCanvas bkg;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology!=1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology!=1)");
   
   TCanvas true_sig;
   draw.Draw(truth,"topology",10,0,10,"topology","(topology==1)");
