5ffbb610b03fb75671ebd0f03f5ac121ef16ac8a
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-25 16:25:46 +0100

    Comment out redundant plots

--- macro/modified_cc1pi.C
+++ macro/modified_cc1pi.C
@@ -7,19 +7,19 @@
   TCanvas effpur;
   draw.DrawEffPurVSCut(mc, 1, "topology==1");
   
-  TCanvas events_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>7");
+  //TCanvas events_preecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>7");
   TCanvas sig_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   TCanvas bkg_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology!=1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
-  TCanvas events_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>9");
+  //TCanvas events_postecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>9");
   TCanvas sig_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   TCanvas bkg_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology!=1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
   TCanvas true_sig;
   draw.Draw(truth,"topology",10,0,10,"topology","(topology==1)");
