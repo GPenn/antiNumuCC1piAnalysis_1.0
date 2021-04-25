853c3e4598c42823180cf946dfd91b4a96eacfe1
Gabriel Penn <callidus@hotmail.co.uk>
2021-04-25 15:46:24 +0100

    Halve number of momentum bins, comment out redundant histograms, add 1pos1neg requirement to others

--- macro/standard_cc1pi_ecalcuts.C
+++ macro/standard_cc1pi_ecalcuts.C
@@ -7,23 +7,23 @@
   TCanvas effpur;
   draw.DrawEffPurVSCut(mc, 1, "topology==1");
   
-  TCanvas events_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>8");
+  //TCanvas events_preecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>8)");
   TCanvas sig_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>8)&&(topology==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>8)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   TCanvas bkg_preecal);
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>8)&&(topology!=1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>8)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
   TCanvas momentum_preecal;
   draw.SetTitleX("Antimuon candidate true momentum (MeV/c)");
-  draw.Draw(default,"selmu_truemom",50,0,5000,"topology","accum_level[][1]>8");
+  draw.Draw(default,"selmu_truemom",25,0,5000,"topology","accum_level[][1]>8");
   
-  TCanvas events_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>10");
+  //TCanvas events_postecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)");
   TCanvas sig_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology==1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   TCanvas bkg_postecal);
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology!=1)");
+  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>10)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
   TCanvas true_sig;
   draw.Draw(truth,"topology",10,0,10,"topology","(topology==1)");
