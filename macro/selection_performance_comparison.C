40d02ca547799a3d43fcaaf0ba433410dea048b6
Gabriel Penn <callidus@hotmail.co.uk>
2021-05-16 17:46:00 +0100

    Adjust 2d plot axes

--- macro/selection_performance_comparison.C
+++ macro/selection_performance_comparison.C
@@ -17,8 +17,8 @@
   draw_mod.DrawEff(mc_mod,"truelepton_mom",25,0,1000,"accum_level[][1]>9", "topology==1", "same", "", "Modified selection");
   
   TCanvas muon_vs_pionmom_ori;
-  draw_ori.Draw(mc_ori,"truelepton_mom:truepi_mom",60,0,6000,60,0,6000,"all","(accum_level[][1]>8)&&(topology==1)", "colz");
+  draw_ori.Draw(mc_ori,"truelepton_mom:truepi_mom",50,0,2000,50,0,2000,"all","(accum_level[][1]>8)&&(topology==1)", "colz");
   TCanvas muon_vs_pionmom_mod;
-  draw_mod.Draw(mc_mod,"truelepton_mom:truepi_mom",60,0,6000,60,0,6000,"all","(accum_level[][1]>9)&&(topology==1)", "colz");
+  draw_mod.Draw(mc_mod,"truelepton_mom:truepi_mom",50,0,2000,50,0,2000,"all","(accum_level[][1]>9)&&(topology==1)", "colz");
   
 }
