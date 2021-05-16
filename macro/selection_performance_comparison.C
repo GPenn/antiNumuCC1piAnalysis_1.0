290c9942d10d7f664c22a664925c2d2b11b5be91
Gabriel Penn <callidus@hotmail.co.uk>
2021-05-16 18:22:59 +0100

    Add axis labels to 2D plots

--- macro/selection_performance_comparison.C
+++ macro/selection_performance_comparison.C
@@ -17,8 +17,12 @@
   draw_mod.DrawEff(mc_mod,"truelepton_mom",25,0,1000,"accum_level[][1]>9", "topology==1", "same", "", "Modified selection");
   
   TCanvas muon_vs_pionmom_ori;
+  draw_ori.SetTitleY("True mu+ true momentum (MeV/c)");
+  draw_ori.SetTitleX("True pi- true momentum (MeV/c)");
   draw_ori.Draw(mc_ori,"truelepton_mom:truepi_mom",25,0,2000,25,0,2000,"all","(accum_level[][1]>8)&&(topology==1)", "colz");
   TCanvas muon_vs_pionmom_mod;
+  draw_mod.SetTitleY("True mu+ true momentum (MeV/c)");
+  draw_mod.SetTitleX("True pi- true momentum (MeV/c)");
   draw_mod.Draw(mc_mod,"truelepton_mom:truepi_mom",25,0,2000,25,0,2000,"all","(accum_level[][1]>9)&&(topology==1)", "colz");
   
 }
