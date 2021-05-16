5af0f9dde992b5ddf4dcd64151145a50d4c4bcb4
Gabriel Penn <callidus@hotmail.co.uk>
2021-05-16 12:58:43 +0100

    Add momentum plot, comment out everything else

--- macro/modified_cc1pi.C
+++ macro/modified_cc1pi.C
@@ -9,19 +9,23 @@
   
   //TCanvas events_preecal;
   //draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>7");
-  TCanvas sig_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
-  TCanvas bkg_preecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
+  //TCanvas sig_preecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
+  //TCanvas bkg_preecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>7)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
   //TCanvas events_postecal;
   //draw.Draw(default,"topology",10,0,10,"topology","accum_level[][1]>9");
-  TCanvas sig_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
-  TCanvas bkg_postecal;
-  draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
+  //TCanvas sig_postecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology==1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
+  //TCanvas bkg_postecal;
+  //draw.Draw(default,"topology",10,0,10,"topology","(accum_level[][1]>9)&&(topology!=1)&&(ntpcposQualityFV==1)&&(ntpcnegQualityFV==1)");
   
-  TCanvas true_sig;
-  draw.Draw(truth,"topology",10,0,10,"topology","(topology==1)");
+  TCanvas momentum_postecal;
+  draw.SetTitleX("Antimuon candidate true momentum (MeV/c)");
+  draw.Draw(default,"selmu_truemom",25,0,5000,"nutype","accum_level[][1]>9");
+  
+  //TCanvas true_sig;
+  //draw.Draw(truth,"topology",10,0,10,"topology","(topology==1)");
   
 }
