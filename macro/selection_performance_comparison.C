10bb63ddfe9a27874b872dac4b9fee652be2b027
Gabriel Penn <callidus@hotmail.co.uk>
2021-05-16 13:42:18 +0100

    Create selection_performance_comparison.C

---
+++ macro/selection_performance_comparison.C
@@ -0,0 +1,11 @@
+{
+  DrawingTools draw_mod("output/output_p6T_allruns_modified_withoptimised.root");
+  DrawingTools draw_ori("output/output_p6T_allruns_standard_withoptimised.root");
+  
+  TCanvas momentum_sig_comparison;
+  
+  draw_ori.SetTitleX("Antimuon candidate true momentum (MeV/c)");
+  draw_ori.Draw(default,"selmu_truemom",30,0,6000,"","accum_level[][1]>8", "", "", "Original selection");
+  draw_mod.Draw(default,"selmu_truemom",30,0,6000,"","accum_level[][1]>9", "same", "", "Modified selection");
+  
+}
