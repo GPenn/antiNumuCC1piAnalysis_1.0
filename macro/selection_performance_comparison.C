cba3b263ffaad3b43a9f2c8f8c40fc808c62ba41
Gabriel Penn <callidus@hotmail.co.uk>
2021-05-16 17:34:07 +0100

    Change momentum to low-mom range

--- macro/selection_performance_comparison.C
+++ macro/selection_performance_comparison.C
@@ -13,8 +13,8 @@
   TCanvas efficiency_comparison;
   draw_ori.SetTitleX("True lepton true momentum (MeV/c)");
   draw_ori.SetTitleY("$\bar{\nu}_\mu$ CC1pi selection efficiency");
-  draw_ori.DrawEff(truth,"truelepton_mom",30,0,6000,"accum_level[][1]>8", "topology==1", "", "", "Original selection");
-  draw_mod.DrawEff(truth,"truelepton_mom",30,0,6000,"accum_level[][1]>9", "topology==1", "same", "", "Modified selection");
+  draw_ori.DrawEff(truth,"truelepton_mom",25,0,1000,"accum_level[][1]>8", "topology==1", "", "", "Original selection");
+  draw_mod.DrawEff(truth,"truelepton_mom",25,0,1000,"accum_level[][1]>9", "topology==1", "same", "", "Modified selection");
   
   //TCanvas muon_vs_pionmom;
   //draw_ori.Draw(default,"selmu_truemom:",30,0,6000,"all","accum_level[][1]>8");
