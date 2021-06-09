35ab14423e3d0873fe6d96a1027890686d30a19a
Gabriel Penn <callidus@hotmail.co.uk>
2021-06-09 16:53:05 +0100

    Add costheta plot

--- macro/particle_gun/kinematic_checks.C
+++ macro/particle_gun/kinematic_checks.C
@@ -4,10 +4,10 @@
   DataSample mc("output/particle_gun/output_pg_antimu_06091555.root");
   
   draw.SetTitleX("Antimuon reco momentum");
-  TCanvas selmu_mom_accum0;
-  draw.Draw(default,"selmu_mom",20,0,5000,"topology","");
-  
-  draw.SetTitleX("Antimuon reco momentum");
   TCanvas selmu_mom_accum4;
   draw.Draw(default,"selmu_mom",20,0,5000,"topology","accum_level[][0]>4");
+  
+  draw.SetTitleX("Antimuon reco costheta");
+  TCanvas selmu_costheta_accum4;
+  draw.Draw(default,"selmu_costheta",20,-1,1,"topology","accum_level[][0]>4");
 }
