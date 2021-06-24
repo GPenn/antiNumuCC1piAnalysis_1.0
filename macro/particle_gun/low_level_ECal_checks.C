c3f4b8407c0395068a73ab56855c893b63512470
Gabriel Penn <callidus@hotmail.co.uk>
2021-06-24 15:50:17 +0100

    Update input file and axis labels

--- macro/particle_gun/low_level_ECal_checks.C
+++ macro/particle_gun/low_level_ECal_checks.C
@@ -1,8 +1,8 @@
 {
-  DrawingTools draw("output/particle_gun/localglobaltest2.root");
-  DataSample mc("output/particle_gun/localglobaltest2.root");
+  DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
+  DataSample mc("output/particle_gun/particlegun_allpositive.root");
   
-  draw.SetTitleX("selmu_ecal_amr");
+  draw.SetTitleX("ECal axis max ratio");
   TCanvas selmu_ecal_amr;
   draw.Draw(default,"selmu_ecal_amr",20,-10,400,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
@@ -14,15 +14,15 @@
   TCanvas selmu_ecal_asymmetry;
   draw.Draw(default,"selmu_ecal_asymmetry",20,0,1,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
-  draw.SetTitleX("selmu_ecal_circularity");
+  draw.SetTitleX("ECal circularity");
   TCanvas selmu_ecal_circularity;
   draw.Draw(default,"selmu_ecal_circularity",20,0,1,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
-  draw.SetTitleX("selmu_ecal_fbr");
+  draw.SetTitleX("ECal FrontBackRatio");
   TCanvas selmu_ecal_fbr;
   draw.Draw(default,"selmu_ecal_fbr",20,0,50,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
-  draw.SetTitleX("selmu_ecal_maxratio");
+  draw.SetTitleX("ECal charge max ratio");
   TCanvas selmu_ecal_maxratio;
   draw.Draw(default,"selmu_ecal_maxratio",20,-50,400,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
@@ -30,11 +30,11 @@
   TCanvas selmu_ecal_meanpos;
   draw.Draw(default,"selmu_ecal_meanpos",20,0,800,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
  
-  draw.SetTitleX("selmu_ecal_showerangle");
+  draw.SetTitleX("ECal shower opening angle (rad)");
   TCanvas selmu_ecal_showerangle;
   draw.Draw(default,"selmu_ecal_showerangle",20,0,1,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
-  draw.SetTitleX("selmu_ecal_showerwidth");
+  draw.SetTitleX("ECal shower width");
   TCanvas selmu_ecal_showerwidth;
   draw.Draw(default,"selmu_ecal_showerwidth",20,0,1,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
@@ -42,7 +42,7 @@
   TCanvas selmu_ecal_tcr;
   draw.Draw(default,"selmu_ecal_tcr",20,0,3,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
   
-  draw.SetTitleX("selmu_ecal_tmr");
+  draw.SetTitleX("ECal truncated max ratio");
   TCanvas selmu_ecal_tmr;
   draw.Draw(default,"selmu_ecal_tmr",20,0,1,"particle","(accum_level[][0]>4)&&(selmu_necals==1)");
 }
