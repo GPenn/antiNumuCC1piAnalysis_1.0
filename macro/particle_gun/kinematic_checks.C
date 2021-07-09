4d9712eaa5e4945d046562de5ab1fb247c2e9ee8
Gabriel Penn <callidus@hotmail.co.uk>
2021-07-09 14:55:49 +0100

    Switch to new input file, stack by particle

--- macro/particle_gun/kinematic_checks.C
+++ macro/particle_gun/kinematic_checks.C
@@ -1,13 +1,13 @@
 
 {
-  DrawingTools draw("output/particle_gun/output_pg_antimu_06101546.root");
-  DataSample mc("output/particle_gun/output_pg_antimu_06101546.root");
+  DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
+  DataSample mc("output/particle_gun/particlegun_allpositive.root");
   
   draw.SetTitleX("Antimuon reco momentum");
   TCanvas selmu_mom_accum4;
-  draw.Draw(default,"selmu_mom",20,0,7000,"topology","accum_level[][0]>4");
+  draw.Draw(default,"selmu_mom",20,0,7000,"particle","accum_level[][0]>4");
   
   draw.SetTitleX("Antimuon reco costheta");
   TCanvas selmu_costheta_accum4;
-  draw.Draw(default,"selmu_costheta",20,-1,1,"topology","accum_level[][0]>4");
+  draw.Draw(default,"selmu_costheta",20,-1,1,"particle","accum_level[][0]>4");
 }
