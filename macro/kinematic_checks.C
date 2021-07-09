4626a8c7156fc155e1cfd17e0c1f191b3add1e9b
Gabriel Penn <callidus@hotmail.co.uk>
2021-07-09 15:39:07 +0100

    Create kinematic_checks.C

---
+++ macro/kinematic_checks.C
@@ -0,0 +1,12 @@
+{
+  DrawingTools draw("output/output_p6T_allruns_standard_withoptimised.root");
+  DataSample mc("output/output_p6T_allruns_standard_withoptimised.root");
+  
+  draw.SetTitleX("Antimuon reco momentum");
+  TCanvas selmu_mom_accum4;
+  draw.Draw(default,"selmu_mom",20,0,7000,"particle","accum_level[][0]>4");
+  
+  draw.SetTitleX("Antimuon reco costheta");
+  TCanvas selmu_costheta_accum4;
+  draw.Draw(default,"selmu_costheta",20,-1,1,"particle","accum_level[][0]>4");
+}
