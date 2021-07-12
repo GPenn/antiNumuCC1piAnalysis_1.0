cb72206620cbd0d50884675d7341fac145d813f0
Gabriel Penn <callidus@hotmail.co.uk>
2021-07-12 16:04:29 +0100

    Add low mom and true mom plots

--- macro/kinematic_checks.C
+++ macro/kinematic_checks.C
@@ -6,6 +6,18 @@
   TCanvas selmu_mom_accum4;
   draw.Draw(default,"selmu_mom",20,0,7000,"particle","accum_level[][0]>4");
   
+  draw.SetTitleX("Antimuon reco momentum");
+  TCanvas selmu_mom_accum4_lowmom;
+  draw.Draw(default,"selmu_mom",20,0,500,"particle","accum_level[][0]>4");
+  
+  draw.SetTitleX("Antimuon true momentum");
+  TCanvas selmu_truemom_accum4;
+  draw.Draw(default,"selmu_truemom",20,0,7000,"particle","accum_level[][0]>4");
+  
+  draw.SetTitleX("Antimuon true momentum");
+  TCanvas selmu_truemom_accum4_lowmom;
+  draw.Draw(default,"selmu_truemom",20,0,500,"particle","accum_level[][0]>4");
+    
   draw.SetTitleX("Antimuon reco costheta");
   TCanvas selmu_costheta_accum4;
   draw.Draw(default,"selmu_costheta",20,-1,1,"particle","accum_level[][0]>4");
