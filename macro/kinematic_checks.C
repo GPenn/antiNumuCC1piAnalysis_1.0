951ec7ba55fbd67f71461ba72dddcd564871d827
Gabriel Penn <callidus@hotmail.co.uk>
2021-07-13 15:44:49 +0100

    Add saves for plots

--- macro/kinematic_checks.C
+++ macro/kinematic_checks.C
@@ -6,24 +6,29 @@
   TCanvas selmu_mom_accum4;
   draw.Draw(default,"selmu_mom",20,0,7000,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(2000,true,"l");
+  selmu_mom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_evt.png");
   
   draw.SetTitleX("Antimuon reco momentum");
   TCanvas selmu_mom_accum4_lowmom;
   draw.Draw(default,"selmu_mom",20,0,500,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(150,true,"r");
+  selmu_mom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_lowmom_evt.png");
   
   draw.SetTitleX("Antimuon true momentum");
   TCanvas selmu_truemom_accum4;
   draw.Draw(default,"selmu_truemom",20,0,7000,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(2000,true,"l");
+  selmu_truemom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_evt.png");
   
   draw.SetTitleX("Antimuon true momentum");
   TCanvas selmu_truemom_accum4_lowmom;
   draw.Draw(default,"selmu_truemom",20,0,500,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(150,true,"r");
+  selmu_truemom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_lowmom_evt.png");
     
   draw.SetTitleX("Antimuon reco costheta");
   TCanvas selmu_costheta_accum4;
   draw.Draw(default,"selmu_costheta",20,-1,1,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(0.4,true,"r");
+  selmu_costheta_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_costheta_accum4_evt.png");
 }
