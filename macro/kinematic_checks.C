ff0c5494161eced012e15d16c7b3872c7564aaf8
Gabriel Penn <callidus@hotmail.co.uk>
2021-07-22 15:46:14 +0100

    Add units to axis labels

--- macro/kinematic_checks.C
+++ macro/kinematic_checks.C
@@ -2,25 +2,25 @@
   DrawingTools draw("output/output_p6T_allruns_standard_withoptimised.root");
   DataSample mc("output/output_p6T_allruns_standard_withoptimised.root");
   
-  draw.SetTitleX("HMPT reco momentum");
+  draw.SetTitleX("HMPT reco momentum (MeV/c)");
   TCanvas selmu_mom_accum4;
   draw.Draw(default,"selmu_mom",20,0,7000,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(2000,true,"l");
   selmu_mom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_evt.png");
   
-  draw.SetTitleX("HMPT reco momentum");
+  draw.SetTitleX("HMPT reco momentum (MeV/c)");
   TCanvas selmu_mom_accum4_lowmom;
   draw.Draw(default,"selmu_mom",20,0,500,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(150,true,"r");
   selmu_mom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_lowmom_evt.png");
   
-  draw.SetTitleX("HMPT true momentum");
+  draw.SetTitleX("HMPT true momentum (MeV/c)");
   TCanvas selmu_truemom_accum4;
   draw.Draw(default,"selmu_truemom",20,0,7000,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(2000,true,"l");
   selmu_truemom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_evt.png");
   
-  draw.SetTitleX("HMPT true momentum");
+  draw.SetTitleX("HMPT true momentum (MeV/c)");
   TCanvas selmu_truemom_accum4_lowmom;
   draw.Draw(default,"selmu_truemom",20,0,500,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(150,true,"r");
@@ -32,7 +32,7 @@
   draw.DrawCutLineVertical(0.4,true,"r");
   selmu_costheta_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_costheta_accum4_evt.png");
   
-  draw.SetTitleX("HMPT reco angle");
+  draw.SetTitleX("HMPT reco angle (deg)");
   TCanvas selmu_theta_accum4;
   draw.Draw(default,"(TMath::ACos(selmu_costheta))*360/6.283",30,0,90,"particle","accum_level[][0]>4");
   draw.DrawCutLineVertical(65,true,"l");
