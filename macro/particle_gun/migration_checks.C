3f4fd7877f5409adc50b8b9648c42e198930dfec
Gabriel Penn <callidus@hotmail.co.uk>
2021-08-11 16:05:54 +0100

    Add migrated plots

--- macro/particle_gun/migration_checks.C
+++ macro/particle_gun/migration_checks.C
@@ -8,31 +8,54 @@
   
   draw.SetTitleX("HMT reco momentum");
   TCanvas selmu_mom_accum4_originals;
-  draw.Draw(default,"selmu_mom",20,0,7000,"particle","(accum_level[][0]>4)&&(parent==0)");
+  draw.Draw(default,"selmu_mom",30,0,3000,"particle","(accum_level[][0]>4)&&(parent==0)");
   draw.DrawCutLineVertical(2000,true,"l");
   //selmu_mom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_pg.png");
   
-  draw.SetTitleX("HMT reco momentum");
+  TCanvas selmu_mom_accum4_migrated;
+  draw.Draw(default,"selmu_mom",30,0,3000,"particle","(accum_level[][0]>4)&&(parent!=0)");
+  draw.DrawCutLineVertical(2000,true,"l");
+  //selmu_mom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_pg.png");
+
   TCanvas selmu_mom_accum4_lowmom_originals;
   draw.Draw(default,"selmu_mom",20,0,500,"particle","(accum_level[][0]>4)&&(parent==0)");
   draw.DrawCutLineVertical(150,true,"r");
   //selmu_mom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_lowmom_pg.png");
   
+  TCanvas selmu_mom_accum4_lowmom_migrated;
+  draw.Draw(default,"selmu_mom",20,0,500,"particle","(accum_level[][0]>4)&&(parent!=0)");
+  draw.DrawCutLineVertical(150,true,"r");
+  //selmu_mom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_mom_accum4_lowmom_pg.png");
+  
   draw.SetTitleX("HMT true momentum");
   TCanvas selmu_truemom_accum4_originals;
-  draw.Draw(default,"selmu_truemom",20,0,7000,"particle","(accum_level[][0]>4)&&(parent==0)");
+  draw.Draw(default,"selmu_truemom",30,0,3000,"particle","(accum_level[][0]>4)&&(parent==0)");
   draw.DrawCutLineVertical(2000,true,"l");
   //selmu_truemom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_pg.png");
   
-  draw.SetTitleX("HMT true momentum");
+  TCanvas selmu_truemom_accum4_migrated;
+  draw.Draw(default,"selmu_truemom",30,0,3000,"particle","(accum_level[][0]>4)&&(parent!=0)");
+  draw.DrawCutLineVertical(2000,true,"l");
+  //selmu_truemom_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_pg.png");
+  
   TCanvas selmu_truemom_accum4_lowmom_originals;
   draw.Draw(default,"selmu_truemom",20,0,500,"particle","(accum_level[][0]>4)&&(parent==0)");
   draw.DrawCutLineVertical(150,true,"r");
   //selmu_truemom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_lowmom_pg.png");
   
+  TCanvas selmu_truemom_accum4_lowmom_migrated;
+  draw.Draw(default,"selmu_truemom",20,0,500,"particle","(accum_level[][0]>4)&&(parent!=0)");
+  draw.DrawCutLineVertical(150,true,"r");
+  //selmu_truemom_accum4_lowmom->SaveAs("img/particlegun/kinematic_checks_selmu_truemom_accum4_lowmom_pg.png");
+  
   draw.SetTitleX("HMT reco angle (deg)");
   TCanvas selmu_theta_accum4_originals;
-  draw.Draw(default,"(TMath::ACos(selmu_costheta))*360/6.283",30,0,90,"particle","(accum_level[][0]>4)&&(parent==0)");
+  draw.Draw(default,"(TMath::ACos(selmu_costheta))*360/6.283",30,0,180,"particle","(accum_level[][0]>4)&&(parent==0)");
+  draw.DrawCutLineVertical(65,true,"l");
+  //selmu_theta_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_theta_accum4_pg.png");
+  
+  TCanvas selmu_theta_accum4_migrated;
+  draw.Draw(default,"(TMath::ACos(selmu_costheta))*360/6.283",30,0,180,"particle","(accum_level[][0]>4)&&(parent!=0)");
   draw.DrawCutLineVertical(65,true,"l");
   //selmu_theta_accum4->SaveAs("img/particlegun/kinematic_checks_selmu_theta_accum4_pg.png");
 }
