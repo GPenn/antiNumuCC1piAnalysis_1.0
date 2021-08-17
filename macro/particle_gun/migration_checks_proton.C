9ce897f9b7e3af2ba46bee6b22e0b64ae3c4d1f6
Gabriel Penn <callidus@hotmail.co.uk>
2021-08-17 11:57:28 +0100

    Create migration_checks_proton.C

---
+++ macro/particle_gun/migration_checks_proton.C
@@ -0,0 +1,9 @@
+{
+  DrawingTools draw("output/particle_gun/particlegun_proton.root");
+  DataSample mc("output/particle_gun/particlegun_proton.root");
+  
+  TCanvas overall_mom;
+  draw.Draw(default,"selmu_truemom",50,0,5000,"particle","(accum_level[][0]>4)");
+  TCanvas trueproton_mom;
+  draw.Draw(default,"selmu_truemom",50,0,5000,"particle","(accum_level[][0]>4)&&(particle==2212)");
+}
