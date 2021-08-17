75929274d7ceec475621f5869b588943b6a542fe
Gabriel Penn <callidus@hotmail.co.uk>
2021-08-17 11:31:12 +0100

    Create generate_pg_lists.sh

---
+++ fileList/generate_pg_lists.sh
@@ -0,0 +1,4 @@
+ls /bundle/data/T2K/users/gpenn/particle_gun/antimu/output/*anal*.root > particlegun_antimu.txt
+ls /bundle/data/T2K/users/gpenn/particle_gun/proton/output/*anal*.root > particlegun_proton.txt
+ls /bundle/data/T2K/users/gpenn/particle_gun/piplus/output/*anal*.root > particlegun_piplus.txt
+ls /bundle/data/T2K/users/gpenn/particle_gun/positron/output/*anal*.root > particlegun_positron.txt
