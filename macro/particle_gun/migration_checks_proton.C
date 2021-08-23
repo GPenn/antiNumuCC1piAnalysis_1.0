5b0a81abc525af5ff8475b0d89ccdb88bc0a4cf4
Gabriel Penn <callidus@hotmail.co.uk>
2021-08-23 15:31:24 +0100

    Add plot saves

--- macro/particle_gun/migration_checks_proton.C
+++ macro/particle_gun/migration_checks_proton.C
@@ -5,10 +5,12 @@
   draw.SetTitleX("Main track true momentum (MeV/c)");
   TCanvas overall_mom;
   draw.Draw(default,"selmu_truemom",60,0,3000,"particle","(accum_level[][0]>4)");
+  overall_mom->SaveAs("img/particlegun/migration_checks_proton_truemom_accum4_pg.png");
   TCanvas trueproton_mom;
   draw.Draw(default,"selmu_truemom",60,0,3000,"particle","(accum_level[][0]>4)&&(particle==2212)");
   
   draw.SetTitleX("Main track reco momentum (MeV/c)");
   TCanvas overall_recomom;
   draw.Draw(default,"selmu_mom",60,0,3000,"particle","(accum_level[][0]>4)");
+  overall_recomom->SaveAs("img/particlegun/migration_checks_proton_recomom_accum4_pg.png");
 }
