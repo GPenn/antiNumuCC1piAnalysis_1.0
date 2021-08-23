cec65eb769ab6c9c4608535d8b698afcf61fe9f0
Gabriel Penn <callidus@hotmail.co.uk>
2021-08-23 12:07:09 +0100

    Add axis titles, reco mom plot

--- macro/particle_gun/migration_checks_proton.C
+++ macro/particle_gun/migration_checks_proton.C
@@ -2,8 +2,13 @@
   DrawingTools draw("output/particle_gun/particlegun_proton.root");
   DataSample mc("output/particle_gun/particlegun_proton.root");
   
+  draw.SetTitleX("Main track true momentum (MeV/c)");
   TCanvas overall_mom;
   draw.Draw(default,"selmu_truemom",60,0,3000,"particle","(accum_level[][0]>4)");
   TCanvas trueproton_mom;
   draw.Draw(default,"selmu_truemom",60,0,3000,"particle","(accum_level[][0]>4)&&(particle==2212)");
+  
+  draw.SetTitleX("Main track reco momentum (MeV/c)");
+  TCanvas overall_recomom;
+  draw.Draw(default,"selmu_mom",60,0,3000,"particle","(accum_level[][0]>4)");
 }
