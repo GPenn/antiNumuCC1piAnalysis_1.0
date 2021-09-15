1f34aea459490c4a5b3446408837d2b10105c4b2
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-15 16:58:34 +0100

    Create BDT_input_testing.C

---
+++ macro/particle_gun/BDT_input_testing.C
@@ -0,0 +1,8 @@
+{
+  DrawingTools draw("output/particle_gun/particlegun_antimu.root");
+  DataSample mc("output/particle_gun/particlegun_antimu.root");
+  
+  draw.SetTitleX("HMT reco momentum");
+  TCanvas selmu_mom_accum4;
+  draw.Draw(default,"selmu_mom",150,0,1500,"particle","accum_level[][0]>4");
+}
