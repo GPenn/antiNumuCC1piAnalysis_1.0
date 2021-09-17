81199f51576412ad658d9e4699c3d2fdf3d57008
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-17 18:02:22 +0100

    Add momentum weighting

--- tmva/BDT_PID_multiclass.C
+++ tmva/BDT_PID_multiclass.C
@@ -226,6 +226,8 @@ void BDT_PID_multiclass( TString myMethodList = "" )
    //factory->AddSpectator( "spec1 := var1*2",  "Spectator 1", "units", 'F' );
    //factory->AddSpectator( "spec2 := var1*3",  "Spectator 2", "units", 'F' );
    //factory->AddSpectator( "selmu_mom",                            "Muon candidate reco mom",          "", 'F' );
+   
+   factory->SetWeightExpression("selmu_mom_weight");
 
    // Read training and test data
    // (it is also possible to use ASCII format as input -> see TMVA Users Guide)
