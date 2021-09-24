c4db7948f80c0791bf67850772c5be7332dcf101
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-24 18:07:52 +0100

    Add other particle hypotheses

--- macro/particle_gun/BDT_output_testing.C
+++ macro/particle_gun/BDT_output_testing.C
@@ -5,4 +5,16 @@
   draw.SetTitleX("BDT mu-like value");
   TCanvas bdt_mu_accum4;
   draw.Draw(default,"selmu_bdt_pid_mu",20,0,1,"particle","(accum_level[][0]>4)&&(particle==-13)");
+  
+  draw.SetTitleX("BDT pi-like value");
+  TCanvas bdt_pi_accum4;
+  draw.Draw(default,"selmu_bdt_pid_pi",20,0,1,"particle","(accum_level[][0]>4)&&(particle==-13)");
+  
+  draw.SetTitleX("BDT p-like value");
+  TCanvas bdt_p_accum4;
+  draw.Draw(default,"selmu_bdt_pid_p",20,0,1,"particle","(accum_level[][0]>4)&&(particle==-13)");
+  
+  draw.SetTitleX("BDT e-like value");
+  TCanvas bdt_e_accum4;
+  draw.Draw(default,"selmu_bdt_pid_e",20,0,1,"particle","(accum_level[][0]>4)&&(particle==-13)");
 }
