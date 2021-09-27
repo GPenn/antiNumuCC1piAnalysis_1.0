a1446accbae9df3b4524ce35dfc728dba0109d62
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-27 16:01:51 +0100

    Add cat for all_positive

--- fileList/generate_pg_lists.sh
+++ fileList/generate_pg_lists.sh
@@ -2,3 +2,4 @@ ls /bundle/data/T2K/users/gpenn/particle_gun/antimu/output/*anal*.root > particl
 ls /bundle/data/T2K/users/gpenn/particle_gun/proton/output/*anal*.root > particlegun_proton.txt
 ls /bundle/data/T2K/users/gpenn/particle_gun/piplus/output/*anal*.root > particlegun_piplus.txt
 ls /bundle/data/T2K/users/gpenn/particle_gun/positron/output/*anal*.root > particlegun_positron.txt
+cat particlegun_antimu.txt particlegun_proton.txt particlegun_piplus.txt particlegun_positron.txt > particlegun_allpositive.root
