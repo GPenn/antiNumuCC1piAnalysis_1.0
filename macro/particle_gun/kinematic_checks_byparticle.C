7d53cedaad1216dd9d5801b4bd0cd346848fba41
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-29 10:51:51 +0100

    Add category to correlation plot draw

--- macro/particle_gun/kinematic_checks_byparticle.C
+++ macro/particle_gun/kinematic_checks_byparticle.C
@@ -5,5 +5,5 @@
   draw_amu.SetTitleX("Antimu reco momentum");
   draw_amu.SetTitleY("Antimu true momentum");
   TCanvas amu_momcorr_accum4;
-  draw_amu.Draw(default,"selmu_mom:selmu_truemom",20,0,7000,20,0,7000,"(accum_level[][0]>4)&&(particle==-13)", "colz");
+  draw_amu.Draw(default,"selmu_mom:selmu_truemom",20,0,7000,20,0,7000, "all", "(accum_level[][0]>4)&&(particle==-13)", "colz");
 }
