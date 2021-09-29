43acfb0d3be3312378b7e689c0c92ab1790942fe
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-29 10:55:28 +0100

    Fix axis labels, update binning

--- macro/particle_gun/kinematic_checks_byparticle.C
+++ macro/particle_gun/kinematic_checks_byparticle.C
@@ -2,8 +2,8 @@
   DrawingTools draw_amu("output/particle_gun/particlegun_antimu.root");
   DataSample mc_amu("output/particle_gun/particlegun_antimu.root");
   
-  draw_amu.SetTitleX("Antimu reco momentum");
-  draw_amu.SetTitleY("Antimu true momentum");
+  draw_amu.SetTitleX("Antimu true momentum");
+  draw_amu.SetTitleY("Antimu reco momentum");
   TCanvas amu_momcorr_accum4;
-  draw_amu.Draw(default,"selmu_mom:selmu_truemom",60,0,3000,60,0,3000, "all", "(accum_level[][0]>4)&&(particle==-13)", "colz");
+  draw_amu.Draw(default,"selmu_mom:selmu_truemom",50,0,2500,60,0,3000, "all", "(accum_level[][0]>4)&&(particle==-13)", "colz");
 }
