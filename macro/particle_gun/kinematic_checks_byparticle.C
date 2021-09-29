d73f07fa1c23cbb92087503dd9f054d84f73c19d
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-29 10:49:36 +0100

    Create kinematic_checks_byparticle.C

---
+++ macro/particle_gun/kinematic_checks_byparticle.C
@@ -0,0 +1,9 @@
+{
+  DrawingTools draw_amu("output/particle_gun/particlegun_antimu.root");
+  DataSample mc_amu("output/particle_gun/particlegun_antimu.root");
+  
+  draw_amu.SetTitleX("Antimu reco momentum");
+  draw_amu.SetTitleY("Antimu true momentum");
+  TCanvas amu_momcorr_accum4;
+  draw_amu.Draw(default,"selmu_mom:selmu_truemom",20,0,7000,20,0,7000,"(accum_level[][0]>4)&&(particle==-13)", "colz");
+}
