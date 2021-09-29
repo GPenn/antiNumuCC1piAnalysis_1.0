b3d2b7dc3116d76d503cd3ca81ff9af578a2341f
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-29 10:56:40 +0100

    Double bin granularity

--- macro/particle_gun/kinematic_checks_byparticle.C
+++ macro/particle_gun/kinematic_checks_byparticle.C
@@ -5,5 +5,5 @@
   draw_amu.SetTitleX("Antimu true momentum");
   draw_amu.SetTitleY("Antimu reco momentum");
   TCanvas amu_momcorr_accum4;
-  draw_amu.Draw(default,"selmu_mom:selmu_truemom",50,0,2500,60,0,3000, "all", "(accum_level[][0]>4)&&(particle==-13)", "colz");
+  draw_amu.Draw(default,"selmu_mom:selmu_truemom",100,0,2500,120,0,3000, "all", "(accum_level[][0]>4)&&(particle==-13)", "colz");
 }
