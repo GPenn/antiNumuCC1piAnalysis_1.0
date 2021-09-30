bc927e67809ebbbf370c7e9df2fafaa208645f75
Gabriel Penn <callidus@hotmail.co.uk>
2021-09-30 12:16:10 +0100

    Create det_theta_checks.C

---
+++ macro/particle_gun/det_theta_checks.C
@@ -0,0 +1,8 @@
+{
+  DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
+  DataSample mc("output/particle_gun/particlegun_allpositive.root");
+  
+  draw.SetTitleX("selmu_det_theta");
+  TCanvas selmu_det_theta_accum4;
+  draw.Draw(default,"selmu_det_theta",50,0.0,7.0,"particle","accum_level[][0]>4");
+}
