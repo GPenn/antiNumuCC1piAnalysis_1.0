fae7a7d6e52b4e8e01c2004e9efac8f6f1b5f9b8
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-01 14:25:14 +0100

    Add positron e-like plot

--- macro/particle_gun/BDT_output_correlations.C
+++ macro/particle_gun/BDT_output_correlations.C
@@ -49,22 +49,27 @@
   //draw_pi.Draw(default,"selmu_bdt_pid_mu:selmu_truemom", 40,150.0,1500.0, 40,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==211)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
   
   
-  /*
+  
   DrawingTools draw_p("output/particle_gun/particlegun_proton.root");
   DataSample mc_p("output/particle_gun/particlegun_proton.root");
   
-  draw_p.SetTitleX("Proton BDT output mu-like");
-  draw_p.SetTitleY("Proton BDT output pi-like");
-  TCanvas p_momcorr_accum4;
-  draw_p.Draw(default,"selmu_bdt_pid_pi:selmu_bdt_pid_mu",20,0.0,1.0,20,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==2212)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
+  //draw_p.SetTitleX("Proton BDT output mu-like");
+  //draw_p.SetTitleY("Proton BDT output pi-like");
+  //TCanvas p_momcorr_accum4;
+  //draw_p.Draw(default,"selmu_bdt_pid_pi:selmu_bdt_pid_mu",20,0.0,1.0,20,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==2212)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
   //p_momcorr_accum4->SaveAs("img/particlegun/reco_vs_true_mom_proton_pg.png");
   
   DrawingTools draw_pos("output/particle_gun/particlegun_positron.root");
   DataSample mc_pos("output/particle_gun/particlegun_positron.root");
   
-  draw_pos.SetTitleX("Positron BDT output mu-like");
-  draw_pos.SetTitleY("Positron BDT output pi-like");
-  TCanvas pos_momcorr_accum4;
-  draw_pos.Draw(default,"selmu_bdt_pid_pi:selmu_bdt_pid_mu",20,0.0,1.0,20,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==-11)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
-  //pos_momcorr_accum4->SaveAs("img/particlegun/reco_vs_true_mom_positron_pg.png");*/
+  //draw_pos.SetTitleX("Positron BDT output mu-like");
+  //draw_pos.SetTitleY("Positron BDT output pi-like");
+  //TCanvas pos_momcorr_accum4;
+  //draw_pos.Draw(default,"selmu_bdt_pid_pi:selmu_bdt_pid_mu",20,0.0,1.0,20,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==-11)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
+  //pos_momcorr_accum4->SaveAs("img/particlegun/reco_vs_true_mom_positron_pg.png");
+  
+  draw_pi.SetTitleX("Positron reco momentum (MeV)");
+  draw_pi.SetTitleY("Positron BDT output e-like");
+  TCanvas pos_bdte_vs_recomom_accum4;
+  draw_pi.Draw(default,"selmu_bdt_pid_e:selmu_mom", 40,150.0,1500.0, 40,0.0,1.0, "all", "(accum_level[][0]>4)&&(particle==-11)&&(selmu_mom > 150.0)&&(selmu_mom < 1500.0)", "colz");
 }
