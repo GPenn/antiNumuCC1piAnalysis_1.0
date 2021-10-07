1686ef67e79587175db28feb13fd0598344c9987
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-07 16:27:58 +0100

    Comment out outputs sum, set separate MinY for each histo

--- macro/particle_gun/BDT_output_testing.C
+++ macro/particle_gun/BDT_output_testing.C
@@ -7,21 +7,25 @@
   
   draw.SetTitleX("BDT mu-like value");
   TCanvas bdt_mu_accum4;
+  draw.SetMinY(100);
   draw.Draw(default,"selmu_bdt_pid_mu",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT pi-like value");
   TCanvas bdt_pi_accum4;
+  draw.SetMinY(100);
   draw.Draw(default,"selmu_bdt_pid_pi",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT p-like value");
   TCanvas bdt_p_accum4;
+  draw.SetMinY(1000);
   draw.Draw(default,"selmu_bdt_pid_p",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT e-like value");
   TCanvas bdt_e_accum4;
+  draw.SetMinY(12);
   draw.Draw(default,"selmu_bdt_pid_e",20,0,1,"particle","(accum_level[][0]>4)");
   
-  draw.SetTitleX("BDT outputs sum");
-  TCanvas bdt_sum_accum4;
-  draw.Draw(default,"selmu_bdt_pid_mu+selmu_bdt_pid_pi+selmu_bdt_pid_p+selmu_bdt_pid_e",20,0,1,"particle","(accum_level[][0]>4)");
+  //draw.SetTitleX("BDT outputs sum");
+  //TCanvas bdt_sum_accum4;
+  //draw.Draw(default,"selmu_bdt_pid_mu+selmu_bdt_pid_pi+selmu_bdt_pid_p+selmu_bdt_pid_e",20,0,1,"particle","(accum_level[][0]>4)");
 }
