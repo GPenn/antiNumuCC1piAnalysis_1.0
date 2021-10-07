e8480ac3de2a4a20b335420e408d74af137784c8
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-07 16:16:18 +0100

    Try setting y to log scale

--- macro/particle_gun/BDT_output_testing.C
+++ macro/particle_gun/BDT_output_testing.C
@@ -2,6 +2,8 @@
   DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
   DataSample mc("output/particle_gun/particlegun_allpositive.root");
   
+  draw.SetLogY(true);
+  
   draw.SetTitleX("BDT mu-like value");
   TCanvas bdt_mu_accum4;
   draw.Draw(default,"selmu_bdt_pid_mu",20,0,1,"particle","(accum_level[][0]>4)");
