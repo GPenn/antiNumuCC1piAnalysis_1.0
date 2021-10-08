4fed35c95b9f68c38340addcaf68241be81dd64c
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-08 16:46:06 +0100

    Add missing namespace

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -1,6 +1,6 @@
 #include "BDTPIDUtils.hxx"
 
-std::vector<Float_t> GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment)
+std::vector<Float_t> BDTPIDUtils::GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment)
 {
   std::vector<Float_t> output = {-1, -1, -1, -1};
   
