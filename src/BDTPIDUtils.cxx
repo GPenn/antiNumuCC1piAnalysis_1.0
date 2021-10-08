ff2ccc8d79ec7144818b034d8acfb4c1fe695958
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-08 15:57:09 +0100

    Create BDTPIDUtils.cxx

---
+++ src/BDTPIDUtils.cxx
@@ -0,0 +1,8 @@
+#include "BDTPIDUtils.hxx"
+
+std::vector<Float_t> GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment)
+{
+  std::vector<Float_t> output = {-1, -1, -1, -1};
+  
+  return output;
+}
