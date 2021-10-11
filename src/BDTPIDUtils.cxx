d582c55ca73da2013c002f9549da305a787fb733
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-11 16:33:42 +0100

    Change function inputs to pointers

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -34,7 +34,8 @@ BDTPIDmanager::BDTPIDmanager() {
 }
 
 //--------------------------------------------------------------------------------------------------//
-std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment) {
+//std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment) {
+std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(AnaTrackB* track, AnaTECALReconObject* localecalsegment) {
 //--------------------------------------------------------------------------------------------------//
   
   // Set variables to defaults:
@@ -46,25 +47,25 @@ std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const 
   //if (track->Momentum < 200) return output;
   
   // Fill kinematic variables:
-  bdt_mom = track.Momentum;
-  TVector3 DirVec = anaUtils::ArrayToTVector3(track.DirectionStart);
+  bdt_mom = track->Momentum;
+  TVector3 DirVec = anaUtils::ArrayToTVector3(track->DirectionStart);
   bdt_theta = TMath::ACos(DirVec[2]);
   
   // Fill TPC variables:
-  bdt_tpc_like_mu = anaUtils::GetPIDLikelihood( track,0);
-  bdt_tpc_like_e  = anaUtils::GetPIDLikelihood( track,1);
-  bdt_tpc_like_p  = anaUtils::GetPIDLikelihood( track,2);
-  bdt_tpc_like_pi = anaUtils::GetPIDLikelihood( track,3);
+  bdt_tpc_like_mu = anaUtils::GetPIDLikelihood( *track,0);
+  bdt_tpc_like_e  = anaUtils::GetPIDLikelihood( *track,1);
+  bdt_tpc_like_p  = anaUtils::GetPIDLikelihood( *track,2);
+  bdt_tpc_like_pi = anaUtils::GetPIDLikelihood( *track,3);
   
   // Fill FGD variables:
-  AnaFGDParticle* FGD1Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( track, static_cast<SubDetId::SubDetEnum >(0)));
+  AnaFGDParticle* FGD1Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(0)));
   if (FGD1Segment) 
   {
     bdt_fgd1pullmu = FGD1Segment->Pullmu;
     bdt_fgd1pullp  = FGD1Segment->Pullp;
     bdt_fgd1pullpi = FGD1Segment->Pullpi;
   }
-  AnaFGDParticle* FGD2Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( track, static_cast<SubDetId::SubDetEnum >(1)));
+  AnaFGDParticle* FGD2Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(1)));
   if (FGD2Segment) 
   {
     bdt_fgd2pullmu = FGD2Segment->Pullmu;
@@ -77,8 +78,8 @@ std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const 
   Int_t ecalsegments = 0;
   
   for (Int_t subdet = 0; subdet<9; subdet++) {
-      if (!SubDetId::GetDetectorUsed(track.Detector, static_cast<SubDetId::SubDetEnum >(subdet+6))) continue;
-        ECALSegment = static_cast<AnaECALParticle*>(anaUtils::GetSegmentInDet( track, static_cast<SubDetId::SubDetEnum >(subdet+6)));
+      if (!SubDetId::GetDetectorUsed(track->Detector, static_cast<SubDetId::SubDetEnum >(subdet+6))) continue;
+        ECALSegment = static_cast<AnaECALParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(subdet+6)));
       if (!ECALSegment) continue;
       
       ecalsegments++;
@@ -93,10 +94,10 @@ std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const 
   
   if (localecalsegment)
   {
-    bdt_ecal_circularity = localecalsegment.PIDCircularity;
-    bdt_ecal_fbr = localecalsegment.PIDFBR;
-    bdt_ecal_qrms = localecalsegment.EMEnergyFitParaQRMS;
-    bdt_ecal_tmr = localecalsegment.PIDTruncatedMaxRatio;
+    bdt_ecal_circularity = localecalsegment->PIDCircularity;
+    bdt_ecal_fbr = localecalsegment->PIDFBR;
+    bdt_ecal_qrms = localecalsegment->EMEnergyFitParaQRMS;
+    bdt_ecal_tmr = localecalsegment->PIDTruncatedMaxRatio;
   }
   
   // Call BDT:
