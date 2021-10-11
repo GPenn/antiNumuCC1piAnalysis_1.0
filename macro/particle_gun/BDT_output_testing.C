c50ac369c0bdbc828831fcb2c054967ca45e7a91
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-11 17:59:55 +0100

    Change input file, disable log plotting

--- macro/particle_gun/BDT_output_testing.C
+++ macro/particle_gun/BDT_output_testing.C
@@ -1,28 +1,28 @@
 {
-  DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
-  DataSample mc("output/particle_gun/particlegun_allpositive.root");
+  DrawingTools draw("output/particle_gun/bdt_testing_selection_test.root");
+  DataSample mc("output/particle_gun/bdt_testing_selection_test.root");
   
-  draw.SetMinY(1);
-  draw.SetLogY(true);
+  //draw.SetMinY(1);
+  //draw.SetLogY(true);
   
   draw.SetTitleX("BDT mu-like value");
   TCanvas bdt_mu_accum4;
-  draw.SetMinY(100);
+  //draw.SetMinY(100);
   draw.Draw(default,"selmu_bdt_pid_mu",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT pi-like value");
   TCanvas bdt_pi_accum4;
-  draw.SetMinY(100);
+  //draw.SetMinY(100);
   draw.Draw(default,"selmu_bdt_pid_pi",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT p-like value");
   TCanvas bdt_p_accum4;
-  draw.SetMinY(1000);
+  //draw.SetMinY(1000);
   draw.Draw(default,"selmu_bdt_pid_p",20,0,1,"particle","(accum_level[][0]>4)");
   
   draw.SetTitleX("BDT e-like value");
   TCanvas bdt_e_accum4;
-  draw.SetMinY(12);
+  //draw.SetMinY(12);
   draw.Draw(default,"selmu_bdt_pid_e",20,0,1,"particle","(accum_level[][0]>4)");
   
   //draw.SetTitleX("BDT outputs sum");
