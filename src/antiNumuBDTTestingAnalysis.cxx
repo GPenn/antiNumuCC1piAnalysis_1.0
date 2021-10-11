1e7cac97c04ac0904872c4e4f010ffbb9a17561c
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-11 15:03:24 +0100

    Add main track check to BDT output call

--- src/antiNumuBDTTestingAnalysis.cxx
+++ src/antiNumuBDTTestingAnalysis.cxx
@@ -143,12 +143,16 @@ void antiNumuBDTTestingAnalysis::FillMicroTrees(bool addBase){
   
   _antiNumuCC1piAnalysis->FillMicroTrees(addBase); 
   
+  if (mybox().MainTrack  ) 
+  {
+  
     std::vector<Float_t> BDT_PID_results = myBDTPIDmanager->GetBDTPIDVars(*mybox().MainTrack, *mybox().MainTrackLocalECalSegment);
     
     output().FillVar(selmu_bdt_pid_mu, BDT_PID_results[0]);
     output().FillVar(selmu_bdt_pid_pi, BDT_PID_results[1]);
     output().FillVar(selmu_bdt_pid_p, BDT_PID_results[2]);
     output().FillVar(selmu_bdt_pid_e, BDT_PID_results[3]);
+  }
   
   baseTrackerAnalysis::FillEffCounters();
     
