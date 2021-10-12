577b8d400ef50ff249ed4d6395d4ac9aed4baba1
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-12 10:20:48 +0100

    Declare BDTPIDMuLikeCut

--- inc/antiNumuBDTTestingSelection.hxx
+++ inc/antiNumuBDTTestingSelection.hxx
@@ -64,5 +64,12 @@ protected:
 
 inline ToyBoxB* antiNumuBDTTestingSelection::MakeToyBox(){return new ToyBoxAntiCC1Pi();}
 
+class BDTPIDMuLikeCut: public StepBase{
+public:
+  using StepBase::Apply;
+  bool Apply(AnaEventC& event, ToyBoxB& box) const;
+  StepBase* MakeClone(){return new BDTPIDMuLikeCut();}
+};
+
 
 #endif
