11d10e74a3f246bf878337f2c49f02d043dfecd0
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-22 18:00:44 +0100

    Create EbyL_momentum_differences.C

---
+++ macro/particle_gun/EbyL_momentum_differences.C
@@ -0,0 +1,11 @@
+{
+  DrawingTools draw("output/particle_gun/particlegun_antimu.root");
+  DataSample mc("output/particle_gun/particlegun_antimu.root");
+  
+  draw.SetTitleX("ECal E/L");
+  TCanvas selmu_ecal_EbyL_below500;
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]/",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
+  
+  TCanvas selmu_ecal_EbyL_above1000;
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]/",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
+}
