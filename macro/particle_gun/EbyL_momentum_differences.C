0e80ec0a2940d8661335e8c8cee45bbe03a07f7d
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-22 18:02:47 +0100

    Remove extra /

--- macro/particle_gun/EbyL_momentum_differences.C
+++ macro/particle_gun/EbyL_momentum_differences.C
@@ -4,8 +4,8 @@
   
   draw.SetTitleX("ECal E/L");
   TCanvas selmu_ecal_EbyL_below500;
-  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]/",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
   
   TCanvas selmu_ecal_EbyL_above1000;
-  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]/",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
 }
