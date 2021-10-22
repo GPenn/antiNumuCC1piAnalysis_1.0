4c24107133f7f3274bdf56ec053e47085f93c741
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-22 18:04:22 +0100

    Fix second plot

--- macro/particle_gun/EbyL_momentum_differences.C
+++ macro/particle_gun/EbyL_momentum_differences.C
@@ -7,5 +7,5 @@
   draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
   
   TCanvas selmu_ecal_EbyL_above1000;
-  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom<500.0)");
+  draw.Draw(default,"selmu_ecal_EMenergy[0]/selmu_ecal_length[0]",50,0.0,4.0,"particle","(accum_level[][0]>4)&&(selmu_necals==1)&&(selmu_mom>1000.0)");
 }
