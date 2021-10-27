5724641be8b57a247818863ac55410aa6c0fa362
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-27 12:44:12 +0100

    Add nhits plots

--- macro/particle_gun/tpcllr_default_value_checks.C
+++ macro/particle_gun/tpcllr_default_value_checks.C
@@ -10,7 +10,13 @@
   
   draw.SetTitleX("Main track TPC dE/dx");
   TCanvas selmu_dedx_default;
-  draw.Draw(default,"selmu_tpc_dedx",50,0,1000,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu<-0.5)");
+  draw.Draw(default,"selmu_tpc_dedx",50,0,10000,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu<-0.5)");
   TCanvas selmu_dedx_nondefault;
   draw.Draw(default,"selmu_tpc_dedx",50,0,1000,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu>-0.5)");
+  
+  draw.SetTitleX("Main track TPC nhits");
+  TCanvas selmu_nhits_default;
+  draw.Draw(default,"selmu_tpc_nhits",50,0,100,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu<-0.5)");
+  TCanvas selmu_nhits_nondefault;
+  draw.Draw(default,"selmu_tpc_nhits",50,0,100,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu>-0.5)");
 }
