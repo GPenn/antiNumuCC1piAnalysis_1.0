2d43cbe8b713d5e34a1733af66bba324a5f435de
Gabriel Penn <callidus@hotmail.co.uk>
2021-10-27 12:08:56 +0100

    Create tpcllr_default_value_checks.C

---
+++ macro/particle_gun/tpcllr_default_value_checks.C
@@ -0,0 +1,12 @@
+{
+  DrawingTools draw("output/particle_gun/particlegun_allpositive.root");
+  DataSample mc("output/particle_gun/particlegun_allpositive.root");
+  
+  draw.SetTitleX("Main track reco momentum");
+  TCanvas selmu_mom_default;
+  draw.Draw(default,"selmu_mom[0]",50,0,3000,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu<-0.5)");
+  
+  draw.SetTitleX("Main track reco momentum");
+  TCanvas selmu_mom_nondefault;
+  draw.Draw(default,"selmu_mom",50,0,3000,"particle","(accum_level[][0]>4)&&(selmu_tpc_like_mu>-0.5)");
+}
