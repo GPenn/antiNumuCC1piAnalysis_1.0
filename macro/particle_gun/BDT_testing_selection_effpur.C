b63e8d8cb28eb066bac68353e6435cff024917b2
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-03 18:47:13 +0000

    Change to particle_pg

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -2,6 +2,6 @@
   DrawingTools draw("output/particle_gun/bdt_testing_selection_test.root");
   DataSample mc("output/particle_gun/bdt_testing_selection_test.root");
   
-  draw.DrawEffPurVSCut(mc,"particle==-13");
+  draw.DrawEffPurVSCut(mc,"particle_pg==-13");
 
 }
