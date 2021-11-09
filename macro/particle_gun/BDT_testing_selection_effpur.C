17294417ee5bb0d738006b816924650783f99632
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-09 11:25:23 +0000

    Switch to allpositive input file

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -1,6 +1,6 @@
 {
-  DrawingTools draw("output/particle_gun/particlegun_antimu_bdttest.root");
-  DataSample mc("output/particle_gun/particlegun_antimu_bdttest.root");
+  DrawingTools draw("output/particle_gun/particlegun_allpositive_bdttest.root");
+  DataSample mc("output/particle_gun/particlegun_allpositive_bdttest.root");
   
   draw.DrawEffPurVSCut(mc,"particle_pg==-13");
 
