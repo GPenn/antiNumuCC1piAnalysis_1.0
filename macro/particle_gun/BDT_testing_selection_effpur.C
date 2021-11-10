1d9a1a335f64bb045244788f4b16e951d9c97311
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-10 17:30:17 +0000

    Add axis labels, narrower plotting range

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -2,32 +2,36 @@
   DrawingTools draw("output/particle_gun/particlegun_allpositive_bdttest.root");
   DataSample mc("output/particle_gun/particlegun_allpositive_bdttest.root");
   
-  TCanvas effpur_mu;
+ /* TCanvas effpur_mu;
   draw.DrawEffPurVSCut(mc, 0, "particle_pg==-13");
-  
-  TCanvas pur_vs_recomom_mu;
-  draw.SetTitleX("Reco momentum (antimu selection)");
-  draw.DrawEff(default, "selmu_mom",50,0,5000,"particle_pg==-13","accum_level[][0]>6");
   
   TCanvas effpur_pi;
   draw.DrawEffPurVSCut(mc, 1, "particle_pg==211");
   
-  TCanvas pur_vs_recomom_pi;
-  draw.SetTitleX("Reco momentum (piplus selection)");
-  draw.DrawEff(default, "selmu_mom",50,0,5000,"particle_pg==211","accum_level[][1]>6");
-  
   TCanvas effpur_p;
   draw.DrawEffPurVSCut(mc, 2, "particle_pg==2212");
   
-  TCanvas pur_vs_recomom_p;
-  draw.SetTitleX("Reco momentum (proton selection)");
-  draw.DrawEff(default, "selmu_mom",50,0,5000,"particle_pg==2212","accum_level[][2]>6");
-  
   TCanvas effpur_e;
-  draw.DrawEffPurVSCut(mc, 3, "particle_pg==-11");
+  draw.DrawEffPurVSCut(mc, 3, "particle_pg==-11");*/
+  
+  TCanvas pur_vs_recomom_mu;
+  draw.SetTitleX("Reco momentum");
+  draw.SetTitleY("Mu+ selection purity");
+  draw.DrawEff(default, "selmu_mom",50,150,1500,"particle_pg==-13","accum_level[][0]>6");
+  
+  TCanvas pur_vs_recomom_pi;
+  draw.SetTitleX("Reco momentum");
+  draw.SetTitleY("Pi+ selection purity");
+  draw.DrawEff(default, "selmu_mom",50,150,1500,"particle_pg==211","accum_level[][1]>6");
+  
+  TCanvas pur_vs_recomom_p;
+  draw.SetTitleX("Reco momentum");
+  draw.SetTitleY("Proton selection purity");
+  draw.DrawEff(default, "selmu_mom",50,150,1500,"particle_pg==2212","accum_level[][2]>6");
   
   TCanvas pur_vs_recomom_e;
-  draw.SetTitleX("Reco momentum (electron selection)");
-  draw.DrawEff(default, "selmu_mom",50,0,5000,"particle_pg==-11","accum_level[][3]>6");
+  draw.SetTitleX("Reco momentum");
+  draw.SetTitleY("Positron selection purity");
+  draw.DrawEff(default, "selmu_mom",50,150,1500,"particle_pg==-11","accum_level[][3]>6");
 
 }
