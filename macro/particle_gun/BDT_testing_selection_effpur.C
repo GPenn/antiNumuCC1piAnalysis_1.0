9c445062f273f743ceb6885a628a6085e7760720
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-10 16:21:52 +0000

    Fix branch arguments

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -2,12 +2,12 @@
   DrawingTools draw("output/particle_gun/particlegun_allpositive_bdttest.root");
   DataSample mc("output/particle_gun/particlegun_allpositive_bdttest.root");
   
-  draw.DrawEffPurVSCut(mc,"particle_pg==-13", 0);
+  draw.DrawEffPurVSCut(mc, 0, "particle_pg==-13");
   
-  draw.DrawEffPurVSCut(mc,"particle_pg==211", 1);
+  draw.DrawEffPurVSCut(mc, 1, "particle_pg==211");
   
-  draw.DrawEffPurVSCut(mc,"particle_pg==2212", 2);
+  draw.DrawEffPurVSCut(mc, 2, "particle_pg==2212");
   
-  draw.DrawEffPurVSCut(mc,"particle_pg==-11", 3);
+  draw.DrawEffPurVSCut(mc, 3, "particle_pg==-11");
 
 }
