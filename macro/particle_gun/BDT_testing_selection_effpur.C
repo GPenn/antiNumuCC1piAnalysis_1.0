d1536e884356ad664abb2e9aec709ff1d9c4ff43
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-10 16:19:46 +0000

    Add other branches

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -2,6 +2,12 @@
   DrawingTools draw("output/particle_gun/particlegun_allpositive_bdttest.root");
   DataSample mc("output/particle_gun/particlegun_allpositive_bdttest.root");
   
-  draw.DrawEffPurVSCut(mc,"particle_pg==-13");
+  draw.DrawEffPurVSCut(mc,"particle_pg==-13", 0);
+  
+  draw.DrawEffPurVSCut(mc,"particle_pg==211", 1);
+  
+  draw.DrawEffPurVSCut(mc,"particle_pg==2212", 2);
+  
+  draw.DrawEffPurVSCut(mc,"particle_pg==-11", 3);
 
 }
