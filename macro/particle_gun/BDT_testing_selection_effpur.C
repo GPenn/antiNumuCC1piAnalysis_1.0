0e076f37b97814f665df01733edfbdad75ad2177
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-10 16:25:26 +0000

    Define canvases

--- macro/particle_gun/BDT_testing_selection_effpur.C
+++ macro/particle_gun/BDT_testing_selection_effpur.C
@@ -2,12 +2,16 @@
   DrawingTools draw("output/particle_gun/particlegun_allpositive_bdttest.root");
   DataSample mc("output/particle_gun/particlegun_allpositive_bdttest.root");
   
+  TCanvas effpur_mu;
   draw.DrawEffPurVSCut(mc, 0, "particle_pg==-13");
   
+  TCanvas effpur_pi;
   draw.DrawEffPurVSCut(mc, 1, "particle_pg==211");
   
+  TCanvas effpur_p;
   draw.DrawEffPurVSCut(mc, 2, "particle_pg==2212");
   
+  TCanvas effpur_e;
   draw.DrawEffPurVSCut(mc, 3, "particle_pg==-11");
 
 }
