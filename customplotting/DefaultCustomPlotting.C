39b97ad78deedcdaf4600448d39b1e854bae2f13
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-15 18:19:01 +0000

    Add TGraph example

--- customplotting/DefaultCustomPlotting.C
+++ customplotting/DefaultCustomPlotting.C
@@ -154,6 +154,17 @@ void DefaultCustomPlotting::Loop()
      
    }
    
+   TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",200,10,500,300);
+   Double_t x[100], y[100];
+   Int_t n = 20;
+   for (Int_t i=0;i<n;i++) {
+      x[i] = i*0.1;
+      y[i] = 10*sin(x[i]+0.2);
+   }
+   TGraph* gr = new TGraph(n,x,y);
+   gr->Draw("AC*");
+   c1->Write();
+   
    // ============= Test existing cuts =============
    
    std::cout << std::endl << std::endl;
