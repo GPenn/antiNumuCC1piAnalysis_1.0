72de6ad3a2249c05d65f5447756037d8c627f975
Gabriel Penn <callidus@hotmail.co.uk>
2021-11-15 13:04:25 +0000

    Add code to count particles and keep track of timing

--- customplotting/DefaultCustomPlotting.C
+++ customplotting/DefaultCustomPlotting.C
@@ -3,6 +3,7 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <ctime> 
 
 void DefaultCustomPlotting::Loop()
 {
@@ -31,13 +32,57 @@ void DefaultCustomPlotting::Loop()
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
 
-   Long64_t nentries = fChain->GetEntriesFast();
+   Long64_t nentries = fChain->GetEntries();
+   
+   std::cout << "Total entries to process: " << nentries << std::endl << std::endl;
+   
+   std::time_t time_start = std::time(0);
+   
+   Int_t nAntimu = 0;
+   Int_t nPiplus = 0;
+   Int_t nProton = 0;
+   Int_t nPositron = 0;
 
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
+      fChain->GetEntry(jentry);
+
+      // Cut on accum_level etc
+      if (accum_level[0][0] <= 4) continue; // Set accum_level
+      
+      if (accum_level[0][0] > 6){
+         
+         if (particle_pg == -13) nAntimu++;
+         if (particle_pg == 211) nPiplus++;
+         if (particle_pg == 2212) nProton++;
+         if (particle_pg == -11) nPositron++;
+      }
+      
+      // Code to keep track of completion percentage and estimate time remaining:
+         
+      Double_t completion = (Double_t)jentry/(Double_t)nentries;
+         
+      std::time_t time_now = std::time(0);
+      std::time_t time_sofar = time_now - time_start;
+         
+      Long64_t time_total_guess = time_sofar / completion;
+      Long64_t time_left_guess = time_total_guess - time_sofar;
+            
+      if (time_sofar >= 2) {
+         std::cout << "Converted " << jentry << " of " << nentries << " entries (" << (Int_t)(completion*100) << "\%). " 
+                   << "Time elapsed: " << time_sofar << "s. "
+                   << "Estimated time remaining: " << time_left_guess << "s. \r";
+      }
    }
+   
+   std::cout << "True antimu in antimu selection: " << nAntimu << std::endl;
+   std::cout << "True piplus in antimu selection: " << nPiplus << std::endl;
+   std::cout << "True protons in antimu selection: " << nProton << std::endl;
+   std::cout << "True positrons in antimu selection: " << nPositron << std::endl;
+   std::cout << "Total signal: " << nAntimu << std::endl;
+   Int_t antimuBkg = nPiplus + nProton + nPositron;
+   std::cout << "Total background: " << antimuBkg << std::endl;
+   Float_t significance = nAntimu/sqrt(nAntimu+antimuBkg);
+   std::cout << "Significance: " << significance << std::endl;
+   
 }
