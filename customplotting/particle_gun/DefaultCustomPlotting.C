487aea4c3df17f4c7855de32443d061cb9c3e5c2
Gabriel Penn <callidus@hotmail.co.uk>
2021-12-07 17:37:44 +0000

    Same thing again

--- customplotting/particle_gun/DefaultCustomPlotting.C
+++ customplotting/particle_gun/DefaultCustomPlotting.C
@@ -68,20 +68,20 @@ void DefaultCustomPlotting::Loop()
    TH1F *qrms_proton = new TH1F("qrms_proton", "Proton", 30, 0.0, 3.0);
    TH1F *qrms_positron = new TH1F("qrms_positron", "Positron", 30, 0.0, 3.0);
    
-   TH1F *mipem_antimu = new TH1F("mipem_antimu", "Antimuon;ECal MipEm variable (dimensionless);Entries", 50, -50.0, 50.0);
-   TH1F *mipem_piplus = new TH1F("mipem_piplus", "Pi+", 50, -50.0, 50.0);
-   TH1F *mipem_proton = new TH1F("mipem_proton", "Proton", 50, -50.0, 50.0);
-   TH1F *mipem_positron = new TH1F("mipem_positron", "Positron", 50, -50.0, 50.0);
+   TH1F *mipem_antimu = new TH1F("mipem_antimu", "Antimuon;ECal MipEm variable (dimensionless);Entries", 60, -50.0, 70.0);
+   TH1F *mipem_piplus = new TH1F("mipem_piplus", "Pi+", 60, -50.0, 70.0);
+   TH1F *mipem_proton = new TH1F("mipem_proton", "Proton", 60, -50.0, 70.0);
+   TH1F *mipem_positron = new TH1F("mipem_positron", "Positron", 60, -50.0, 70.0);
    
-   TH1F *emhip_antimu = new TH1F("emhip_antimu", "Antimuon;ECal EmHip variable (dimensionless);Entries", 50, -50.0, 50.0);
-   TH1F *emhip_piplus = new TH1F("emhip_piplus", "Pi+", 50, -50.0, 50.0);
-   TH1F *emhip_proton = new TH1F("emhip_proton", "Proton", 50, -50.0, 50.0);
-   TH1F *emhip_positron = new TH1F("emhip_positron", "Positron", 50, -50.0, 50.0);
+   TH1F *emhip_antimu = new TH1F("emhip_antimu", "Antimuon;ECal EmHip variable (dimensionless);Entries", 40, -40.0, 40.0);
+   TH1F *emhip_piplus = new TH1F("emhip_piplus", "Pi+", 40, -40.0, 40.0);
+   TH1F *emhip_proton = new TH1F("emhip_proton", "Proton", 40, -40.0, 40.0);
+   TH1F *emhip_positron = new TH1F("emhip_positron", "Positron", 40, -40.0, 40.0);
    
-   TH1F *mippion_antimu = new TH1F("mippion_antimu", "Antimuon;ECal MipPion variable (dimensionless);Entries", 50, -50.0, 50.0);
-   TH1F *mippion_piplus = new TH1F("mippion_piplus", "Pi+", 50, -50.0, 50.0);
-   TH1F *mippion_proton = new TH1F("mippion_proton", "Proton", 50, -50.0, 50.0);
-   TH1F *mippion_positron = new TH1F("mippion_positron", "Positron", 50, -50.0, 50.0);
+   TH1F *mippion_antimu = new TH1F("mippion_antimu", "Antimuon;ECal MipPion variable (dimensionless);Entries", 50, -30.0, 70.0);
+   TH1F *mippion_piplus = new TH1F("mippion_piplus", "Pi+", 50, -30.0, 70.0);
+   TH1F *mippion_proton = new TH1F("mippion_proton", "Proton", 50, -30.0, 70.0);
+   TH1F *mippion_positron = new TH1F("mippion_positron", "Positron", 50, -30.0, 70.0);
    
    TH1F *tpc2dedx_antimu = new TH1F("tpc2dedx_antimu", "Antimuon;TPC2 dE/dx truncated mean ();Entries/", 50, 0.0, 2000.0);
    TH1F *tpc2dedx_piplus = new TH1F("tpc2dedx_piplus", "Pi+", 50, 0.0, 2000.0);
@@ -620,7 +620,7 @@ void DefaultCustomPlotting::Loop()
    
    TCanvas* canvas_tpclikemu = new TCanvas("canvas_tpclikemu","",200,10,1000,600);
    
-   tpclikemu_antimu->GetYaxis()->SetRangeUser(0.0, 45000.0);
+   tpclikemu_antimu->GetYaxis()->SetRangeUser(0.0, 80000.0);
   
    SetHistParticleStyle(tpclikemu_antimu, "antimu");
    SetHistParticleStyle(tpclikemu_piplus, "piplus");
@@ -638,7 +638,7 @@ void DefaultCustomPlotting::Loop()
    
    TCanvas* canvas_tpclikepi = new TCanvas("canvas_tpclikepi","",200,10,1000,600);
    
-   tpclikepi_antimu->GetYaxis()->SetRangeUser(0.0, 45000.0);
+   tpclikepi_antimu->GetYaxis()->SetRangeUser(0.0, 80000.0);
   
    SetHistParticleStyle(tpclikepi_antimu, "antimu");
    SetHistParticleStyle(tpclikepi_piplus, "piplus");
@@ -728,7 +728,7 @@ void DefaultCustomPlotting::Loop()
    
    TCanvas* canvas_nsmrds = new TCanvas("canvas_nsmrds","",200,10,1000,600);
    
-   nsmrds_antimu->GetYaxis()->SetRangeUser(0.0, 100000.0);
+   nsmrds_antimu->GetYaxis()->SetRangeUser(0.0, 80000.0);
   
    SetHistParticleStyle(nsmrds_antimu, "antimu");
    SetHistParticleStyle(nsmrds_piplus, "piplus");
