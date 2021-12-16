033a9b998552b4f8cf822096c39885d2c3b669b0
Gabriel Penn <callidus@hotmail.co.uk>
2021-12-16 13:55:03 +0000

    Add negative version

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -39,19 +39,139 @@ BDTPIDmanager::BDTPIDmanager() {
   
   tmvareader->AddVariable( "nsmrds := selmu_nsmrds",                          &bdt_nsmrds);
   
-  // Book the BDT
+  // Book the BDTs
   tmvareader->BookMVA( "BDTG_tree3", "parameters/weights/BDT_PID_multiclass_BDTG_tree3.weights.xml" );
   tmvareader->BookMVA( "BDTG_tree3_noweights", "parameters/weights/BDT_PID_multiclass_BDTG_tree3_noweights.weights.xml" );
   
+  tmvareader->BookMVA( "BDTG_neg_tree3", "parameters/weights/BDT_PID_multiclass_negative_BDTG_neg_tree3.weights.xml" );
 }
 
 //--------------------------------------------------------------------------------------------------//
 //std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(const AnaTrackB& track, const AnaTECALReconObject& localecalsegment) {
-std::vector<Float_t> BDTPIDmanager::GetBDTPIDVars(AnaTrackB* track, AnaTECALReconObject* localecalsegment, TString BDTname) {
+std::vector<Float_t> BDTPIDmanager::GetBDTPIDVarsPos(AnaTrackB* track, AnaTECALReconObject* localecalsegment, TString BDTname) {
 //--------------------------------------------------------------------------------------------------//
   
   // Set variables to defaults:
   std::vector<Float_t> output = {-1, -1, -1, -1};
+  
+  bdt_mom = -1.0;
+  bdt_theta = -1.0;
+  bdt_ecal_EMenergy = -100.0;
+  
+  bdt_fgd1pullmu = -30.0;
+  bdt_fgd1pullpi = -30.0;
+  bdt_fgd1pullp = -30.0;
+  bdt_fgd1_EbyL = -1.0;
+  
+  bdt_fgd2pullmu = -30.0;
+  bdt_fgd2pullpi = -30.0;
+  bdt_fgd2pullp = -30.0;
+  bdt_fgd2_EbyL = -1.0;
+  
+  bdt_tpc_like_mu = -0.5;
+  bdt_tpc_like_e = -0.5;
+  bdt_tpc_like_p = -0.5;
+  bdt_tpc_like_pi = -0.5;
+  bdt_tpc2_dedx = -100.0;
+  bdt_tpc3_dedx = -100.0;
+  
+  bdt_ecal_EbyP = -1.0;
+  bdt_ecal_EbyL = -1.0;
+  bdt_ecal_circularity = -0.5;
+  bdt_ecal_fbr = -5.0;
+  bdt_ecal_tmr = -0.2;
+  bdt_ecal_qrms = -0.1;
+  bdt_ecal_mipem = -100.0;
+  bdt_ecal_emhip = -100.0;
+  bdt_ecal_mippion = -100.0;
+  
+  bdt_nsmrds = 0;
+  
+  // Sanity check:
+  if (!track) return output;
+  //if (track->Momentum < 200) return output;
+  
+  // Fill kinematic variables:
+  //std::cout << "DEBUG: Filling BDT kinematic variables..." << std::endl;
+  bdt_mom = track->Momentum;
+  TVector3 DirVec = anaUtils::ArrayToTVector3(track->DirectionStart);
+  bdt_theta = TMath::ACos(DirVec[2]);
+  
+  // Fill TPC variables:
+  //std::cout << "DEBUG: Filling BDT TPC variables..." << std::endl;
+  bdt_tpc_like_mu = anaUtils::GetPIDLikelihood( *track,0);
+  bdt_tpc_like_e  = anaUtils::GetPIDLikelihood( *track,1);
+  bdt_tpc_like_p  = anaUtils::GetPIDLikelihood( *track,2);
+  bdt_tpc_like_pi = anaUtils::GetPIDLikelihood( *track,3);
+  //bdt_tpc2_dedx = 
+  //bdt_tpc3_dedx = 
+  
+  // Fill FGD variables:
+  //std::cout << "DEBUG: Filling BDT FGD1 variables..." << std::endl;
+  AnaFGDParticle* FGD1Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(0)));
+  if (FGD1Segment) 
+  {
+    bdt_fgd1pullmu = FGD1Segment->Pullmu;
+    bdt_fgd1pullp  = FGD1Segment->Pullp;
+    bdt_fgd1pullpi = FGD1Segment->Pullpi;
+    bdt_fgd1_EbyL = FGD1Segment->E / FGD1Segment->Length;
+  }
+  //std::cout << "DEBUG: Filling BDT FGD2 variables..." << std::endl;
+  AnaFGDParticle* FGD2Segment = static_cast<AnaFGDParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(1)));
+  if (FGD2Segment) 
+  {
+    bdt_fgd2pullmu = FGD2Segment->Pullmu;
+    bdt_fgd2pullp  = FGD2Segment->Pullp;
+    bdt_fgd2pullpi = FGD2Segment->Pullpi;
+    bdt_fgd2_EbyL = FGD2Segment->E / FGD2Segment->Length;
+  }
+  
+  // Fill ECal variables:
+  //std::cout << "DEBUG: Filling BDT ECal variables..." << std::endl;
+  AnaECALParticle* ECALSegment = NULL;
+  Int_t ecalsegments = 0;
+  
+  for (Int_t subdet = 0; subdet<9; subdet++) {
+      if (!SubDetId::GetDetectorUsed(track->Detector, static_cast<SubDetId::SubDetEnum >(subdet+6))) continue;
+        ECALSegment = static_cast<AnaECALParticle*>(anaUtils::GetSegmentInDet( *track, static_cast<SubDetId::SubDetEnum >(subdet+6)));
+      if (!ECALSegment) continue;
+      
+      ecalsegments++;
+  }
+  
+  if (ECALSegment && (ecalsegments==1))
+  {
+    bdt_ecal_EMenergy = ECALSegment->EMEnergy;
+    bdt_ecal_EbyL = (ECALSegment->EMEnergy)/(ECALSegment->Length);
+    bdt_ecal_EbyP = bdt_ecal_EMenergy/bdt_mom;
+    bdt_ecal_mipem = ECALSegment->PIDMipEm;
+    bdt_ecal_emhip = ECALSegment->PIDEmHip;
+    bdt_ecal_mippion = ECALSegment->PIDMipPion;
+  }
+  //std::cout << "DEBUG: Filling BDT local ECal variables..." << std::endl;
+  if (localecalsegment)
+  {
+    bdt_ecal_circularity = localecalsegment->PIDCircularity;
+    bdt_ecal_fbr = localecalsegment->PIDFBR;
+    bdt_ecal_qrms = localecalsegment->EMEnergyFitParaQRMS;
+    bdt_ecal_tmr = localecalsegment->PIDTruncatedMaxRatio;
+  }
+  
+  bdt_nsmrds = track->nSMRDSegments;
+  
+  // Call BDT:
+  //std::cout << "DEBUG: Evaluating BDT output..." << std::endl;
+  output = tmvareader->EvaluateMulticlass( BDTname );
+  
+  return output;
+}
+
+//--------------------------------------------------------------------------------------------------//
+std::vector<Float_t> BDTPIDmanager::GetBDTPIDVarsNeg(AnaTrackB* track, AnaTECALReconObject* localecalsegment, TString BDTname) {
+//--------------------------------------------------------------------------------------------------//
+  
+  // Set variables to defaults:
+  std::vector<Float_t> output = {-1, -1, -1};
   
   bdt_mom = -1.0;
   bdt_theta = -1.0;
