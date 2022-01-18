dfb03eb6bfc0210db62a508d3000308af985094f
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-18 15:18:26 +0000

    Create submit script for batch particle gun testing job

---
+++ scripts/batch/particlegun/submit_particlegun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -p compute
+#SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/run5-%j.out
+#SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/run5-%j.err
+#SBATCH -J pg_test
+#SBATCH -t 24:00:00
+
+#run the application:
+source ~/highlandsetup.sh
+RunAntiNumuCC1piAnalysis.exe /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/fileList/particle_gun/particlegun_allpositive_testing.txt  -o 
+output/particlegun_allpositive_testing.root
