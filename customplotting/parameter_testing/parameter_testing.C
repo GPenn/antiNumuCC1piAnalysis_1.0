738080bfad153ce46b6b4cee7d89bf4a4a95dca1
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-19 12:51:56 +0000

    Add cc1pi pi points

--- customplotting/parameter_testing/parameter_testing.C
+++ customplotting/parameter_testing/parameter_testing.C
@@ -8,7 +8,14 @@
     treedepth_cc1pi_mu->SetPoint(2, 4.0, 34.0363);
     treedepth_cc1pi_mu->SetPoint(3, 5.0, 34.2227);
     
-    TCanvas* canvas_treedepth = new TCanvas("treedepth","",200,10,1000,600);
+    TGraph* treedepth_cc1pi_pi = new TGraph();
+    treedepth_cc1pi_pi->SetTitle(" ;Maximum tree depth;Optimal selection significance;");
+    treedepth_cc1pi_pi->SetPoint(0, 2.0, 35.4578);
+    treedepth_cc1pi_pi->SetPoint(1, 3.0, 37.6588);
+    treedepth_cc1pi_pi->SetPoint(2, 4.0, 37.9999);
+    treedepth_cc1pi_pi->SetPoint(3, 5.0, 38.4385);
     
+    TCanvas* canvas_treedepth = new TCanvas("treedepth","",200,10,1000,600);
     treedepth_cc1pi_mu->Draw("AL*");
+    treedepth_cc1pi_pi->Draw("L* same");
 }
