67d3fea880ee721f832af4af2097355f1f15c42b
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-20 11:54:19 +0000

    Add TGraph titles, set fill color to white

--- customplotting/parameter_testing/parameter_testing.C
+++ customplotting/parameter_testing/parameter_testing.C
@@ -2,14 +2,14 @@
     // =============== Tree depth ===============
     
     TGraph* treedepth_cc1pi_mu = new TGraph();
-    treedepth_cc1pi_mu->SetTitle(" ;Maximum tree depth;Optimal selection significance;");
+    treedepth_cc1pi_mu->SetTitle("Mu-like (CC1pi);Maximum tree depth;Optimal selection significance;");
     treedepth_cc1pi_mu->SetPoint(0, 2.0, 33.4749);
     treedepth_cc1pi_mu->SetPoint(1, 3.0, 34.1666);
     treedepth_cc1pi_mu->SetPoint(2, 4.0, 34.0363);
     treedepth_cc1pi_mu->SetPoint(3, 5.0, 34.2227);
     
     TGraph* treedepth_cc1pi_pi = new TGraph();
-    treedepth_cc1pi_pi->SetTitle(" ;Maximum tree depth;Optimal selection significance;");
+    treedepth_cc1pi_pi->SetTitle("Pi-like (CC1pi) ;Maximum tree depth;Optimal selection significance;");
     treedepth_cc1pi_pi->SetPoint(0, 2.0, 35.4578);
     treedepth_cc1pi_pi->SetPoint(1, 3.0, 37.6588);
     treedepth_cc1pi_pi->SetPoint(2, 4.0, 37.9999);
@@ -49,7 +49,8 @@
     
     treedepth_cc1pi_mu->GetYaxis()->SetRangeUser(30.0, 40.0);
     treedepth_cc1pi_mu->Draw("AL*");
-    treedepth_cc1pi_pi->SetLineColor( kRed); treedepth_cc1pi_pi->SetMarkerColor( kRed); treedepth_cc1pi_pi->Draw("L* same");
+    treedepth_cc1pi_pi->SetLineColor( kRed); treedepth_cc1pi_pi->SetMarkerColor( kRed); treedepth_cc1pi_pi->SetFillColor( kWhite); 
+    treedepth_cc1pi_pi->Draw("L* same");
     canvas_treedepth->cd(1)->BuildLegend();
     
     canvas_treedepth->cd(2);
