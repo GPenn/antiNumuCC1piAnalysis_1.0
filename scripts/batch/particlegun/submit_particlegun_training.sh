d646cfca6dc7dc91a4779d6ba46970661a4dc65b
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-25 12:13:06 +0000

    Edit as needed

--- scripts/batch/particlegun/submit_particlegun_training.sh
+++ scripts/batch/particlegun/submit_particlegun_training.sh
@@ -4,9 +4,9 @@
 #SBATCH -p compute
 #SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/pg-%j.out
 #SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/pg-%j.err
-#SBATCH -J pg_test
+#SBATCH -J pg_train
 #SBATCH -t 24:00:00
 
 #run the application:
 source ~/highlandsetup.sh
-RunAntiNumuCC1piAnalysis.exe /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/fileList/particlegun_allpositive_testing.txt -o output/particle_gun/particlegun_allpositive_testing.root
+RunAntiNumuCC1piAnalysis.exe /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/fileList/particlegun_allpositive_training.txt -o output/particle_gun/particlegun_allpositive_training.root
