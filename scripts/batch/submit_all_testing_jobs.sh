dddcb2f1c1c672935cbb31e5cd11a7d2b8ebdafe
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-29 16:31:10 +0000

    Create master script for testing jobs

---
+++ scripts/batch/submit_all_testing_jobs.sh
@@ -0,0 +1,5 @@
+source scripts/batch/run5/submit_batch_jobs_run5.sh
+source scripts/batch/run6/submit_batch_jobs_run6.sh
+source scripts/batch/run7/submit_batch_jobs_run7.sh
+source scripts/batch/run9/submit_batch_jobs_run9.sh
+sbatch scripts/batch/particlegun/submit_particlegun_testing.sh
