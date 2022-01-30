52b0ec161e800f16d4da9d53664d264953a87e6d
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-30 14:34:45 +0000

    Add missing )

--- customplotting/parameter_testing/parameter_testing.C
+++ customplotting/parameter_testing/parameter_testing.C
@@ -54,7 +54,7 @@
     treedepth_pg_e->SetPoint(0, 2.0, 189.429);
     treedepth_pg_e->SetPoint(1, 3.0, 190.245);
     treedepth_pg_e->SetPoint(2, 4.0, 190.529);
-    treedepth_pg_e->SetPoint(3, 5.0, 190.811;
+    treedepth_pg_e->SetPoint(3, 5.0, 190.811);
     treedepth_pg_e->SetLineColor( kMagenta); treedepth_pg_e->SetMarkerColor( kMagenta); treedepth_pg_e->SetFillColor( kWhite); 
     
     TCanvas* canvas_treedepth = new TCanvas("canvas_treedepth","",200,10,1000,400);
