46fb67897c660810b2817b503b6448ba91e7c0e2
Gabriel Penn <callidus@hotmail.co.uk>
2022-01-31 08:42:13 +0000

    Switch to extra shrinkage test

--- src/antiNumuCC1piAnalysis.cxx
+++ src/antiNumuCC1piAnalysis.cxx
@@ -406,31 +406,31 @@ void antiNumuCC1piAnalysis::FillMicroTrees(bool addBase){
     output().FillVar(selmu_bdt_pid_p, BDT_PID_results_vector[2]);
     output().FillVar(selmu_bdt_pid_e, BDT_PID_results_vector[3]);
     
-    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_4vars");
+    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_0.01shrinkage");
     output().FillVar(selmu_bdt_pid_mu_test1, BDT_PID_results_vector[0]);
     output().FillVar(selmu_bdt_pid_pi_test1, BDT_PID_results_vector[1]);
     output().FillVar(selmu_bdt_pid_p_test1, BDT_PID_results_vector[2]);
     output().FillVar(selmu_bdt_pid_e_test1, BDT_PID_results_vector[3]);
     
-    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_6vars");
+    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_0.02shrinkage");
     output().FillVar(selmu_bdt_pid_mu_test2, BDT_PID_results_vector[0]);
     output().FillVar(selmu_bdt_pid_pi_test2, BDT_PID_results_vector[1]);
     output().FillVar(selmu_bdt_pid_p_test2, BDT_PID_results_vector[2]);
     output().FillVar(selmu_bdt_pid_e_test2, BDT_PID_results_vector[3]);
     
-    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_8vars");
+    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_0.04shrinkage");
     output().FillVar(selmu_bdt_pid_mu_test3, BDT_PID_results_vector[0]);
     output().FillVar(selmu_bdt_pid_pi_test3, BDT_PID_results_vector[1]);
     output().FillVar(selmu_bdt_pid_p_test3, BDT_PID_results_vector[2]);
     output().FillVar(selmu_bdt_pid_e_test3, BDT_PID_results_vector[3]);
     
-    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_10vars");
+    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_0.06shrinkage");
     output().FillVar(selmu_bdt_pid_mu_test4, BDT_PID_results_vector[0]);
     output().FillVar(selmu_bdt_pid_pi_test4, BDT_PID_results_vector[1]);
     output().FillVar(selmu_bdt_pid_p_test4, BDT_PID_results_vector[2]);
     output().FillVar(selmu_bdt_pid_e_test4, BDT_PID_results_vector[3]);
     
-    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_12vars");
+    BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().MainTrack, mybox().MainTrackLocalECalSegment, "BDTG_0.08shrinkage");
     output().FillVar(selmu_bdt_pid_mu_test5, BDT_PID_results_vector[0]);
     output().FillVar(selmu_bdt_pid_pi_test5, BDT_PID_results_vector[1]);
     output().FillVar(selmu_bdt_pid_p_test5, BDT_PID_results_vector[2]);
@@ -543,19 +543,19 @@ void antiNumuCC1piAnalysis::FillMicroTrees(bool addBase){
     
       //output().FillVar(hmnt_bdt_pid_pi_cc1pi, BDT_PID_results_vector[1]);
     
-      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_4vars");
+      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_0.01shrinkage");
       output().FillVar(hmnt_bdt_pid_pi_test1, BDT_PID_results_vector[1]);
     
-      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_6vars");
+      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_0.02shrinkage");
       output().FillVar(hmnt_bdt_pid_pi_test2, BDT_PID_results_vector[1]);
     
-      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_8vars");
+      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_0.04shrinkage");
       output().FillVar(hmnt_bdt_pid_pi_test3, BDT_PID_results_vector[1]);
     
-      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_10vars");
+      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_0.06shrinkage");
       output().FillVar(hmnt_bdt_pid_pi_test4, BDT_PID_results_vector[1]);
     
-      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_12vars");
+      BDT_PID_results_vector = myBDTPIDmanagerCC1pi->GetBDTPIDVarsPos(mybox().HMNtrack, mybox().HMNTLocalECalSegment, "BDTG_0.08shrinkage");
       output().FillVar(hmnt_bdt_pid_pi_test5, BDT_PID_results_vector[1]);
       
     
