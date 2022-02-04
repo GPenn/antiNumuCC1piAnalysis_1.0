94da46152bde705236b57294e1d25a7cc2310018
Gabriel Penn <callidus@hotmail.co.uk>
2022-02-04 10:30:25 +0000

    Switch to n-1 fgd2EbyL

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -48,8 +48,8 @@ BDTPIDmanager::BDTPIDmanager() {
   
   tmvareader->AddVariable( "mom := selmu_mom",                               &bdt_mom);
   tmvareader->AddVariable( "theta := selmu_theta",                           &bdt_theta);
-  //tmvareader->AddVariable("fgd1EbyL := selmu_fgd1_EbyL",                      &bdt_fgd1_EbyL);
-  tmvareader->AddVariable("fgd2EbyL := selmu_fgd2_EbyL",                      &bdt_fgd2_EbyL);
+  tmvareader->AddVariable("fgd1EbyL := selmu_fgd1_EbyL",                      &bdt_fgd1_EbyL);
+  //tmvareader->AddVariable("fgd2EbyL := selmu_fgd2_EbyL",                      &bdt_fgd2_EbyL);
   tmvareader->AddVariable( "tpclikemu := selmu_tpc_like_mu",                 &bdt_tpc_like_mu);
   tmvareader->AddVariable( "tpclikee := selmu_tpc_like_e",                   &bdt_tpc_like_e);
   tmvareader->AddVariable( "tpclikep := selmu_tpc_like_p",                   &bdt_tpc_like_p);
@@ -115,7 +115,7 @@ BDTPIDmanager::BDTPIDmanager() {
   //tmvareader->BookMVA( "BDTG_ecallowlevel", "parameters/weights/BDT_PID_multiclass_BDTG_ecallowlevel.weights.xml" );
   //tmvareader->BookMVA( "BDTG_ecalEbyP", "parameters/weights/BDT_PID_multiclass_BDTG_ecalEbyP.weights.xml" );
   
-  tmvareader->BookMVA( "BDTG_minus_fgd1EbyL", "parameters/weights/BDT_PID_multiclass_BDTG_minus_fgd1EbyL.weights.xml" );
+  tmvareader->BookMVA( "BDTG_minus_fgd1EbyL", "parameters/weights/BDT_PID_multiclass_BDTG_minus_fgd2EbyL.weights.xml" );
 }
 
 //--------------------------------------------------------------------------------------------------//
