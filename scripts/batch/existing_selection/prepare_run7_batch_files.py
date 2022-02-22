68d782f88679bbdda74bcc761e3e4919af1950a2
Gabriel Penn <callidus@hotmail.co.uk>
2022-02-22 17:20:16 +0000

    Copy code for testing existing cc1pi selection

---
+++ scripts/batch/existing_selection/prepare_run7_batch_files.py
@@ -0,0 +1,37 @@
+import random
+
+launchscriptfile = open("/user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/batch/run7/submit_batch_jobs_run7.sh", 'w')
+
+print "Generating job scripts..."
+
+launchscriptfile.write("cd /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0\n")
+
+listsfile = open('/user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/fileList/run7/lists.txt', 'r')
+lists = listsfile.readlines()
+
+count = 0
+
+for list in lists:
+    count += 1
+    shname = "/user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/batch/run7/submit_run7_" + str(count) + ".sh"
+    shfile = open(shname, 'w')
+    shfile.write("#!/bin/bash\n")
+    shfile.write("#SBATCH -N 1\n")
+    shfile.write("#SBATCH -c 1\n")
+    shfile.write("#SBATCH -p compute\n")
+    shfile.write("#SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/run7-%j.out\n")
+    shfile.write("#SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/run7-%j.err\n")
+    shfile.write("#SBATCH -J run7_" + str(count) + "\n")
+    shfile.write("#SBATCH -t 24:00:00\n\n")
+    shfile.write("#run the application:\n")
+    shfile.write("source ~/highlandsetup.sh\n")
+    shfile.write("RunAntiNumuCC1piAnalysis.exe " + list.strip() + " -o output/run7/oaAnalysis_prod6Trun7_" + str(count) + ".root\n")
+    shfile.close()
+
+    launchscriptfile.write("sbatch " + shname + "\n")
+
+
+launchscriptfile.write("\n echo \"All jobs submitted.\"\n")
+launchscriptfile.close()
+
+print "Done.\n"
