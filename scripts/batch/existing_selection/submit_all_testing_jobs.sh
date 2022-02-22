860dcff8b8e313bf10996c79943b24c19ff4faa0
Gabriel Penn <callidus@hotmail.co.uk>
2022-02-22 17:36:09 +0000

    Edit as needed

--- scripts/batch/existing_selection/submit_all_testing_jobs.sh
+++ scripts/batch/existing_selection/submit_all_testing_jobs.sh
@@ -1,5 +1,4 @@
-source scripts/batch/run5/submit_batch_jobs_run5.sh
-source scripts/batch/run6/submit_batch_jobs_run6.sh
-source scripts/batch/run7/submit_batch_jobs_run7.sh
-source scripts/batch/run9/submit_batch_jobs_run9.sh
-sbatch scripts/batch/particlegun/submit_particlegun_testing.sh
+source scripts/batch/existing_selection/run5/submit_batch_jobs_run5.sh
+source scripts/batch/existing_selection/run6/submit_batch_jobs_run6.sh
+source scripts/batch/existing_selection/run7/submit_batch_jobs_run7.sh
+source scripts/batch/existing_selection/run9/submit_batch_jobs_run9.sh
