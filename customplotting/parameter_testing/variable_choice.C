7f01ad1e1cab29ea928dc9e674dd35f9b12ed7e1
Gabriel Penn <callidus@hotmail.co.uk>
2022-03-20 17:09:11 +0000

    Fix bar colour and automate bar offset

--- customplotting/parameter_testing/variable_choice.C
+++ customplotting/parameter_testing/variable_choice.C
@@ -1,9 +1,11 @@
 {
     gStyle->SetTitleYOffset(1.5);
+    gStyle->SetOptStat(0);
     
     Float_t refvalue_cc1pi_mu, refvalue_cc1pi_pi, refvalue_pg_mu, refvalue_pg_pi, refvalue_pg_p, refvalue_pg_e;
     
     Float_t bar_width = 0.1;
+    Float_t bar_offset = 0.5 - bar_width*3;
     
     // =============== TPC2 variable choice ===============
     
@@ -17,7 +19,7 @@
     TPC2_cc1pi_mu->Fill(tpc2varchoices[2],33.9678/refvalue_cc1pi_mu);
     TPC2_cc1pi_mu->SetLineColor( kBlue); TPC2_cc1pi_mu->SetFillColor( kBlue);
     TPC2_cc1pi_mu->SetBarWidth(bar_width);
-    TPC2_cc1pi_mu->SetBarOffset(bar_width*1);
+    TPC2_cc1pi_mu->SetBarOffset(bar_offset + (bar_width*0));
     
     refvalue_cc1pi_pi = 36.7011;
     TH1F* TPC2_cc1pi_pi = new TH1F("TPC2_cc1pi_pi","CC1pi selection #pi^{-}",3,0,3);
@@ -26,16 +28,16 @@
     TPC2_cc1pi_pi->Fill(tpc2varchoices[2],38.2774/refvalue_cc1pi_pi);
     TPC2_cc1pi_pi->SetLineColor( kRed); TPC2_cc1pi_pi->SetFillColor( kRed);
     TPC2_cc1pi_pi->SetBarWidth(bar_width);
-    TPC2_cc1pi_pi->SetBarOffset(bar_width*2);
+    TPC2_cc1pi_pi->SetBarOffset(bar_offset + (bar_width*1));
     
     refvalue_pg_mu = 210.16;
     TH1F* TPC2_pg_mu = new TH1F("TPC2_pg_mu","Particle gun selection #mu^{+}",3,0,3);
     TPC2_pg_mu->Fill(tpc2varchoices[0],210.16/refvalue_pg_mu);
     TPC2_pg_mu->Fill(tpc2varchoices[1],213.597/refvalue_pg_mu);
     TPC2_pg_mu->Fill(tpc2varchoices[2],214.249/refvalue_pg_mu);
-    TPC2_pg_mu->SetLineColor( kBlue); TPC2_pg_mu->SetFillColor( kRed);
+    TPC2_pg_mu->SetLineColor( kBlue); TPC2_pg_mu->SetFillColor( kBlue);
     TPC2_pg_mu->SetBarWidth(bar_width);
-    TPC2_pg_mu->SetBarOffset(bar_width*3);
+    TPC2_pg_mu->SetBarOffset(bar_offset + (bar_width*2));
     
     refvalue_pg_pi = 157.581;
     TH1F* TPC2_pg_pi = new TH1F("TPC2_pg_pi","Particle gun selection #pi^{+}",3,0,3);
@@ -44,7 +46,7 @@
     TPC2_pg_pi->Fill(tpc2varchoices[2],161.309/refvalue_pg_pi);
     TPC2_pg_pi->SetLineColor( kRed); TPC2_pg_pi->SetFillColor( kRed);
     TPC2_pg_pi->SetBarWidth(bar_width);
-    TPC2_pg_pi->SetBarOffset(bar_width*4);
+    TPC2_pg_pi->SetBarOffset(bar_offset + (bar_width*3));
     
     refvalue_pg_p = 187.364;
     TH1F* TPC2_pg_p = new TH1F("TPC2_pg_p","Particle gun selection p",3,0,3);
@@ -53,7 +55,7 @@
     TPC2_pg_p->Fill(tpc2varchoices[2],195.318/refvalue_pg_p);
     TPC2_pg_p->SetLineColor( kGreen); TPC2_pg_p->SetFillColor( kGreen);
     TPC2_pg_p->SetBarWidth(bar_width);
-    TPC2_pg_p->SetBarOffset(bar_width*5);
+    TPC2_pg_p->SetBarOffset(bar_offset + (bar_width*4));
     
     refvalue_pg_e = 223.002;
     TH1F* TPC2_pg_e = new TH1F("TPC2_pg_e","Particle gun selection e^{+}",3,0,3);
@@ -62,7 +64,7 @@
     TPC2_pg_e->Fill(tpc2varchoices[2],226.972/refvalue_pg_e);
     TPC2_pg_e->SetLineColor( kMagenta); TPC2_pg_e->SetFillColor( kMagenta);
     TPC2_pg_e->SetBarWidth(bar_width);
-    TPC2_pg_e->SetBarOffset(bar_width*6);
+    TPC2_pg_e->SetBarOffset(bar_offset + (bar_width*5));
     
     TCanvas* canvas_TPC2_choices = new TCanvas("canvas_TPC2_choices","",200,10,1000,800);
     TPC2_cc1pi_mu->Draw("B");
