d32535bcb8f20eae167d67938417a8bc55ea9b2d
Gabriel Penn <callidus@hotmail.co.uk>
2022-03-21 17:49:09 +0000

    Add n-1 plot

--- customplotting/parameter_testing/variable_choice.C
+++ customplotting/parameter_testing/variable_choice.C
@@ -224,5 +224,156 @@
     TPC3_cc1pi_pi->Draw("B same");
     
     canvas_TPC3_choices->BuildLegend();
+    
+    // =============== N-1 studies ===============
+    
+    const char *nminus1varchoices[16] = {"Momentum","Angle","FGD1 E/L","FGD2 E/L","TPC2 mu-like","TPC2 pi-like","TPC2 p-like","TPC2 e-like",
+                                   "TPC3 dE/dx","ECal EM energy","ECal E/L", "ECal circularity","ECal FBR", "ECal TMR", "ECal QRMS", "nSMRDs"};
+    
+    refvalue_pg_mu = 217.434;
+    TH1F* nminus1_pg_mu = new TH1F("nminus1_pg_mu","Particle gun selection #mu^{+}",3,0,3);
+    nminus1_pg_mu->Fill(nminus1varchoices[0],215.815/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[1],217.277/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[2],215.867/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[3],215.894/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[4],216.95/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[5],217.559/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[6],217.395/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[7],217.218/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[8],218.982/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[9],216.783/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[10],215.513/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[11],216.886/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[12],217.112/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[13],217.282/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[14],216.72/refvalue_pg_mu);
+    nminus1_pg_mu->Fill(nminus1varchoices[15],215.32/refvalue_pg_mu);
+    nminus1_pg_mu->SetLineColor( kBlue); nminus1_pg_mu->SetFillColor( kBlue);
+    nminus1_pg_mu->SetBarWidth(bar_width);
+    nminus1_pg_mu->SetBarOffset(bar_offset + (bar_width*0));
+    
+    refvalue_pg_pi = 162.945;
+    TH1F* nminus1_pg_pi = new TH1F("nminus1_pg_pi","Particle gun selection #pi^{+}",3,0,3);
+    nminus1_pg_pi->Fill(nminus1varchoices[0],162.902/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[1],161.993/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[2],159.805/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[3],163.302/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[4],164.653/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[5],156.678/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[6],162.301/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[7],162.154/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[8],173.86/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[9],162.441/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[10],162.471/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[11],162.1/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[12],162.814/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[13],161.54/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[14],161.566/refvalue_pg_pi);
+    nminus1_pg_pi->Fill(nminus1varchoices[15],162.186/refvalue_pg_pi);
+    nminus1_pg_pi->SetLineColor( kRed); nminus1_pg_pi->SetFillColor( kRed);
+    nminus1_pg_pi->SetBarWidth(bar_width);
+    nminus1_pg_pi->SetBarOffset(bar_offset + (bar_width*1));
+    
+    refvalue_pg_p = 205.627;
+    TH1F* nminus1_pg_p = new TH1F("nminus1_pg_p","Particle gun selection p",3,0,3);
+    nminus1_pg_p->Fill(nminus1varchoices[0],202.643/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[1],206.435/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[2],199.125/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[3],204.102/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[4],204.816/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[5],206.427/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[6],201.879/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[7],203.156/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[8],216.782/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[9],204.324/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[10],201.988/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[11],204.329/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[12],205.953/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[13],203.829/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[14],204.326/refvalue_pg_p);
+    nminus1_pg_p->Fill(nminus1varchoices[15],204.906/refvalue_pg_p);
+    nminus1_pg_p->SetLineColor( kGreen); nminus1_pg_p->SetFillColor( kGreen);
+    nminus1_pg_p->SetBarWidth(bar_width);
+    nminus1_pg_p->SetBarOffset(bar_offset + (bar_width*2));
+    
+    refvalue_pg_e = 234.699;
+    TH1F* nminus1_pg_e = new TH1F("nminus1_pg_e","Particle gun selection e^{+}",3,0,3);
+    nminus1_pg_e->Fill(nminus1varchoices[0],231.661/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[1],234.731/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[2],230.196/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[3],234.774/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[4],234.139/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[5],234.966/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[6],234.444/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[7],233.494/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[8],241.307/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[9],233.692/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[10],234.88/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[11],234.079/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[12],234.087/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[13],232.997/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[14],233.894/refvalue_pg_e);
+    nminus1_pg_e->Fill(nminus1varchoices[15],234.148/refvalue_pg_e);
+    nminus1_pg_e->SetLineColor( kMagenta); nminus1_pg_e->SetFillColor( kMagenta);
+    nminus1_pg_e->SetBarWidth(bar_width);
+    nminus1_pg_e->SetBarOffset(bar_offset + (bar_width*3));
+    
+    refvalue_cc1pi_mu = 34.3838;
+    TH1F* nminus1_cc1pi_mu = new TH1F("nminus1_cc1pi_mu","CC1pi selection #mu^{+}",3,0,3);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[0],34.0072/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[1],34.1127/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[2],34.0343/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[3],33.9817/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[4],34.3462/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[5],34.5932/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[6],34.4939/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[7],34.4615/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[8],34.5206/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[9],34.511/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[10],34.2904/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[11],34.3347/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[12],34.3508/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[13],34.5029/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[14],34.3894/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->Fill(nminus1varchoices[15],34.3198/refvalue_cc1pi_mu);
+    nminus1_cc1pi_mu->SetLineColor( kBlue + 2); nminus1_cc1pi_mu->SetFillColor( kBlue + 2);
+    nminus1_cc1pi_mu->SetBarWidth(bar_width);
+    nminus1_cc1pi_mu->SetBarOffset(bar_offset + (bar_width*4));
+    
+    refvalue_cc1pi_pi = 39.6111;
+    TH1F* nminus1_cc1pi_pi = new TH1F("nminus1_cc1pi_pi","CC1pi selection #pi^{-}",3,0,3);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[0],39.5201/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[1],39.3991/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[2],40.0751/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[3],39.3576/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[4],39.874/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[5],38.7619/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[6],39.6025/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[7],39.5602/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[8],39.2842/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[9],39.5661/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[10],39.6219/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[11],39.2113/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[12],39.5754/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[13],39.6465/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[14],39.5392/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->Fill(nminus1varchoices[15],39.2564/refvalue_cc1pi_pi);
+    nminus1_cc1pi_pi->SetLineColor( kRed + 2); nminus1_cc1pi_pi->SetFillColor( kRed + 2);
+    nminus1_cc1pi_pi->SetBarWidth(bar_width);
+    nminus1_cc1pi_pi->SetBarOffset(bar_offset + (bar_width*5));
+    
+    TCanvas* canvas_nminus1_choices = new TCanvas("canvas_nminus1_choices","",200,10,1000,800);
+    nminus1_pg_mu->GetYaxis()->SetRangeUser(0.5, 1.5);
+    nminus1_pg_mu->GetYaxis()->SetTitle("Track ID max significance (relative)");
+    nminus1_pg_mu->GetYaxis()->SetLabelSize(0.025);
+    nminus1_pg_mu->GetYaxis()->SetTitleSize(0.025);
+    nminus1_pg_mu->Draw("B");
+    nminus1_pg_pi->Draw("B same");
+    nminus1_pg_p->Draw("B same");
+    nminus1_pg_e->Draw("B same");
+    nminus1_cc1pi_mu->Draw("B same");
+    nminus1_cc1pi_pi->Draw("B same");
+    
+    canvas_nminus1_choices->BuildLegend();
   
 }
