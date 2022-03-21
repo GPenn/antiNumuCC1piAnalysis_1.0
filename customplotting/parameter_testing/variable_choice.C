fe903bfacfbb92dda1e4778c74b1e1c449d1d68d
Gabriel Penn <callidus@hotmail.co.uk>
2022-03-21 12:43:44 +0000

    Move CC1pi plots, adjust colours

--- customplotting/parameter_testing/variable_choice.C
+++ customplotting/parameter_testing/variable_choice.C
@@ -9,26 +9,7 @@
     
     // =============== TPC2 variable choice ===============
     
-    const Int_t nx = 3;
-    const char *tpc2varchoices[nx] = {"TPC2 dE/dx","TPC2 pulls","TPC2 likelihoods"};
-    
-    refvalue_cc1pi_mu = 33.3699;
-    TH1F* TPC2_cc1pi_mu = new TH1F("TPC2_cc1pi_mu","CC1pi selection #mu^{+}",3,0,3);
-    TPC2_cc1pi_mu->Fill(tpc2varchoices[0],33.3699/refvalue_cc1pi_mu);
-    TPC2_cc1pi_mu->Fill(tpc2varchoices[1],33.7904/refvalue_cc1pi_mu);
-    TPC2_cc1pi_mu->Fill(tpc2varchoices[2],33.9678/refvalue_cc1pi_mu);
-    TPC2_cc1pi_mu->SetLineColor( kBlue); TPC2_cc1pi_mu->SetFillColor( kBlue);
-    TPC2_cc1pi_mu->SetBarWidth(bar_width);
-    TPC2_cc1pi_mu->SetBarOffset(bar_offset + (bar_width*0));
-    
-    refvalue_cc1pi_pi = 36.7011;
-    TH1F* TPC2_cc1pi_pi = new TH1F("TPC2_cc1pi_pi","CC1pi selection #pi^{-}",3,0,3);
-    TPC2_cc1pi_pi->Fill(tpc2varchoices[0],36.7011/refvalue_cc1pi_pi);
-    TPC2_cc1pi_pi->Fill(tpc2varchoices[1],37.3834/refvalue_cc1pi_pi);
-    TPC2_cc1pi_pi->Fill(tpc2varchoices[2],38.2774/refvalue_cc1pi_pi);
-    TPC2_cc1pi_pi->SetLineColor( kRed); TPC2_cc1pi_pi->SetFillColor( kRed);
-    TPC2_cc1pi_pi->SetBarWidth(bar_width);
-    TPC2_cc1pi_pi->SetBarOffset(bar_offset + (bar_width*1));
+    const char *tpc2varchoices[3] = {"TPC2 dE/dx","TPC2 pulls","TPC2 likelihoods"};
     
     refvalue_pg_mu = 210.16;
     TH1F* TPC2_pg_mu = new TH1F("TPC2_pg_mu","Particle gun selection #mu^{+}",3,0,3);
@@ -66,35 +47,33 @@
     TPC2_pg_e->SetBarWidth(bar_width);
     TPC2_pg_e->SetBarOffset(bar_offset + (bar_width*5));
     
+    refvalue_cc1pi_mu = 33.3699;
+    TH1F* TPC2_cc1pi_mu = new TH1F("TPC2_cc1pi_mu","CC1pi selection #mu^{+}",3,0,3);
+    TPC2_cc1pi_mu->Fill(tpc2varchoices[0],33.3699/refvalue_cc1pi_mu);
+    TPC2_cc1pi_mu->Fill(tpc2varchoices[1],33.7904/refvalue_cc1pi_mu);
+    TPC2_cc1pi_mu->Fill(tpc2varchoices[2],33.9678/refvalue_cc1pi_mu);
+    TPC2_cc1pi_mu->SetLineColor( kBlue + 2); TPC2_cc1pi_mu->SetFillColor( kBlue + 2);
+    TPC2_cc1pi_mu->SetBarWidth(bar_width);
+    TPC2_cc1pi_mu->SetBarOffset(bar_offset + (bar_width*0));
+    
+    refvalue_cc1pi_pi = 36.7011;
+    TH1F* TPC2_cc1pi_pi = new TH1F("TPC2_cc1pi_pi","CC1pi selection #pi^{-}",3,0,3);
+    TPC2_cc1pi_pi->Fill(tpc2varchoices[0],36.7011/refvalue_cc1pi_pi);
+    TPC2_cc1pi_pi->Fill(tpc2varchoices[1],37.3834/refvalue_cc1pi_pi);
+    TPC2_cc1pi_pi->Fill(tpc2varchoices[2],38.2774/refvalue_cc1pi_pi);
+    TPC2_cc1pi_pi->SetLineColor( kRed - 2); TPC2_cc1pi_pi->SetFillColor( kRed - 2);
+    TPC2_cc1pi_pi->SetBarWidth(bar_width);
+    TPC2_cc1pi_pi->SetBarOffset(bar_offset + (bar_width*1));
+    
     TCanvas* canvas_TPC2_choices = new TCanvas("canvas_TPC2_choices","",200,10,1000,800);
-    TPC2_cc1pi_mu->GetYaxis()->SetRangeUser(0.995, 1.045);
-    TPC2_cc1pi_mu->Draw("B");
-    TPC2_cc1pi_pi->Draw("B same");
-    TPC2_pg_mu->Draw("B same");
+    TPC2_pg_mu->GetYaxis()->SetRangeUser(0.995, 1.045);
+    TPC2_pg_mu->Draw("B");
     TPC2_pg_pi->Draw("B same");
     TPC2_pg_p->Draw("B same");
     TPC2_pg_e->Draw("B same");
+    TPC2_cc1pi_mu->Draw("B same");
+    TPC2_cc1pi_pi->Draw("B same");
     
     canvas_TPC2_choices->BuildLegend();
-    
-    /*TCanvas* canvas_treedepth = new TCanvas("canvas_treedepth","",200,10,1000,400);
-    canvas_treedepth->Divide(2,1,0.005,0.005);
-    canvas_treedepth->cd(1);
-    
-    treedepth_cc1pi_mu->GetYaxis()->SetRangeUser(0.97, 1.02);
-    treedepth_cc1pi_mu->GetXaxis()->SetTitle("Maximum tree depth"); treedepth_cc1pi_mu->GetYaxis()->SetTitle("Optimal selection significance");
-    
-    treedepth_cc1pi_mu->Draw("AL*");
-    treedepth_cc1pi_pi->Draw("L* same");
-    canvas_treedepth->cd(1)->BuildLegend();
-    
-    canvas_treedepth->cd(2);
-    treedepth_pg_mu->GetYaxis()->SetRangeUser(0.97, 1.02);
-    treedepth_pg_mu->GetXaxis()->SetTitle("Maximum tree depth"); treedepth_pg_mu->GetYaxis()->SetTitle("Optimal selection significance");
-    treedepth_pg_mu->Draw("AL*");
-    treedepth_pg_pi->Draw("L* same");
-    treedepth_pg_p->Draw("L* same");
-    treedepth_pg_e->Draw("L* same");
-    canvas_treedepth->cd(2)->BuildLegend();*/
   
 }
