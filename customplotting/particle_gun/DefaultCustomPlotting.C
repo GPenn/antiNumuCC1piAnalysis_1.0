3d8da788fec0e32b63f418300674a01dfe907d72
Gabriel Penn <callidus@hotmail.co.uk>
2022-04-14 10:06:52 +0100

    Add BDT validity criteria to efficiency calculation

--- customplotting/particle_gun/DefaultCustomPlotting.C
+++ customplotting/particle_gun/DefaultCustomPlotting.C
@@ -790,7 +790,7 @@ void DefaultCustomPlotting::Loop()
       
       // ============= Fill histograms to find optimal cuts =============
       
-      if (accum_level[0][1] > 4){ 
+      if ((accum_level[0][1] > 4) && (selmu_necals < 2) && (selmu_mom[0] > 200.0) && (selmu_mom[0] < 1500.0) && (selmu_det_theta < 1.0472)){ 
          
          if (particle == -13)
          {
