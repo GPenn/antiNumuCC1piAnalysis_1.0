2310f9a8ede325e1e27d25950092b9aedbb13b39
Gabriel Penn <callidus@hotmail.co.uk>
2022-04-17 10:43:48 +0100

    Switch to extra shrinkage test

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -122,9 +122,15 @@ BDTPIDmanager::BDTPIDmanager() {
   //tmvareader->BookMVA( "BDTG_ecalEbyP", "parameters/weights/BDT_PID_multiclass_BDTG_ecalEbyP.weights.xml" );
   //tmvareader->BookMVA( "BDTG_ntpcs", "parameters/weights/BDT_PID_multiclass_BDTG_ntpcs.weights.xml" );
   
-  tmvareader->BookMVA( "BDTG_2vars", "parameters/weights/BDT_PID_multiclass_BDTG_2vars.weights.xml" );
-  tmvareader->BookMVA( "BDTG_3vars", "parameters/weights/BDT_PID_multiclass_BDTG_3vars.weights.xml" );
+  //tmvareader->BookMVA( "BDTG_2vars", "parameters/weights/BDT_PID_multiclass_BDTG_2vars.weights.xml" );
+  //tmvareader->BookMVA( "BDTG_3vars", "parameters/weights/BDT_PID_multiclass_BDTG_3vars.weights.xml" );
   //tmvareader->BookMVA( "BDTG_4vars_redo", "parameters/weights/BDT_PID_multiclass_BDTG_4vars_redo.weights.xml" );
+  
+  tmvareader->BookMVA( "BDTG_0.1shrinkage_2500trees", "parameters/weights/BDT_PID_multiclass_BDTG_0.1shrinkage_2500trees.weights.xml" );
+  tmvareader->BookMVA( "BDTG_0.3shrinkage_2500trees", "parameters/weights/BDT_PID_multiclass_BDTG_0.3shrinkage_2500trees.weights.xml" );
+  tmvareader->BookMVA( "BDTG_0.5shrinkage_2500trees", "parameters/weights/BDT_PID_multiclass_BDTG_0.5shrinkage_2500trees.weights.xml" );
+  tmvareader->BookMVA( "BDTG_0.7shrinkage_2500trees", "parameters/weights/BDT_PID_multiclass_BDTG_0.7shrinkage_2500trees.weights.xml" );
+  tmvareader->BookMVA( "BDTG_0.9shrinkage_2500trees", "parameters/weights/BDT_PID_multiclass_BDTG_0.9shrinkage_2500trees.weights.xml" );
 }
 
 //--------------------------------------------------------------------------------------------------//
