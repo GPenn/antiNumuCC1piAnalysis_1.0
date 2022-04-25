b93097b1610f7406514e27d49085a20b9c42537f
Gabriel Penn <callidus@hotmail.co.uk>
2022-04-25 11:22:12 +0100

    Add existing PID checker

--- customplotting/particle_gun/quick_recompile.sh
+++ customplotting/particle_gun/quick_recompile.sh
@@ -1,1 +1,2 @@
 g++ -o bin/defaultCustomPlotting.exe DefaultCustomPlotting.C `root-config --cflags --libs`
+g++ -o bin/defaultCustomPlotting_existingPID.exe DefaultCustomPlotting_existingPID.C `root-config --cflags --libs`
