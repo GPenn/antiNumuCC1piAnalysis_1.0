88dfd573c971cd05746abc9c11dd8eda3c1c5eb2
Gabriel Penn <callidus@hotmail.co.uk>
2022-04-28 11:35:22 +0100

    Create submit_bdt_training_nminus1_tpc3dedx_gq.C

---
+++ scripts/batch/tmva/submit_bdt_training_nminus1_tpc3dedx_gq.C
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -p compute
+#SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/tmva-%j.out
+#SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/tmva-%j.err
+#SBATCH -J bdt_train
+#SBATCH -t 24:00:00
+
+#run the application:
+source ~/highlandsetup.sh
+cd tmva
+root -b BDT_PID_multiclass_nminus1_tpc3dedx_gq.C
