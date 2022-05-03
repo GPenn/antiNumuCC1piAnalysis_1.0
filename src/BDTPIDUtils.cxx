4543b51d792fd5f536274033dd7bd2c621dc84ba
Gabriel Penn <callidus@hotmail.co.uk>
2022-05-03 17:33:35 +0100

    Switch to theta test

--- src/BDTPIDUtils.cxx
+++ src/BDTPIDUtils.cxx
@@ -57,8 +57,8 @@ BDTPIDmanager::BDTPIDmanager() {
   
   tmvareader->AddVariable( "nsmrds := selmu_nsmrds",                          &bdt_nsmrds);*/
   
-  //tmvareader->AddVariable( "mom := selmu_mom",                               &bdt_mom);
-  tmvareader->AddVariable( "theta := selmu_theta",                           &bdt_theta);
+  tmvareader->AddVariable( "mom := selmu_mom",                               &bdt_mom);
+  //tmvareader->AddVariable( "theta := selmu_theta",                           &bdt_theta);
   tmvareader->AddVariable( "fgd1EbyL := selmu_fgd1_EbyL",                      &bdt_fgd1_EbyL);
   tmvareader->AddVariable( "fgd2EbyL := selmu_fgd2_EbyL",                      &bdt_fgd2_EbyL);
   tmvareader->AddVariable( "tpcpullmu := selmu_tpc2_pullmu",                 &bdt_tpc2_pullmu);
@@ -145,7 +145,7 @@ BDTPIDmanager::BDTPIDmanager() {
   
   //tmvareader->BookMVA( "BDTG_tunedparams", "parameters/weights/BDT_PID_multiclass_BDTG_tunedparams.weights.xml" );
   
-  tmvareader->BookMVA( "BDTG_tuned", "parameters/weights/BDT_PID_multiclass_nminus1_mom_BDTG_tuned.weights.xml" );
+  tmvareader->BookMVA( "BDTG_tuned", "parameters/weights/BDT_PID_multiclass_nminus1_theta_BDTG_tuned.weights.xml" );
   
 }
 
