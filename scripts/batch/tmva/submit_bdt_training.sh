dae9b050aa6de52f0815821f83a097f7a00d186f
Gabriel Penn <callidus@hotmail.co.uk>
2022-05-11 00:10:07 +0900

    Switch to short job

--- scripts/batch/tmva/submit_bdt_training.sh
+++ scripts/batch/tmva/submit_bdt_training.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 1
-#SBATCH -p compute
+#SBATCH -p short
 #SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/tmva-%j.out
 #SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/tmva-%j.err
 #SBATCH -J bdt_train
-#SBATCH -t 24:00:00
+#SBATCH -t 3:00:00
 
 #run the application:
 source ~/highlandsetup.sh
