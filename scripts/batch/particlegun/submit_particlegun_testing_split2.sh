ff847be568dc11eeaf4d8d5de64c97c6a907d740
Gabriel Penn <callidus@hotmail.co.uk>
2022-05-11 17:32:04 +0100

    Create submit_particlegun_testing_split2.sh

---
+++ scripts/batch/particlegun/submit_particlegun_testing_split2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH -p short
+#SBATCH -o /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/pg-%j.out
+#SBATCH -e /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/scripts/log/pg-%j.err
+#SBATCH -J pg_test
+#SBATCH -t 3:00:00
+
+#run the application:
+source ~/highlandsetup.sh
+RunAntiNumuCC1piAnalysis.exe /user/gpenn/nd280software/nd280release_12.31/antiNumuCC1piAnalysis_1.0/fileList/particlegun_allpositive_testing_split2.txt -o output/particle_gun/particlegun_allpositive_testing_split2.root
