b572ec963f339c2f63b19e90e398312788a94f69
Gabriel Penn <callidus@hotmail.co.uk>
2022-05-12 18:15:58 +0100

    Switch to tpc3 pulls test

--- tmva/BDT_PID_multiclass.C
+++ tmva/BDT_PID_multiclass.C
@@ -216,10 +216,10 @@ void BDT_PID_multiclass( TString myMethodList = "" )
    factory->AddVariable( "fgd2EbyL := selmu_fgd2_EbyL",                      "FGD2 E/L",               "", 'F' );
    
    // Muon candidate TPC variables
-   factory->AddVariable( "tpclikemu := selmu_tpc_like_mu",                 "TPC2 mu-like",    "", 'F' );
-   factory->AddVariable( "tpclikee := selmu_tpc_like_e",                   "TPC2 e-like",     "", 'F' );
-   factory->AddVariable( "tpclikep := selmu_tpc_like_p",                   "TPC2 p-like",     "", 'F' );
-   factory->AddVariable( "tpclikepi := selmu_tpc_like_pi",                 "TPC2 pi-like",    "", 'F' );
+   //factory->AddVariable( "tpclikemu := selmu_tpc_like_mu",                 "TPC2 mu-like",    "", 'F' );
+   //factory->AddVariable( "tpclikee := selmu_tpc_like_e",                   "TPC2 e-like",     "", 'F' );
+   //factory->AddVariable( "tpclikep := selmu_tpc_like_p",                   "TPC2 p-like",     "", 'F' );
+   //factory->AddVariable( "tpclikepi := selmu_tpc_like_pi",                 "TPC2 pi-like",    "", 'F' );
    //factory->AddVariable( "tpc2pullmu := selmu_tpc2_pullmu",                  "TPC2 mu pull",    "", 'F' );
    //factory->AddVariable( "tpc2pulle := selmu_tpc2_pullele",                  "TPC2 e pull",     "", 'F' );
    //factory->AddVariable( "tpc2pullp := selmu_tpc2_pullp",                    "TPC2 p pull",     "", 'F' );
@@ -227,15 +227,15 @@ void BDT_PID_multiclass( TString myMethodList = "" )
    //factory->AddVariable( "tpc2dedx := selmu_tpc2_dedx ",                   "TPC2 dE/dx",    "", 'F' );
    
    //factory->AddVariable( "tpc3dedx := selmu_tpc3_dedx ",                   "TPC3 dE/dx",    "", 'F' );
-   //factory->AddVariable( "tpc3pullmu := selmu_tpc3_pullmu",                  "TPC3 mu pull",    "", 'F' );
-   //factory->AddVariable( "tpc3pulle := selmu_tpc3_pullele",                  "TPC3 e pull",     "", 'F' );
-   //factory->AddVariable( "tpc3pullp := selmu_tpc3_pullp",                    "TPC3 p pull",     "", 'F' );
-   //factory->AddVariable( "tpc3pullpi := selmu_tpc3_pullpi",                  "TPC3 pi pull",    "", 'F' );
+   factory->AddVariable( "tpc3pullmu := selmu_tpc3_pullmu",                  "TPC3 mu pull",    "", 'F' );
+   factory->AddVariable( "tpc3pulle := selmu_tpc3_pullele",                  "TPC3 e pull",     "", 'F' );
+   factory->AddVariable( "tpc3pullp := selmu_tpc3_pullp",                    "TPC3 p pull",     "", 'F' );
+   factory->AddVariable( "tpc3pullpi := selmu_tpc3_pullpi",                  "TPC3 pi pull",    "", 'F' );
    //factory->AddVariable( "tpc3dedx_gq := selmu_tpc3_dedx_gq ",                   "TPC3 dE/dx (GQ)",    "", 'F' );
-   factory->AddVariable( "tpc3pullmu_gq := selmu_tpc3_pullmu_gq",                  "TPC3 mu pull (GQ)",    "", 'F' );
-   factory->AddVariable( "tpc3pulle_gq := selmu_tpc3_pullele_gq",                  "TPC3 e pull (GQ)",     "", 'F' );
-   factory->AddVariable( "tpc3pullp_gq := selmu_tpc3_pullp_gq",                    "TPC3 p pull (GQ)",     "", 'F' );
-   factory->AddVariable( "tpc3pullpi_gq := selmu_tpc3_pullpi_gq",                  "TPC3 pi pull (GQ)",    "", 'F' );
+   //factory->AddVariable( "tpc3pullmu_gq := selmu_tpc3_pullmu_gq",                  "TPC3 mu pull (GQ)",    "", 'F' );
+   //factory->AddVariable( "tpc3pulle_gq := selmu_tpc3_pullele_gq",                  "TPC3 e pull (GQ)",     "", 'F' );
+   //factory->AddVariable( "tpc3pullp_gq := selmu_tpc3_pullp_gq",                    "TPC3 p pull (GQ)",     "", 'F' );
+   //factory->AddVariable( "tpc3pullpi_gq := selmu_tpc3_pullpi_gq",                  "TPC3 pi pull (GQ)",    "", 'F' );
    factory->AddVariable( "ntpcs := selmu_ntpcs ",                          "TPC segments",    "", 'I' );
    
    // Muon candidate ECal variables
@@ -587,7 +587,7 @@ void BDT_PID_multiclass( TString myMethodList = "" )
    //factory->BookMethod( TMVA::Types::kBDT, "BDTG_minus_nsmrds", "!H:!V:NTrees=2000:BoostType=Grad:Shrinkage=0.1:nCuts=2000:MaxDepth=4:UseBaggedBoost:BaggedSampleFraction=0.70:UseRandomisedTrees:UseNvars=10");
    
    //factory->BookMethod( TMVA::Types::kBDT, "BDTG_tunedparams", "!H:!V:NTrees=1500:BoostType=Grad:Shrinkage=1.0:nCuts=2000:MaxDepth=4");
-   factory->BookMethod( TMVA::Types::kBDT, "BDTG_tpc3pulls_gq", "!H:!V:NTrees=1500:BoostType=Grad:Shrinkage=1.0:nCuts=2000:MaxDepth=4");
+   factory->BookMethod( TMVA::Types::kBDT, "BDTG_tpc3pulls", "!H:!V:NTrees=1500:BoostType=Grad:Shrinkage=1.0:nCuts=2000:MaxDepth=4");
    //factory->BookMethod( TMVA::Types::kBDT, "BDTG_tunedvars", "!H:!V:NTrees=1500:BoostType=Grad:Shrinkage=1.0:nCuts=2000:MaxDepth=4");
    
    // For an example of the category classifier usage, see: TMVAClassificationCategory
