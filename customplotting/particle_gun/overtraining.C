9e99b0126b362e0920861b7378fb78b79fae01b8
Gabriel Penn <callidus@hotmail.co.uk>
2022-06-02 18:34:57 +0100

    Add testing graphs and set to dashed

--- customplotting/particle_gun/overtraining.C
+++ customplotting/particle_gun/overtraining.C
@@ -1,10 +1,9 @@
 {
+    TCanvas* canvas_overtrain_check = new TCanvas("canvas_overtrain_check","canvas_overtrain_check",200,10,1000,600);
+    
     TFile *trainingfile = new TFile("output/trainingsample.root");
-
     trainingfile.ls();
     
-    TCanvas* canvas_overtrain_check = new TCanvas("canvas_overtrain_check","canvas_overtrain_check",200,10,1000,600);
-
     TGraph* training_antimu = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_antimu");
     TGraph* training_piplus = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_piplus");
     TGraph* training_proton = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_proton");
@@ -14,4 +13,22 @@
     training_piplus->Draw("L same");
     training_proton->Draw("L same");
     training_positron->Draw("L same");
+    
+    TFile *testingfile = new TFile("output/testingsample.root");
+    testingfile.ls();
+    
+    TGraph* testing_antimu = (TGraph*)testingfile.Get("graph_bdt_efficiency_recomom_antimu");
+    TGraph* testing_piplus = (TGraph*)testingfile.Get("graph_bdt_efficiency_recomom_piplus");
+    TGraph* testing_proton = (TGraph*)testingfile.Get("graph_bdt_efficiency_recomom_proton");
+    TGraph* testing_positron = (TGraph*)testingfile.Get("graph_bdt_efficiency_recomom_positron");
+    
+    testing_antimu->SetLineStyle("kDashed");
+    testing_piplus->SetLineStyle("kDashed");
+    testing_proton->SetLineStyle("kDashed");
+    testing_positron->SetLineStyle("kDashed");
+    
+    testing_antimu->Draw("L same");
+    testing_piplus->Draw("L same");
+    testing_proton->Draw("L same");
+    testing_positron->Draw("L same");
 }
