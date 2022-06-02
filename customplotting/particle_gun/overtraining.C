74c0743816d80a67f11463cade9e4785cb486afa
Gabriel Penn <callidus@hotmail.co.uk>
2022-06-02 18:16:57 +0100

    Add other training graphs to canvas

--- customplotting/particle_gun/overtraining.C
+++ customplotting/particle_gun/overtraining.C
@@ -5,7 +5,13 @@
     
     TCanvas* canvas_overtrain_check = new TCanvas("canvas_overtrain_check","canvas_overtrain_check",200,10,1000,600);
 
-    TGraph * test = new TGraph();
-    test = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_antimu");
-    test->Draw();
+    TGraph* training_antimu = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_antimu");
+    TGraph* training_piplus = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_piplus");
+    TGraph* training_proton = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_proton");
+    TGraph* training_positron = (TGraph*)trainingfile.Get("graph_bdt_efficiency_recomom_positron");
+    
+    training_antimu->Draw("AL");
+    training_piplus->Draw("L same");
+    training_proton->Draw("L same");
+    training_positron->Draw("L same");
 }
