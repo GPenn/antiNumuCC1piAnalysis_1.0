c8e2b94c7e491825289d20fb83d7db27938d1219
Gabriel Penn <callidus@hotmail.co.uk>
2022-06-14 15:24:29 +0100

    Add improved selection

--- customplotting/combined/cc1pi_effpur.C
+++ customplotting/combined/cc1pi_effpur.C
@@ -1,25 +1,31 @@
 {
-    TCanvas* canvas_eff_kinlimited = new TCanvas("canvas_eff_kinlimited","canvas_eff_kinlimited",200,10,1000,600);
-    
     TFile *file_existing_selection_kinlimited = new TFile("../existing_selection/output/existing_selection_kinlimited.root");
     //file_existing_selection_kinlimited.ls();
-    
     TGraph* exsel_kinlimited_eff = (TGraph*)file_existing_selection_kinlimited.Get("graph_eff_vs_recomom");
     TGraph* exsel_kinlimited_pur = (TGraph*)file_existing_selection_kinlimited.Get("graph_pur_vs_recomom");
     
-    
+    TFile *file_improved_selection_kinlimited = new TFile("../existing_selection_noposmult/output/improved_selection_kinlimited.root");
+    //file_existing_selection_kinlimited.ls();
+    TGraph* impsel_kinlimited_eff = (TGraph*)file_improved_selection_kinlimited.Get("graph_eff_vs_recomom");
+    TGraph* impsel_kinlimited_pur = (TGraph*)file_improved_selection_kinlimited.Get("graph_pur_vs_recomom");
     
     exsel_kinlimited_eff->SetTitle("Existing selection");
     exsel_kinlimited_pur->SetTitle("Existing selection");
+    impsel_kinlimited_eff->SetTitle("Improved selection");
+    impsel_kinlimited_pur->SetTitle("Improved selection");
+    
+    impsel_kinlimited_eff->SetLineStyle(kDashed);
+    impsel_kinlimited_pur->SetLineStyle(kDashed);
+    
+    TCanvas* canvas_eff_kinlimited = new TCanvas("canvas_eff_kinlimited","canvas_eff_kinlimited",200,10,1000,600);
     
     exsel_kinlimited_eff->Draw("AL");
-    //training_piplus->Draw("L same");
-    //training_proton->Draw("L same");
-    //training_positron->Draw("L same");
+    impsel_kinlimited_eff->Draw("L same");
     
     TCanvas* canvas_pur_kinlimited = new TCanvas("canvas_pur_kinlimited","canvas_pur_kinlimited",200,10,1000,600);
     
     exsel_kinlimited_pur->Draw("AL");
+    impsel_kinlimited_pur->Draw("L same");
     
     canvas_eff_kinlimited->BuildLegend();
     canvas_pur_kinlimited->BuildLegend();
