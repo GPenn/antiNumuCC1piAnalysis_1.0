1e590b57432b3c0897ff67f33b3e503e77881412
Gabriel Penn <callidus@hotmail.co.uk>
2022-06-14 16:06:02 +0100

    Add full kinematics plots

--- customplotting/combined/cc1pi_effpur.C
+++ customplotting/combined/cc1pi_effpur.C
@@ -58,4 +58,64 @@
     
     canvas_eff_kinlimited->BuildLegend();
     canvas_pur_kinlimited->BuildLegend();
+    
+    TFile *file_existing_selection_kinfull = new TFile("../existing_selection/output/existing_selection_kinfull.root");
+    //file_existing_selection_kinfull.ls();
+    TGraph* exsel_kinfull_eff = (TGraph*)file_existing_selection_kinfull.Get("graph_eff_vs_recomom");
+    TGraph* exsel_kinfull_pur = (TGraph*)file_existing_selection_kinfull.Get("graph_pur_vs_recomom");
+    
+    TFile *file_improved_selection_kinfull = new TFile("../existing_selection_noposmult/output/improved_selection_kinfull.root");
+    //file_improved_selection_kinfull.ls();
+    TGraph* impsel_kinfull_eff = (TGraph*)file_improved_selection_kinfull.Get("graph_eff_vs_recomom");
+    TGraph* impsel_kinfull_pur = (TGraph*)file_improved_selection_kinfull.Get("graph_pur_vs_recomom");
+    
+    TFile *file_nomuonrej_selection_kinfull = new TFile("../existing_selection_noposmult/output/nomuonrejection_kinfull.root");
+    //file_nomuonrej_selection_kinfull.ls();
+    TGraph* nomuonrej_kinfull_eff = (TGraph*)file_nomuonrej_selection_kinfull.Get("graph_eff_vs_recomom");
+    TGraph* nomuonrej_kinfull_pur = (TGraph*)file_nomuonrej_selection_kinfull.Get("graph_pur_vs_recomom");
+    
+    TFile *file_muonrej_selection_kinfull = new TFile("../existing_selection_noposmult/output/muonrejection_kinfull.root");
+    //file_muonrej_selection_kinfull.ls();
+    TGraph* muonrej_kinfull_eff = (TGraph*)file_muonrej_selection_kinfull.Get("graph_eff_vs_recomom");
+    TGraph* muonrej_kinfull_pur = (TGraph*)file_muonrej_selection_kinfull.Get("graph_pur_vs_recomom");
+    
+    exsel_kinfull_eff->SetTitle("Existing selection");
+    exsel_kinfull_pur->SetTitle("Existing selection");
+    impsel_kinfull_eff->SetTitle("Improved selection");
+    impsel_kinfull_pur->SetTitle("Improved selection");
+    nomuonrej_kinfull_eff->SetTitle("BDT selection (no #mu^{-} rejection)");
+    nomuonrej_kinfull_pur->SetTitle("BDT selection (no #mu^{-} rejection)");
+    muonrej_kinfull_eff->SetTitle("BDT selection (full)");
+    muonrej_kinfull_pur->SetTitle("BDT selection (full)");
+    
+    exsel_kinfull_eff->GetYaxis()->SetRangeUser(0.0, 1.0);
+    exsel_kinfull_pur->GetYaxis()->SetRangeUser(0.0, 1.0);
+    exsel_kinfull_eff->GetYaxis()->SetTitle("#bar{#nu}_{#mu} selection efficiency");
+    exsel_kinfull_pur->GetYaxis()->SetTitle("#bar{#nu}_{#mu} selection purity");
+    exsel_kinfull_eff->GetXaxis()->SetTitle("Antimuon candidate reconstructed momentum (MeV/c)");
+    exsel_kinfull_pur->GetXaxis()->SetTitle("Antimuon candidate reconstructed momentum (MeV/c)");                                        
+    
+    impsel_kinfull_eff->SetLineStyle(kDashed);
+    impsel_kinfull_pur->SetLineStyle(kDashed);
+    nomuonrej_kinfull_eff->SetLineStyle(10);
+    nomuonrej_kinfull_pur->SetLineStyle(10);
+    muonrej_kinfull_eff->SetLineStyle(kDotted);
+    muonrej_kinfull_pur->SetLineStyle(kDotted);
+    
+    TCanvas* canvas_eff_kinfull = new TCanvas("canvas_eff_kinfull","canvas_eff_kinfull",200,10,1000,600);
+    
+    exsel_kinfull_eff->Draw("AL");
+    impsel_kinfull_eff->Draw("L same");
+    nomuonrej_kinfull_eff->Draw("L same");
+    muonrej_kinfull_eff->Draw("L same");
+    
+    TCanvas* canvas_pur_kinfull = new TCanvas("canvas_pur_kinfull","canvas_pur_kinfull",200,10,1000,600);
+    
+    exsel_kinfull_pur->Draw("AL");
+    impsel_kinfull_pur->Draw("L same");
+    nomuonrej_kinfull_pur->Draw("L same");
+    muonrej_kinfull_pur->Draw("L same");
+    
+    canvas_eff_kinfull->BuildLegend();
+    canvas_pur_kinfull->BuildLegend();
 }
