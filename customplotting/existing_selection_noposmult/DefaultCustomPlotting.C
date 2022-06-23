e7a044a8ce7268e440245e9e3bcf26cb07e121e8
Gabriel Penn <callidus@hotmail.co.uk>
2022-06-23 12:49:19 +0100

    Add option to limit to two-track selection

--- customplotting/existing_selection_noposmult/DefaultCustomPlotting.C
+++ customplotting/existing_selection_noposmult/DefaultCustomPlotting.C
@@ -34,6 +34,7 @@ void DefaultCustomPlotting::Loop()
 //by  b_branchname->GetEntry(ientry); //read only this branch
    
    Bool_t limit_kinematics = true;
+   Bool_t two_track_only = true;
    
    gStyle->SetOptStat(0);
    gStyle->SetOptTitle(0);
@@ -206,6 +207,7 @@ void DefaultCustomPlotting::Loop()
       //if (accum_level[0][0] <= 4) continue; // Set accum_level
       
       if ((limit_kinematics) && !((selmu_mom[0] > 200.0) && (selmu_mom[0] < 1500.0) && (selmu_det_theta < 1.0472) && (HMNT_mom > 200.0) && (HMNT_mom < 1500.0))) continue;
+      if ((two_track_only) && !((ntpcposQualityFV==1) && (ntpcnegQualityFV==1))) continue;
       
       if (accum_level[0][1] > 2){
          
