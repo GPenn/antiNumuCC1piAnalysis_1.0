52544f1d95c7ca5bf0946193f9a3c90bfe29dc17
Gabriel Penn <callidus@hotmail.co.uk>
2022-08-19 15:24:12 +0100

    Delete RunAntiNumuBDTTestingAnalysis.cxx

--- app/RunAntiNumuBDTTestingAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "antiNumuBDTTestingAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  antiNumuBDTTestingAnalysis* ana = new antiNumuBDTTestingAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
