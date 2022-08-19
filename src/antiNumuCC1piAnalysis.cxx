f98f3e81bf9f3d0a65a8853dcc81b8abff5e455f
Gabriel Penn <callidus@hotmail.co.uk>
2022-08-19 15:54:10 +0100

    Add further debug statements

--- src/antiNumuCC1piAnalysis.cxx
+++ src/antiNumuCC1piAnalysis.cxx
@@ -276,8 +276,10 @@ void antiNumuCC1piAnalysis::FillMicroTrees(bool addBase){
   if (mybox().MainTrack  ) 
   {
     std::cout << "DEBUG: Filling muon candidate variables..." << std::endl;
+    std::cout << "DEBUG: Filling PDG ID..." << std::endl;
     output().FillVar(particle_pg, mybox().MainTrack->GetTrueParticle()->PDG);
     
+    std::cout << "DEBUG: Filling angle wrt detector..." << std::endl;
     TVector3 nuDirVec = anaUtils::GetNuDirRec(box().MainTrack->PositionStart);
     TVector3 muDirVec = anaUtils::ArrayToTVector3(box().MainTrack->DirectionStart);
     //double costheta_mu_nu = nuDirVec.Dot(muDirVec);
