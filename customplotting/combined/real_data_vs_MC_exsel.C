f88a10ce1bc29b51ca3258bcd88c07acaf1175e8
Gabriel Penn <callidus@hotmail.co.uk>
2022-11-23 15:23:21 +0000

    Set maximum to 50

--- customplotting/combined/real_data_vs_MC_exsel.C
+++ customplotting/combined/real_data_vs_MC_exsel.C
@@ -8,7 +8,7 @@
     TCanvas* canvas_comparison_recomom_exsel = new TCanvas("canvas_comparison_recomom_exsel","canvas_comparison_recomom_exsel",200,10,1000,600);
     
     recomom_exsel_stack->Draw("");
-    recomom_exsel_stack->SetMaximum(45.0);
+    recomom_exsel_stack->SetMaximum(50.0);
     recomom_exsel_stack->Draw("");
     recomom_exsel_all->Draw("same E1");
     canvas_comparison_recomom_exsel->BuildLegend();
