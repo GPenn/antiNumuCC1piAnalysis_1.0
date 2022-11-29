cef1cc9f21440d7d85ed6b722fb6ca65d7f5d7a6
Gabriel Penn <callidus@hotmail.co.uk>
2022-11-29 17:59:05 +0000

    Add altbkg plot

--- customplotting/combined/real_data_vs_MC_exsel.C
+++ customplotting/combined/real_data_vs_MC_exsel.C
@@ -1,11 +1,15 @@
 {
     TFile *file_MC = new TFile("../existing_selection/output/MC_for_data_comparison_exsel.root");
     THStack* recomom_exsel_stack = (THStack*)file_MC.Get("recomom_exsel_stack");
+    THStack* recomom_exsel_stack_altbkg = (THStack*)file_MC.Get("recomom_exsel_stack_altbkg");
     TH1F* recomom_exsel_cc0pi = (TH1F*)file_MC.Get("recomom_exsel_cc0pi");
     TH1F* recomom_exsel_cc1pi = (TH1F*)file_MC.Get("recomom_exsel_cc1pi");
     TH1F* recomom_exsel_ccother = (TH1F*)file_MC.Get("recomom_exsel_ccother");
     TH1F* recomom_exsel_bkg = (TH1F*)file_MC.Get("recomom_exsel_bkg");
     TH1F* recomom_exsel_oofv = (TH1F*)file_MC.Get("recomom_exsel_oofv");
+    TH1F* recomom_exsel_numubarccbkg = (TH1F*)file_MC.Get("recomom_exsel_numubarccbkg");
+    TH1F* recomom_exsel_numucc = (TH1F*)file_MC.Get("recomom_exsel_bkg");
+    TH1F* recomom_exsel_otherbkg = (TH1F*)file_MC.Get("recomom_exsel_otherbkg");
     
     TFile *file_realdata = new TFile("../real_data_exsel/output/data_for_MC_comparison.root");
     TH1F* recomom_exsel_all = (TH1F*)file_realdata.Get("recomom_exsel_all");
@@ -26,4 +30,21 @@
     legend->AddEntry(recomom_exsel_bkg,"NEUT MC: non-#bar{#nu}_{#mu}-CC backgrounds","f");
     legend->AddEntry(recomom_exsel_oofv,"NEUT MC: OOFV backgrounds","f");
     legend->Draw();
+    
+    TCanvas* canvas_comparison_recomom_exsel_altbkg = new TCanvas("canvas_comparison_recomom_exsel_altbkg","canvas_comparison_recomom_exsel_altbkg",200,10,1000,600);
+    
+    recomom_exsel_stack_altbkg->Draw("");
+    recomom_exsel_stack_altbkg->SetMaximum(53.0);
+    recomom_exsel_stack_altbkg->Draw("");
+    recomom_exsel_all->Draw("same E1");
+    //canvas_comparison_recomom_exsel->BuildLegend();
+    
+    auto legend_altbkg = new TLegend(0.1,0.7,0.48,0.9);
+    legend_altbkg->AddEntry(recomom_exsel_all,"T2K data (Run 5+6+7+9)","lep");
+    legend_altbkg->AddEntry(recomom_exsel_cc1pi,"NEUT MC: #bar{#nu}_{#mu} CC1pi (signal)","f");
+    legend_altbkg->AddEntry(recomom_exsel_numubarccbkg,"NEUT MC: #bar{#nu}_{#mu} CC backgrounds","f");
+    legend_altbkg->AddEntry(recomom_exsel_numucc,"NEUT MC: #nu_{#mu} CC backgrounds","f");
+    legend_altbkg->AddEntry(recomom_exsel_otherbkg,"NEUT MC: other backgrounds","f");
+    legend_altbkg->AddEntry(recomom_exsel_oofv,"NEUT MC: OOFV","f");
+    legend_altbkg->Draw();
 }
