2885000ddf3d695afb4e124e550cc4d70d7f62f6
Gabriel Penn <callidus@hotmail.co.uk>
2022-12-06 12:19:45 +0000

    Add hmnt mom plot

--- customplotting/combined/real_data_vs_MC_impsel.C
+++ customplotting/combined/real_data_vs_MC_impsel.C
@@ -11,8 +11,20 @@
     TH1F* recomom_impsel_numucc = (TH1F*)file_MC.Get("recomom_impsel_bkg");
     TH1F* recomom_impsel_otherbkg = (TH1F*)file_MC.Get("recomom_impsel_otherbkg");
     
+    THStack* recopimom_impsel_stack = (THStack*)file_MC.Get("recopimom_impsel_stack");
+    THStack* recopimom_impsel_stack_altbkg = (THStack*)file_MC.Get("recopimom_impsel_stack_altbkg");
+    TH1F* recopimom_impsel_cc0pi = (TH1F*)file_MC.Get("recopimom_impsel_cc0pi");
+    TH1F* recopimom_impsel_cc1pi = (TH1F*)file_MC.Get("recopimom_impsel_cc1pi");
+    TH1F* recopimom_impsel_ccother = (TH1F*)file_MC.Get("recopimom_impsel_ccother");
+    TH1F* recopimom_impsel_bkg = (TH1F*)file_MC.Get("recopimom_impsel_bkg");
+    TH1F* recopimom_impsel_oofv = (TH1F*)file_MC.Get("recopimom_impsel_oofv");
+    TH1F* recopimom_impsel_numubarccbkg = (TH1F*)file_MC.Get("recopimom_impsel_numubarccbkg");
+    TH1F* recopimom_impsel_numucc = (TH1F*)file_MC.Get("recopimom_impsel_bkg");
+    TH1F* recopimom_impsel_otherbkg = (TH1F*)file_MC.Get("recopimom_impsel_otherbkg");
+    
     TFile *file_realdata = new TFile("../real_data/output/data_for_MC_comparison_impsel.root");
     TH1F* recomom_impsel_all = (TH1F*)file_realdata.Get("recomom_impsel_all");
+    TH1F* recopimom_impsel_all = (TH1F*)file_realdata.Get("recopimom_impsel_all");
     
     TCanvas* canvas_comparison_recomom_impsel = new TCanvas("canvas_comparison_recomom_impsel","canvas_comparison_recomom_impsel",200,10,1000,600);
     
@@ -46,5 +58,22 @@
     legend_altbkg->AddEntry(recomom_impsel_numucc,"NEUT MC: #nu_{#mu} CC backgrounds","f");
     legend_altbkg->AddEntry(recomom_impsel_otherbkg,"NEUT MC: other backgrounds","f");
     legend_altbkg->AddEntry(recomom_impsel_oofv,"NEUT MC: OOFV","f");
+    legend_altbkg->Draw();
+    
+    TCanvas* canvas_comparison_recopimom_impsel_altbkg = new TCanvas("canvas_comparison_recopimom_impsel_altbkg","canvas_comparison_recopimom_impsel_altbkg",200,10,1000,600);
+    
+    recopimom_impsel_stack_altbkg->Draw("");
+    recopimom_impsel_stack_altbkg->SetMaximum(53.0);
+    recopimom_impsel_stack_altbkg->Draw("");
+    recopimom_impsel_all->Draw("same E1");
+    //canvas_comparison_recopimom_impsel->BuildLegend();
+    
+    auto legend_altbkg = new TLegend(0.1,0.7,0.48,0.9);
+    legend_altbkg->AddEntry(recopimom_impsel_all,"T2K data (Run 5+6+7+9)","lep");
+    legend_altbkg->AddEntry(recopimom_impsel_cc1pi,"NEUT MC: #bar{#nu}_{#mu} CC1pi (signal)","f");
+    legend_altbkg->AddEntry(recopimom_impsel_numubarccbkg,"NEUT MC: #bar{#nu}_{#mu} CC backgrounds","f");
+    legend_altbkg->AddEntry(recopimom_impsel_numucc,"NEUT MC: #nu_{#mu} CC backgrounds","f");
+    legend_altbkg->AddEntry(recopimom_impsel_otherbkg,"NEUT MC: other backgrounds","f");
+    legend_altbkg->AddEntry(recopimom_impsel_oofv,"NEUT MC: OOFV","f");
     legend_altbkg->Draw();
 }
