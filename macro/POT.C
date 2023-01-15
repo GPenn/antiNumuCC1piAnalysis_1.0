56fcc1d7daca79642d9811eedaf5b6bdd7f6eaac
Gabriel Penn <callidus@hotmail.co.uk>
2023-01-15 14:59:53 +0000

    Update POT.C

--- macro/POT.C
+++ macro/POT.C
@@ -1,5 +1,5 @@
 {
-    #include <string>;
+    #include <sstream>;
     
     // Initialise DrawingTools
     DrawingTools drawingtools("/bundle/data/T2K/users/gpenn/highlandBDTtests/final/cc1pi/nomuonrejection/oaAnalysis_prod6Trun5_1.root");
@@ -10,10 +10,9 @@
     // Add Run 5 MC POT info
     for (int i = 2; i <= 11s; i++)
     {
-        std::string filename = "/bundle/data/T2K/users/gpenn/highlandBDTtests/final/cc1pi/nomuonrejection/oaAnalysis_prod6Trun5_";
-        filename += std::to_string(i);
-        filename += ".root";
-        MC.AddHeader(filename);
+        std::stringstream filename;
+        filename << "/bundle/data/T2K/users/gpenn/highlandBDTtests/final/cc1pi/nomuonrejection/oaAnalysis_prod6Trun5_" << i << ".root";
+        MC.AddHeader(filename.str());
     }
     
     drawingtools.DumpPOT(MC);
