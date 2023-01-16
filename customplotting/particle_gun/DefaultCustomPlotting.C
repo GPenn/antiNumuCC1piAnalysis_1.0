30bb628f2f95b68009bcc0fd5c4cefa7c1107ae5
Gabriel Penn <callidus@hotmail.co.uk>
2023-01-16 17:00:01 +0000

    Try mu-like ROC curve

--- customplotting/particle_gun/DefaultCustomPlotting.C
+++ customplotting/particle_gun/DefaultCustomPlotting.C
@@ -3388,6 +3388,19 @@ void DefaultCustomPlotting::Loop()
    */
    
    
+   // ROC curves
+   
+   TGraph* roc_purvseff_mulike = new TGraph();
+   roc_purvseff_mulike->SetTitle(" ;Antimuon selection efficiency;Antimuon selection purity;");
+   
+   for (Int_t cut = 1; cut <= optimisation_nbins; cut++)
+   {
+      Float_t efficiency = opt_mulike_sig->Integral(cut,optimisation_nbins)/opt_mulike_sig->GetEntries();
+      Float_t purity = opt_mulike_sig->Integral(cut,optimisation_nbins)/(opt_mulike_sig->Integral(cut,optimisation_nbins) + opt_mulike_bkg->Integral(cut,optimisation_nbins));
+      roc_purvseff_mulike->SetPoint(cut, opt_elike_sig->GetBinLowEdge(cut), significance);
+   }
+   
+   roc_purvseff_mulike->Write();
       
    std::cout << std::endl << "All entries processed. Writing output file...\n\n";
    
