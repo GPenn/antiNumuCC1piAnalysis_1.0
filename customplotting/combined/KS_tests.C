5b88c3a9d82f4fd42205aafbd19c028ae2a73a6c
Gabriel Penn <callidus@hotmail.co.uk>
2023-01-19 14:12:13 +0000

    Add mu-like KS test

--- customplotting/combined/KS_tests.C
+++ customplotting/combined/KS_tests.C
@@ -13,12 +13,26 @@
     for (Int_t i=0; i < n_training; i++)
     {
         ks_test_bdt_mulike_pilike_train->GetPoint(i,training_mulike[i],training_pilike[i]);
-        std::cout << "DEBUG: training_mulike[" << i << "] = " << training_mulike[i] << std::endl;
+        //std::cout << "DEBUG: training_mulike[" << i << "] = " << training_mulike[i] << std::endl;
     }
 
     TFile *file_testingsample = new TFile("../particle_gun/output/ks_testingsample.root");
     TGraph* ks_test_bdt_mulike_pilike_test  = (TGraph*)file_testingsample.Get("ks_test_bdt_mulike_pilike_test");
     TGraph* ks_test_bdt_plike_elike_test  = (TGraph*)file_testingsample.Get("ks_test_bdt_plike_elike_test");
+    Int_t n_testing = ks_test_bdt_mulike_pilike_test->GetN();
+    Double_t testing_mulike[n_testing];
+    Double_t testing_pilike[n_testing];
+    Double_t testing_plike[n_testing];
+    Double_t testing_elike[n_testing];
     
+    std::cout << "Reading " << n_testing << " points from testing sample..." << std::endl;
     
+    for (Int_t i=0; i < n_testing; i++)
+    {
+        ks_test_bdt_mulike_pilike_test->GetPoint(i,testing_mulike[i],testing_pilike[i]);
+        //std::cout << "DEBUG: testing_mulike[" << i << "] = " << testing_mulike[i] << std::endl;
+    }
+    
+    Double_t ks_result_mulike = TMath::KolmogorovTest(n_training, training_mulike, n_testing, testing_mulike, "D");
+    std::cout << "Mu-like output KS test result: " << ks_result_mulike << std::endl;
 }
