7a342c8577eb3e458e0f09075883945eda8f22ce
Gabriel Penn <callidus@hotmail.co.uk>
2023-01-19 14:49:03 +0000

    Add debug hists

--- customplotting/combined/KS_tests.C
+++ customplotting/combined/KS_tests.C
@@ -7,12 +7,14 @@
     Double_t training_pilike[n_training];
     Double_t training_plike[n_training];
     Double_t training_elike[n_training];
+    TH1F* training_mulike_debug_hist = new TH1F("training_mulike_debug_hist", "training_mulike_debug_hist", 20, 0.0, 1.0);
     
     std::cout << "Reading " << n_training << " points from training sample..." << std::endl;
     
     for (Int_t i=0; i < n_training; i++)
     {
         ks_test_bdt_mulike_pilike_train->GetPoint(i,training_mulike[i],training_pilike[i]);
+        training_mulike_debug_hist->Fill(training_mulike[i]);
         //std::cout << "DEBUG: training_mulike[" << i << "] = " << training_mulike[i] << std::endl;
         ks_test_bdt_plike_elike_train->GetPoint(i,training_plike[i],training_elike[i]);
     }
@@ -25,12 +27,14 @@
     Double_t testing_pilike[n_testing];
     Double_t testing_plike[n_testing];
     Double_t testing_elike[n_testing];
+    TH1F* testing_mulike_debug_hist = new TH1F("testing_mulike_debug_hist", "testing_mulike_debug_hist", 20, 0.0, 1.0);
     
     std::cout << "Reading " << n_testing << " points from testing sample..." << std::endl;
     
     for (Int_t i=0; i < n_testing; i++)
     {
         ks_test_bdt_mulike_pilike_test->GetPoint(i,testing_mulike[i],testing_pilike[i]);
+        testing_mulike_debug_hist->Fill(testing_mulike[i]);
         //std::cout << "DEBUG: testing_mulike[" << i << "] = " << testing_mulike[i] << std::endl;
         ks_test_bdt_plike_elike_test->GetPoint(i,testing_plike[i],testing_elike[i]);
     }
@@ -43,4 +47,8 @@
     std::cout << "Proton-like output KS test result: " << ks_result_plike << std::endl;
     Double_t ks_result_elike = TMath::KolmogorovTest(n_training, training_elike, n_testing, testing_elike, "D");
     std::cout << "Electron-like output KS test result: " << ks_result_elike << std::endl;
+    
+    TCanvas* mulike_debug_canvas = new TCanvas("mulike_debug_canvas","mulike_debug_canvas",200,10,1000,600);
+    training_mulike_debug_hist->Draw();
+    testing_mulike_debug_hist->Draw();
 }
