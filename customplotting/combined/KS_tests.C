fc5eb8d50fb7dc3cfb875822fa53b530662e998b
Gabriel Penn <callidus@hotmail.co.uk>
2023-01-19 11:42:27 +0000

    Fix file names

--- customplotting/combined/KS_tests.C
+++ customplotting/combined/KS_tests.C
@@ -1,5 +1,5 @@
 {
-    TFile *file_trainingsample = new TFile("../particle_gun/output/ks_trainingsample.root.root");
+    TFile *file_trainingsample = new TFile("../particle_gun/output/ks_trainingsample.root");
     TGraph* ks_test_bdt_mulike_pilike_train  = (TGraph*)file_trainingsample.Get("ks_test_bdt_mulike_pilike_train");
     TGraph* ks_test_bdt_mulike_pilike_train  = (TGraph*)file_trainingsample.Get("ks_test_bdt_mulike_pilike_train");
     Int_t n_training = ks_test_bdt_mulike_pilike_train->GetN();
@@ -16,7 +16,7 @@
         std::cout << "DEBUG: training_mulike[" << i << "] = " << training_mulike[i] << std::endl;
     }
 
-    TFile *file_testingsample = new TFile("../particle_gun/output/ks_testingsample.root.root");
+    TFile *file_testingsample = new TFile("../particle_gun/output/ks_testingsample.root");
     TGraph* ks_test_bdt_mulike_pilike_test  = (TGraph*)file_testingsample.Get("ks_test_bdt_mulike_pilike_test");
     TGraph* ks_test_bdt_mulike_pilike_test  = (TGraph*)file_testingsample.Get("ks_test_bdt_mulike_pilike_test");
     
