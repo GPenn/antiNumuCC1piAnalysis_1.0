525dde2dc68caf6219c9bcfd89c0f8aec64fc836
Gabriel Penn <callidus@hotmail.co.uk>
2023-02-06 12:15:12 +0000

    Update KS_tests.C

--- customplotting/combined/KS_tests.C
+++ customplotting/combined/KS_tests.C
@@ -103,9 +103,9 @@
     bdt_mulike_antimu_train->Draw();
     bdt_mulike_antimu_test->Draw("same");
     
-    Double_t ks_result_hists = training_mulike_debug_hist->KolmogorovTest(testing_mulike_debug_hist, "D");
+    Double_t ks_result_hists = training_mulike_debug_hist->KolmogorovTest(testing_mulike_debug_hist, "XD");
     std::cout << "Debug hist KS test result: " << ks_result_hists << std::endl;
     
-    Double_t ks_result_bdt_mulike_antimu = bdt_mulike_antimu_train->KolmogorovTest(bdt_mulike_antimu_test, "D");
+    Double_t ks_result_bdt_mulike_antimu = bdt_mulike_antimu_train->KolmogorovTest(bdt_mulike_antimu_test, "XD");
     std::cout << "bdt_mulike_antimu KS test result: " << ks_result_bdt_mulike_antimu << std::endl;
 }
